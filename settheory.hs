7b4224219e8973d57727e6a50b1250e2a0e07f93
katherineye <kye@princeton.edu>
2016-09-27 20:10:47 -0400

    color in intersection of 2-set diagram, need to do 3-arc then sets/parser/expr then 3-set diagram

--- settheory.hs
+++ settheory.hs
@@ -72,9 +72,58 @@ b = St "B"
 c :: Set
 c = Intersection a b
 
-venn :: Diagram B
-venn = (cir <> cir # translateX 1) # opacity 0.3
-     where cir = circle 1 # lw none # fc red
+--------------------
 
-main = mainWith venn
+andThen t1 t2 = t1 <> t2 # rotate (angleBetween d1 d2)
+  where
+    d1 = tangentAtEnd t1
+    d2 = tangentAtStart t2
+
+str = fromOffsets [unitX]
+cap = arc (negated xDir) (1/2 @@ turn)
+arm = str `andThen` cap `andThen` str
+
+armUnit = arm `andThen` (arc xDir (3/10 @@ turn) # reflectX)
+
+example :: Diagram B
+example = foldr andThen mempty (replicate 5 armUnit)
+  # glueLine # strokeLoop # fc blue
+  # rotateBy (1/20)
+  # centerXY # pad 1.1 # sized (mkWidth 2)
+
+--------------------
+
+vennRed :: Diagram B
+vennRed = (cir <> cir # translateX 1) # opacity 0.3
+       where cir = circle 1 # lw none # fc red
+
+-- figure out how to make a "circle" loop and fill it X?
+-- figure out what angle, and fill that X
+-- figure out how to draw 3-arc and fill it
+-- sets, parser, expression, etc.
+arctest :: Diagram B
+arctest = (arc (d angle) a <> arc (d (-(1/4 - 1/12))) a)
+        -- # lc red
+        # closeLine # strokeLoop # fc red # lw none # centerXY
+        where angle = 1/4 + 1/12 -- 1/4 of a circle + 1/12
+              d :: Double -> Direction V2 Double
+              d a1 = rotateBy a1 xDir
+              a :: Angle Double
+              a = tau / 3 @@ rad
+
+venn :: Diagram B
+venn = cir # translateX (-0.5) <> cir # translateX 0.5
+       where cir = circle 1
+----
+
+p = pentagon 1 # onLineSegments init
+
+example_pentagon :: Diagram B
+example_pentagon = iterateN 5 (rotateBy (1/5)) p
+   # mconcat # glueLine # strokeLoop
+   # fc green # centerXY # pad 1.1 # sized (mkWidth 2)
+
+main = mainWith (venn <> arctest)
+-- mainWith (armUnit # glueLine # strokeLoop # fc blue)
+-- mainWith (venn <> ab)
 -- main = putStrLn (show keenanSpec)
