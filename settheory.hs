fe677292610753771a7c6d315f14955c8372eef3
katherineye <kye@princeton.edu>
2016-09-29 10:24:16 -0400

    finish visualizing 2-sets (without parser and universe/complement), add some examples

--- settheory.hs
+++ settheory.hs
@@ -20,9 +20,12 @@
 -}
 
 import Data.List
+import qualified Data.Set as Set
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import Diagrams.BoundingBox        
+import Debug.Trace
+import Diagrams.TwoD.Text
 
 data Set =
      St String
@@ -74,39 +77,95 @@ c = Intersection a b
 
 --------------------
 
--- some unrelated / helpful examples from the diagrams site
-        
-andThen t1 t2 = t1 <> t2 # rotate (angleBetween d1 d2)
-  where
-    d1 = tangentAtEnd t1
-    d2 = tangentAtStart t2
+-- TODO:
+-- parser: read command-line argument, deal with 2 arbitrary set names
+-- batch visualizer: draw all examples   
+-- figure out how to fill 3-arc
+-- support complements and universe
+-- generalize to 3 sets   
+data Region = ASubB | AIntersectB | BSubA deriving (Eq, Ord)
 
-str = fromOffsets [unitX]
-cap = arc (negated xDir) (1/2 @@ turn)
-arm = str `andThen` cap `andThen` str
+-- using Haskell sets
+a_regions :: Set.Set Region
+a_regions = Set.fromList [ASubB, AIntersectB]
 
-armUnit = arm `andThen` (arc xDir (3/10 @@ turn) # reflectX)
+b_regions :: Set.Set Region
+b_regions = Set.fromList [BSubA, AIntersectB]
 
-example :: Diagram B
-example = foldr andThen mempty (replicate 5 armUnit)
-  # glueLine # strokeLoop # fc blue
-  # rotateBy (1/20)
-  # centerXY # pad 1.1 # sized (mkWidth 2)
+-- examples: empty, Universe, A, B, A n B, A u B, A \ B, B \ A, A \ B u B \ A (A del B), A^c, A^c U B^c, (A n B)^c, (A U B)^c, A^c n B^c, U n A, U n <expr>
+ex_empty :: Set
+ex_empty = Empty
+
+ex_intersect = Intersection a b
+ex_union = Union a b
+ex_AsubB = Minus a b
+ex_BsubA = Minus b a
+ex_AuA = Union a a
+ex_sym_diff = Union ex_AsubB ex_BsubA
+
+-- doesn't deal with these yet b/c fill issues
+ex_Ac = Complement a
+ex_Bc = Complement b
+ex_c_identity_1a = Union ex_Ac ex_Bc
+ex_c_identity_1b = Complement ex_intersect
+ex_c_identity_2a = Intersection ex_Ac ex_Bc
+ex_c_identity_2b = Complement ex_union
+
+ex_universe = Universe
+ex_universe_id = Intersect ex_universe a
+ex_universe_id2 = Intersect ex_universe ex_c_identity_2a
 
 ----
 
-p = pentagon 1 # onLineSegments init
+-- does not currently support custom set names
+regions :: String -> Set.Set Region
+regions "A" = a_regions
+regions "B" = b_regions
+regions _ = Set.empty
 
-example_pentagon :: Diagram B
-example_pentagon = iterateN 5 (rotateBy (1/5)) p
-   # mconcat # glueLine # strokeLoop
-   # fc green # centerXY # pad 1.1 # sized (mkWidth 2)
-   
---------------------
+ast_to_regions :: Set -> Set.Set Region
+ast_to_regions set =
+               case set of
+                  St st              -> regions st
+                  Empty              -> Set.empty
+                  Union s1 s2        -> ast_to_regions s1 `Set.union`
+                                        ast_to_regions s2
+                  Intersection s1 s2 -> ast_to_regions s1 `Set.intersection`
+                                        ast_to_regions s2
+                  Minus s1 s2        -> ast_to_regions s1 `Set.difference`
+                                        ast_to_regions s2
+                  Universe           -> Set.empty -- TODO
+                  Complement s1      -> Set.empty -- TODO
 
--- figure out how to fill 3-arc
--- sets, parser, expression, etc.
--- generalize to 3 sets   
+-- note: leftmost on top?
+regions_to_diagram :: [Region] -> [Diagram B]
+regions_to_diagram [] = [venn]
+regions_to_diagram (region:regions) = result : (regions_to_diagram regions)
+                   where result = case region of
+                                  ASubB       -> a_minus_b 
+                                  AIntersectB -> a_intersect_b
+                                  BSubA       -> b_minus_a
+
+-- TODO abstract out walking over AST
+prettyPrint :: Set -> String
+prettyPrint set =
+               case set of
+                  St st              -> st
+                  Empty              -> "∅"
+                  Union s1 s2        -> prettyPrint s1 ++ " ∪ " ++
+                                        prettyPrint s2
+                  Intersection s1 s2 -> prettyPrint s1 ++ " ∩ " ++
+                                        prettyPrint s2
+                  Minus s1 s2        -> prettyPrint s1 ++ " ∖ " ++
+                                        prettyPrint s2
+                  Universe           -> "Ω"
+                  Complement s1      -> "ᶜ"
+
+-- change this to change what it draws
+ex_to_use = ex_sym_diff
+        
+----
+
 a_intersect_b :: Diagram B
 a_intersect_b = (arc (d angle) a <> arc (d (-(1/4 - 1/12))) a)
         -- # lc red
@@ -146,5 +205,10 @@ venn = cir # translateX (-0.5) <> cir # translateX 0.5
        where cir = circle 1
 
 -- leftmost on top. TODO put venn on top
-main = mainWith (b_minus_a <> a_minus_b <> a_intersect_b <> venn)
+main = mainWith
+     ((alignedText 0 0 $ prettyPrint ex_to_use) # scale 0.2
+     <> (mconcat $ regions_to_diagram
+     $ Set.toList $ ast_to_regions
+     ex_to_use))
+-- main = mainWith (b_minus_a <> a_minus_b <> a_intersect_b <> venn)
 -- main = putStrLn (show keenanSpec)
