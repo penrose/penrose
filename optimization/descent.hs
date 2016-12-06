21ecb524abef87598637eae422ba79d1b8f11833
katherineye <kye@princeton.edu>
2016-12-06 17:52:12 -0500

    add simple objective functions (parabola) and GIFs

--- optimization/descent.hs
+++ optimization/descent.hs
@@ -3,7 +3,7 @@ import Data.Function
 import Graphics.Gloss.Interface.Pure.Game
 
 main = play                    -- TODO change to play
-       (InWindow "Nice Window" -- display mode, window name
+       (InWindow "optimization-based layout" -- display mode, window name
                   (picWidth, picHeight)   -- size
                   (10, 10))    -- position
        white                   -- background color
@@ -30,10 +30,14 @@ initState = State { x = 0, y = 0, down = False }
 divf :: Int -> Int -> Float
 divf a b = (fromIntegral a) / (fromIntegral b)
 
+pw2 = picWidth `divf` 2
+ph2 = picHeight `divf` 2
+
 picOf :: State -> Picture
-picOf s = Pictures [line', pt']
+picOf s = Pictures [line', pt', objectiveTxt]
     where pt' = color (light violet) $ translate (x s) (y s) $ circleSolid 10
-          line' = Line [(-picWidth `divf` 2, 0), (picWidth `divf` 2, 0)]
+          line' = Line [(-pw2, 0), (pw2, 0)]
+          objectiveTxt = translate (-pw2+50) (ph2-100) $ scale 0.2 0.2 $ text "objective function: f(x) = -x^2"
 
 handler :: Event -> State -> State
 handler (EventKey (MouseButton LeftButton) Down _ (xm, ym)) s = State { x = xm, y = y s, down = True }
@@ -43,7 +47,20 @@ handler (EventMotion (xm, ym)) s =
 handler (EventKey (MouseButton LeftButton) Up _ (xm, ym)) s = State {x = xm, y = y s, down = False }
 handler _ s = s
 
--- TODO gradient descent here
+clamp :: Float -> Float
+clamp x = if x < -pw2 then -pw2 else if x > pw2 then pw2 else x
+
+-- implement gradient descent
+-- TODO: is there a haskell autodifferentiator?
 step :: Float -> State -> State
 step t s = if down s then s -- don't step when dragging
-           else State { x = x s + 1, y = y s, down = down s}
+           else State { x = clamp x', y = y s, down = down s}
+                where x' = neg_parabola' (x s) t
+
+-- objective function, differentiated and discretized
+-- f(x) = x^2
+parabola' :: Float -> Float -> Float
+parabola' x t = x - t * 2 * x
+
+neg_parabola' :: Float -> Float -> Float
+neg_parabola' x t = x + t * 2 * x
