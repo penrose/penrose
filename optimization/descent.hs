b738bf5e5121a98b215aae4a172b21850789251e
katherineye <kye@princeton.edu>
2016-12-07 18:32:35 -0500

    generalize to 2D gradient descent on a list of objects (circ, label). need to add dragging, state resampling, and better objective function

--- optimization/descent.hs
+++ optimization/descent.hs
@@ -19,12 +19,26 @@ picWidth = 800
 picHeight :: Int
 picHeight = 700
 
-data State = State { x :: Float
-                   , y :: Float
+-- gonna need typeclasses...
+data Circ = Circ { xc :: Float
+                     , yc :: Float
+                     , r :: Float }
+
+data Label = Label { xl :: Float
+                   , yl :: Float
+                   , textl :: String
+                   , scalel :: Float } -- calculate h,w from it
+
+data Obj = C Circ | L Label -- | Label | Point | Line
+
+data State = State { objs :: [Obj]
                    , down :: Bool } -- left mouse button is down (dragging)
 
+-- TODO randomly sample s0
 initState :: State
-initState = State { x = 0, y = 0, down = False }
+initState = State { objs = objsInit, down = False }
+          where objsInit = [C $ Circ { xc = -300, yc = 300, r = 30 },
+                            L $ Label { xl = 500, yl = -500, textl = "CircLabel", scalel = 0.2 } ]
 
 -- divide two integers to obtain a float
 divf :: Int -> Int -> Float
@@ -33,34 +47,71 @@ divf a b = (fromIntegral a) / (fromIntegral b)
 pw2 = picWidth `divf` 2
 ph2 = picHeight `divf` 2
 
-picOf :: State -> Picture
-picOf s = Pictures [line', pt', objectiveTxt]
-    where pt' = color (light violet) $ translate (x s) (y s) $ circleSolid 10
-          line' = Line [(-pw2, 0), (pw2, 0)]
-          objectiveTxt = translate (-pw2+50) (ph2-100) $ scale 0.2 0.2 $ text "objective function: f(x) = -x^2"
+renderCirc :: Circ -> Picture
+renderCirc c = color (light violet) $ translate (xc c) (yc c) $ circle (r c)
 
+renderLabel :: Label -> Picture
+renderLabel l = color azure $ scale 0.2 0.2 $ translate (xl l) (yl l) $ text (textl l)
+
+renderObj :: Obj -> Picture
+renderObj (C circ) = renderCirc circ
+renderObj (L label) = renderLabel label
+
+picOfState :: State -> Picture
+picOfState s = Pictures $ map renderObj (objs s)
+
+picOf :: State -> Picture
+picOf s = Pictures [lineX, lineY, picOfState s, objectiveTxt]
+    where lineX = Line [(-pw2, 0), (pw2, 0)]
+          lineY = Line [(0, -ph2), (0, ph2)]
+          objectiveTxt = translate (-pw2+50) (ph2-100) $ scale 0.2 0.2 
+                         $ text "objective function: f(x, y) = x^2 + y^2"
+
+-- TODO "in object" tests
+-- TODO what if you press a key while down? then reset the entire state (then Up will just reset again)
 handler :: Event -> State -> State
-handler (EventKey (MouseButton LeftButton) Down _ (xm, ym)) s = State { x = xm, y = y s, down = True }
+handler (EventKey (MouseButton LeftButton) Down _ (xm, ym)) s = 
+        State { objs = objs s, down = True }
 -- dragging mouse when down
-handler (EventMotion (xm, ym)) s =
-        if down s then State {x = xm, y = y s, down = down s } else s
-handler (EventKey (MouseButton LeftButton) Up _ (xm, ym)) s = State {x = xm, y = y s, down = False }
+handler (EventMotion (xm, ym)) s = 
+        if down s then State { objs = objs s, down = down s }
+        else s
+handler (EventKey (MouseButton LeftButton) Up _ (xm, ym)) s = 
+        State { objs = objs s, down = False }
 handler _ s = s
 
-clamp :: Float -> Float
-clamp x = if x < -pw2 then -pw2 else if x > pw2 then pw2 else x
+-- TODO clamp needs to take into account bbox
+clampX :: Float -> Float
+clampX x = if x < -pw2 then -pw2 else if x > pw2 then pw2 else x
+
+clampY :: Float -> Float
+clampY y = if y < -ph2 then -ph2 else if y > ph2 then ph2 else y
 
 -- implement gradient descent
 -- TODO: is there a haskell autodifferentiator?
+-- TODO: step state
 step :: Float -> State -> State
 step t s = if down s then s -- don't step when dragging
-           else State { x = clamp x', y = y s, down = down s}
-                where x' = neg_parabola' (x s) t
+           else State { objs = map (stepObj t) (objs s), down = down s}
+
+-- currently ignores rest of state 
+-- TODO typeclasses???
+-- TODO differentiate type-level b/t timestep and coord
+stepObj :: Float -> Obj -> Obj
+stepObj t (C c) = C $ Circ { xc = x', yc = y', r = r c }
+        where (x', y') = gradDescent t (xc c) (yc c)
+stepObj t (L l) = L $ Label { xl = x', yl = y', textl = textl l, scalel = scalel l }
+        where (x', y') = gradDescent t (xl l) (yl l)
+
+gradDescent :: Float -> Float -> Float -> (Float, Float)
+gradDescent t x y = (clampX x', clampY y')
+            where x' = parabola' t x
+                  y' = parabola' t y
 
 -- objective function, differentiated and discretized
 -- f(x) = x^2
 parabola' :: Float -> Float -> Float
-parabola' x t = x - t * 2 * x
+parabola' t x = x - t * 2 * x
 
 neg_parabola' :: Float -> Float -> Float
-neg_parabola' x t = x + t * 2 * x
+neg_parabola' t x = x + t * 2 * x
