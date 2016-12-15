1bd044c33bc96f87a85aebb0f9ce9d4d5615031c
katherineye <kye@princeton.edu>
2016-12-14 22:52:02 -0500

    add obj function to center two sets while repelling them. not an accurate 2D obj fn and needs constraints

--- optimization/descent.hs
+++ optimization/descent.hs
@@ -104,13 +104,15 @@ initRng = mkStdGen seed
 
 rad = 150 -- TODO don't hardcode into constant
 
+clamp1D y = if clampflag then 0 else y
+
 -- TODO randomly sample s0
 initState :: State
 initState = State { objs = objsInit, down = False, rng = initRng }
-          where objsInit = [l1, c1] -- only handles two objects!
-                c1 = C $ Circ { xc = -300, yc = 200, r = rad, selc = False }
-                c2 = C $ Circ { xc = 300, yc = -200, r = rad, selc = False }          
-                l1 = L $ Label { xl = -100, yl = 200, textl = "B1", scalel = 0.2, sell = False }
+          where objsInit = [c1, c2] -- only handles two objects!
+                c1 = C $ Circ { xc = -300, yc = clamp1D 200, r = rad, selc = False }
+                c2 = C $ Circ { xc = 300, yc = clamp1D (-200), r = rad-50, selc = False }          
+                l1 = L $ Label { xl = -100, yl = clamp1D 200, textl = "B1", scalel = 0.2, sell = False }
 
 -- divide two integers to obtain a float
 divf :: Int -> Int -> Float
@@ -141,11 +143,11 @@ picOf s = Pictures [picOfState s, objectiveTxt]
     where lineX = Line [(-pw2, 0), (pw2, 0)] -- unused
           lineY = Line [(0, -ph2), (0, ph2)]
           objectiveTxt = translate (-pw2+50) (ph2-50) $ scale 0.1 0.1
-                         $ text "objective: settle in center of set or right outside of it"
+                         $ text "objective: stay close to the center but away from other set"
 
 -- TODO 1D clamp flag
 sample :: RandomGen g => g -> Obj -> (Obj, g)
-sample gen o = (setX x' $ setY y' o, gen'')
+sample gen o = (setX x' $ setY (clamp1D y') o, gen'')
        where (x', gen') = randomR widthRange gen
              (y', gen'') = randomR heightRange gen'
 
@@ -179,7 +181,9 @@ handler (EventKey (MouseButton LeftButton) Down _ (xm, ym)) s =
 handler (EventMotion (xm, ym)) s =
         if down s then s { objs = map (ifSelectedMoveTo (xm, ym)) (objs s), down = down s }
         else s
-        where ifSelectedMoveTo (xm, ym) o = if selected o then setX xm $ setY ym o else o
+        where ifSelectedMoveTo (xm, ym) o = if selected o
+                                            then setX xm $ setY (clamp1D ym) o else o
+
 handler (EventKey (MouseButton LeftButton) Up _ _) s =
         s { objs = map deselect $ objs s, down = False }
 
@@ -204,16 +208,40 @@ firstTwo (x1 : x2 : _) = (x1, x2)
 -- TODO: is there a haskell autodifferentiator?
 -- TODO: step state
 type Time = Float
-   
+
 step :: Time -> State -> State
 step t s = -- if down s then s -- don't step when dragging 
-            s { objs = stepObjs t $ firstTwo (objs s), down = down s}
+            if stepFlag then s { objs = stepObjs t $ firstTwo (objs s), down = down s} else s
 
 stepObjs :: Located a => Time -> (a, a) -> [a]
 stepObjs t (o1, o2) = [setX x1'c $ setY y1'c o1, setX x2'c $ setY y2'c o2]
         where (x1, y1, x2, y2) = (getX o1, getY o1, getX o2, getY o2)
-              (x1', x2', y1', y2') = cubicCenterOrRadius t x1 x2 y1 y2
+              (x1', x2', y1', y2') = centerAndRepel t x1 x2 y1 y2
               (x1'c, x2'c, y1'c, y2'c) = (clampX x1', clampX x2', clampY y1', clampY y2')
+
+stepT :: Time -> Float -> Float -> Float
+stepT t x dxdt = x - t * dxdt
+
+stepFlag = True
+clampflag = False
+debug = False
+
+debugF = if debug then traceShowId else id
+
+-- TODO factor out the stepT
+-- TODO can't figure out how to get the repelling behavior as optimization; may have to be a constraint
+centerAndRepel :: Time -> Float -> Float -> Float -> Float -> (Float, Float, Float, Float)
+centerAndRepel t x1 x2 y1 y2 = ({-traceShowId $ -}stepT t' x1 dx1dt, stepT t' x2 dx2dt,
+                                     stepT t' y1 dy1dt, stepT t' y2 dy2dt)
+              where t' = t/10000
+                    -- first two terms repel from other circle; last term attracts to center
+                    dx1dt = debugF $ -2*x1^3 + 2*x2^3 + 4*x1^3
+                    dx2dt = 2*x1^3 - 2*x2^3 + 4*x2^3
+                   -- TODO not correct wrt minimizing 2d distance, but it works well enough
+                    dy1dt = -2*y1^3 + 2*y2^3 + 4*y1^3 
+                    dy2dt = 2*y1^3 - 2*y2^3 + 4*y2^3
+
+------- parameters specific to cubicCenterOrRadius
 
 eps :: Float
 eps = 60 -- why is this 100??
@@ -245,9 +273,6 @@ gradClamp g = if abs g < zeroClamp then 0 -- clamp down
               else g
               where sign = if g < 0 then -1 else 1
 
-stepT :: Time -> Float -> Float -> Float
-stepT t x dx = x - t * dx
-
 -- objective functions, differentiated and discretized
 -- attract label to center of circle or to outside of circle
 -- wolframalpha: derivative with respect to x1 of f(x1, y1, x2, y2) = (sqrt((x1-x2)^2 + (y1-y2)^2))^3 - (c1 + c2) (sqrt((x1-x2)^2 + (y1-y2)^2))^2 + c1 * c2 * (sqrt((x1-x2)^2 + (y1-y2)^2))
@@ -255,19 +280,15 @@ stepT t x dx = x - t * dx
 cubicCenterOrRadius :: Time -> Float -> Float -> Float -> Float -> (Float, Float, Float, Float)
 cubicCenterOrRadius t x1 x2 y1 y2 = ({-traceShowId $ -}stepT t' x1 dx1, stepT t' x2 dx2,
                                      stepT t' y1 dy1, stepT t' y2 dy2)
-                    -- TODO factor out common parts
-                    -- should these all be the same? oh, they were all the function--i didn't run the code
               where t' = t/tFactor -- otherwise it jitters b/t -inf and inf, doesn't reach zeroes
-                    -- also: doesn't settle in the outside correctly
-                    -- instantly jumps to inside bc grad is probably very large, but slow inside...
+                    -- doesn't settle in the outside correctly
+                    -- instantly jumps to inside bc grad is probably very large, but slow inside
                     -- and if too far away, jitters between -bigval and bigval until clicked/dragged
                     -- also, these are in fact dx1/dt (etc.)
-                    -- bug: needed sqrt for distance, otherwise zeros misplaced. type the fn properly
-                    -- need to hand-calibrate timestepping and clamping...
-                    -- don't know if different grad fns will play well
+                    -- need to hand-calibrate timestepping and clamping
                     -- TODO if x1 = x2 and y1 = y2, then NaN
                     -- TODO step each one WRT the already-stepped ones to reduce jitter?
-                    dx1 = gradClamp $ traceShowId $ (-2)*(c1 + c2)*(x1 - x2) + (c1*c2*(x1 - x2))/sqrt((x1 - x2)^2 + (y1 - y2)^2) + 3*(x1 - x2)*sqrt((x1 - x2)^2 + (y1 - y2)^2)
+                    dx1 = gradClamp $ {-traceShowId $-} (-2)*(c1 + c2)*(x1 - x2) + (c1*c2*(x1 - x2))/sqrt((x1 - x2)^2 + (y1 - y2)^2) + 3*(x1 - x2)*sqrt((x1 - x2)^2 + (y1 - y2)^2)
                     dy1 = gradClamp $ (-2)*(c1 + c2)*(y1 - y2) + (c1*c2*(y1 - y2))/sqrt((x1 - x2)^2 + (y1 - y2)^2) + 3*sqrt((x1 - x2)^2 + (y1 - y2)^2)*(y1 - y2)
                     -- same as dx1 and dy1 except moving toward each other
                     dx2 = -1 * (gradClamp $ traceShowId $ (-2)*(c1 + c2)*(x1 - x2) + (c1*c2*(x1 - x2))/sqrt((x1 - x2)^2 + (y1 - y2)^2) + 3*(x1 - x2)*sqrt((x1 - x2)^2 + (y1 - y2)^2))
