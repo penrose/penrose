c67c5e116cbbc85494ad8c441f31d97ea8e14f4b
katherineye <kye@princeton.edu>
2017-01-07 20:47:16 -0500

    add Style grammar and parser for Shape lines only, Style prog for continuousmap1, and TODOs for ILR/ALR (layout representation)

--- compiler/compiler.hs
+++ compiler/compiler.hs
@@ -23,10 +23,14 @@ data SubObj = OS Set | OP Pt | OM Map
      deriving (Show, Eq)
 -- TODO open sets?? they should inherit style characteristics from sets
 
+data SubObjType = Set SetType | Pt | Map -- how to tie this to the types above
+     deriving (Show, Eq)
+
 data SubDecl = Decl SubObj
      deriving (Show, Eq)
 
 -- TODO vs. Set Set. user only specifies names
+-- TODO we assume that non-subset sets must not overlap (implicit constraint)
 data SubConstraint = Subset String String | PointIn String String
      deriving (Show, Eq)
 
@@ -106,18 +110,142 @@ subValidate s = (s == (subPrettyPrint $ subParse s))
 subValidateAll :: [String] -> Bool
 subValidateAll = all subValidate
 
--- Substance typechecker TODO
+-- Substance reference checker TODO
+-- returns lines in same order as in program
+subSeparate :: SubSpec -> SubSpecDiv
+subSeparate = foldr separate ([], [])
+            where separate line (decls, constrs) =
+                           case line of
+                           (LD x) -> (x : decls, constrs)
+                           (LC x) -> (decls, x : constrs)
 
--- Substance reference checker
+-- Substance typechecker TODO
 
 ---------------------------------------
 
--- Style grammar (relies on the Substance grammar...)
+-- Style grammar (relies on the Substance grammar, specifically SubObj)
+-- no styling for constraints
 
--- Style parser
+data SetShape = SetCircle | Box
+     deriving (Show, Eq)
+
+data PtShape = SolidCirc
+     deriving (Show, Eq)
+
+data MapShape = LeftArr | RightArr | DoubleArr
+     deriving (Show, Eq)
+
+data Direction = Horiz | Vert | Angle
+     deriving (Show, Eq)
+
+data SubShape = SS SetShape | SP PtShape | SM MapShape
+     deriving (Show, Eq)
+
+data LineType = Solid | Dotted 
+     deriving (Show, Eq)
+
+data Color = Red | Blue | Black | Yellow -- idk
+     deriving (Show, Eq)
+
+data M a = Auto | Override a -- short for Maybe (option type)
+     deriving (Show, Eq)
+
+data StyLevel = SubVal String | LabelOfSubVal String | SubType SubObjType | Global
+     deriving (Show, Eq)
+-- LabelOfSubObj is meant to allow styling of whatever label object A (originally named A) now has
+-- e.g. it could be named "hello" with Label (SubValue "A") (Override "Hello"). (don't label labels)
+-- then do (Position (LabelOfSubObj "A") 100 200)
+
+type Opacity = Float -- 0 to 100%
+type Priority' = Float -- higher = higher priority
+type Angle = Float
+
+-- There are three different layers of Style: global setting (over all types), 
+-- type setting (over all values of that type), and value setting. 
+-- The more specific ones implicitly override the more general ones.
+-- If there are two conflicting ones at the same level, the more recent one will be used for "everything"
+-- TODO more sophisticated system with scope
+-- TODO if some aspect of style unspecified, then supplement with default
+-- any setting is implicitly an override of the global default style
+-- can choose to leave out an aspect (= implicitly auto), or explicitly specify auto; same thing
+-- TODO need to validate that the shape specified matches that of the type
+-- e.g. Shape Map Diamond is invalid
+-- also, order does not matter between lines
+data StyLine = Shape StyLevel (M SubShape) -- implicitly solid unless line is specified
+               | Line StyLevel (M LineType) (M Float) -- hollow shape; non-negative thickness
+               | Color StyLevel (M Color) (M Opacity)
+               | Priority StyLevel (M Priority') -- for line breaking
+               | Dir StyLevel (M Direction)
+               | Label StyLevel (M String)
+               | Size StyLevel (M Float) -- size and position in pixels
+               | AbsPos StyLevel (M Float) (M Float) -- TODO relative positions
+     deriving (Show, Eq)
+
+type StySpec = [StyLine]
 
 -- Sample Style programs and tests
-sty1 = "Line Set Solid 2\nShape Set Circle"
+-- TODO style is more complicated than substance; this doesn't test it fully
+
+nl = "\n"
+together = intercalate nl
+sty0 = "Shape Global Circle"
+sty1 = "Shape Set Box"
+sty2 = "Shape A Circle"
+sty3 = together [sty0, sty1, sty2] -- test overrides
+sty4 = "Shape Set Circle\nShape Map RightArr\nLine Map Solid"
+-- test all
+sty_all = "Shape Set Circle\nShape Map RightArr\nLine Map Solid\nColor Global Blue 100\nPriority Map 100\nPriority Set 50\nDir Map Horiz\nDir A Vert\nLabel A NewA\nSize A 100\nPosition A -100 501\nColor Label_A Blue 100"
+-- TODO deal with OpenSet, ClosedSet
+-- TODO write tests of substance working *with* style
+
+-- TODO add tests that it should fail
+styf1 = "Shape"
+styf2 = "Shape Label_A"
+
+-- TODO syntactically valid but semantically invalid programs
+styfs1 = "Shape Map Circle"
+
+-- Style parser
+styParse :: String -> StySpec -- same as subParse
+styParse = map (styToLine . words) . filter nonempty . lines
+         where nonempty x = (x /= "")
+
+getLevel :: String -> StyLevel
+getLevel s = if s == "All" then Global
+             -- TODO will need to update parsers whenever I add a new type...
+             else if s == "Set" then SubType (Set Unspecified)
+             else if s == "OpenSet" then SubType (Set Open)
+             else if s == "ClosedSet" then SubType (Set Closed)
+             else if s == "Point" then SubType Pt
+             else if s == "Map" then SubType Map
+             else if (take 6 s == "Label_")
+                  then let res = drop 6 s in
+                       if length res > 0 then LabelOfSubVal res
+                       else error "Empty object name ('Label_') in style level"
+             else SubVal s -- sets could be named anything; later we validate that this ref exists
+
+getShape :: [String] -> M SubShape
+getShape [] = error "No Style shape param"
+getShape [x] = if x == "Auto" then Auto
+               else if x == "Circle" then Override (SS SetCircle)
+               else if x == "Box" then Override (SS Box)
+               else if x == "SolidCircle" then Override (SP SolidCirc)
+               else if x == "LeftArrow" then Override (SM LeftArr)
+               else if x == "RightArrow" then Override (SM RightArr)
+               else if x == "DoubleArrow" then Override (SM DoubleArr)
+               else error $ "Invalid shape param '" ++ show x ++ "'"
+getShape s = error $ "Too many style shape params in '" ++ show s ++ "'"
+
+styToLine :: [String] -> StyLine
+styToLine [] = error "Style spec line is empty"
+styToLine s@[x] = error $ "Style spec line '" ++ show s ++ "' is only 1 token"
+styToLine s@(x : y : xs) = let level = getLevel y in -- throws its own errors
+                           if x == "Shape" then
+                              let shp = getShape xs in
+                              Shape level shp
+                           -- TODO write out the rest of this
+                           else error $ "Style spec line: '" ++ show s
+                                ++ "' does not begin with Shape/Line/Color/Priority/Direction/Label/Size/Position"
 
 -- Pretty-printer for Style AST
 
@@ -130,9 +258,24 @@ sty1 = "Line Set Solid 2\nShape Set Circle"
 ---------------------------------------
 
 -- Take a Substance and Style program, and produce the abstract layout representation
--- TODO try doing this w/o Style first?
 
--- Also produce the constraints and objective function
+-- TODO try doing this w/o Style first? everything is compiled to a default style with default labels
+-- write out applying Style on top: applying global overrides, then by type, then by name
+-- going to need some kind of abstract intermediate type
+-- figure out the intermediate subsets of the language I can support 
+  -- e.g. map requires me to draw arrows, don't support direction and priority for now
+-- how many objects I can support, e.g. A -> B -> C -> D requires scaling the size of each obj to fit on canvas
+-- how the optimization code needs to scale up to meet the needs of multiple objects (labels only?)
+-- how to lay things out w/ constraints only (maybe)
+-- how to apply optimization to the labels & what their obj functions should be
+
+-- TODO finish parser for both, put into Slack tonight w/ description of override, continuousmap1.sub/sty
+
+-- Substance + Style typechecker
+
+-- Substance + Style reference checker
+
+-- Produce the constraints and objective function
 
 -- Add rendering and interaction info to produce a world state for gloss
 
@@ -145,9 +288,11 @@ sty1 = "Line Set Solid 2\nShape Set Circle"
 ---------------------------------------
 
 -- ghc compiler.hs; ./compiler <filename>
-main = do
+subContMapTest = do
        args <- getArgs
        let fileIn = head args
        program <- readFile fileIn
        putStrLn $ show $ subParse program
        putStrLn $ show $ subValidate program
+
+main = subContMapTest
