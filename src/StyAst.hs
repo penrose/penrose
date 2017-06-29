dcc7e79064868e197061f735aecf2ee2bf62d678
wodeni <wn2155@columbia.edu>
2017-06-28 23:56:32 -0400

    [tree-diagram] First working version of the parser

--- src/StyAst.hs
+++ src/StyAst.hs
@@ -1,6 +1,5 @@
--- module StyAst where
--- module Main (main) where
-module StyAst where
+    -- module StyAst where
+module Main (main) where
 
 import Control.Monad (void)
 import Text.Megaparsec
@@ -12,81 +11,232 @@ import qualified Text.Megaparsec.Lexer as L
 
 -- Style grammar (relies on the Substance grammar, specifically SubObj)
 
-data SubType
-    = Set
-    | Pt
-    | Map
+data SubType = Set | Pt | Map | Intersect | NoIntersect | Subset | PointIn | PointNotIn
     deriving (Show, Eq)
 
 data StySpec = StySpec {
     spType :: SubType,
     spId :: String,
-    spShape :: SubShape,
+    spShape :: StyObj,
     spColor :: Color
 } deriving (Show)
 
 type StyProg = [Block]
+-- type Block = (String, [Stmt])
+type Block = (Selector, [Stmt])
 
-data Block
-    = GlobalBlock [Stmt]
-    | TypeBlock SubType [Stmt]
-    | ObjBlock String [Stmt]
-    | ConstraintBlock [Stmt]
+data Selector = Selector
+              { selTyp :: SubType
+              , selPatterns :: [Pattern]
+              , selIds :: [String] }
+              deriving (Show)
+data Pattern
+    = RawID String
+    | WildCard String
     deriving (Show)
-
---     = GlobalBlock [Stmt]
---     | GeneralBlock Selector [Stmt]
--- type Selector = SubType | ID |
 
 data Stmt
     = Assign String Expr
-    | ConstrFn String [Expr]
+    | Fn String [Expr]
+    | Avoid String [Expr]
+    -- | E Expr  -- TODO: is an expression a statement?
     deriving (Show)
 
 data Expr
-    = Id String
-    | Shape SubShape
-    | Color Color
-    | Dir Direction
-    | IntLit Int
+    = IntLit Integer
     | FloatLit Float
+    | Id String
+    | BinOp BinaryOp Expr Expr
+    | Cons StyObj [Stmt] -- Constructors for objects
     deriving (Show)
 
-data SubShape = SS SetShape | SP PtShape | SM MapShape | NoShape
-     deriving (Show, Eq)
+data BinaryOp = Access deriving (Show)
 
-data SetShape = SetCircle | Box
-     deriving (Show, Eq)
+data Color = RndColor | Colo
+          { r :: Float
+          , g :: Float
+          , b :: Float
+          , a :: Float }
+          deriving (Show)
+--
+data StyObj
+    = Circle
+    | Box
+    | Dot
+    | Arrow
+    | NoShape
+    | Color
+    deriving (Show)
 
-data PtShape = SolidDot | HollowDot | Cross
-     deriving (Show, Eq)
+-- data Shape
+--     = Circle [(String, Expr)]
+--     | Box [(String, Expr)]
+--     | Dot [(String, Expr)]
+--     | Arrow [(String, Expr)]
+--     | NoShape
+--     deriving (Show)
 
-data MapShape = SolidArrow
-     deriving (Show, Eq)
+---- Style program
+styleParser :: Parser [Block]
+styleParser = between sc eof styProg
 
-data Direction = Horiz | Vert | Angle Float
-     deriving (Show, Eq)
+-- TODO: required global and/or style block or not?
+-- TODO: How can I write something like noop???
+-- NOTE: sequence matters here
+styProg :: Parser [Block]
+styProg = some block
+-- globalBlock <|> typeBlock <|> objBlock
+-- <|> emptyProg
 
-data LineType = Solid | Dotted
-     deriving (Show, Eq, Read)
+---- Style blocks
+block :: Parser Block
+block = do
+    sel <- selector
+    void (symbol "{")
+    newline'
+    sc
+    stmts <- try stmtSeq
+    -- res <- try stmtSeq
+    -- let stmts = case res of
+    --                 Just a -> a
+    --                 Nothing -> []
+    void (symbol "}")
+    newline'
+    return (sel, stmts)
+    -- return (sel, [])
 
-data Color = Red | Blue | Black | Yellow | Random -- idk
-     deriving (Show, Eq, Read)
+selector :: Parser Selector
+selector = do
+    typ <- subtype
+    -- sc
+    pat <- patterns
+    -- void sc <|> void (symbol ":")
+    -- ids <- many identifier
+    res <- optional $ symbol ":" *> many identifier
+    let ids = case res of
+                Just a -> a
+                Nothing -> []
+    return $ Selector typ pat ids
 
--- Lexer functions
+subtype :: Parser SubType
+subtype = do
+    str <- symbol "Set" <|> symbol "Point" <|> symbol "Subset"
+    return (convert str)
+    where convert s
+            | s == "Set" = Set
+            | s == "Point" = Subset
+            | s == "Subset" = Subset
+styObj :: Parser StyObj
+styObj = do
+    str <- symbol "Color" <|> symbol "None" <|> symbol "Arrow"
+    return (convert str)
+    where convert s
+            | s == "Color" = Color
+            | s == "Arrow" = Arrow
+            | s == "None"  = NoShape
+
+patterns :: Parser [Pattern]
+patterns = many pattern
+    where pattern = (WildCard <$> symbol "_" <|> RawID <$> identifier)
+
+    -- manyTill anyChar (symbol "{")
+---- Statements
+stmtSeq :: Parser [Stmt]
+stmtSeq = endBy stmt newline'
+
+stmt :: Parser Stmt
+stmt =
+    try objFn
+    <|> try assignStmt
+    <|> try avoidObjFn
+--
+assignStmt :: Parser Stmt
+assignStmt = do
+    var  <- attribute
+    sc
+    void (symbol "=")
+    e    <- expr
+    return (Assign var e)
+--
+objFn :: Parser Stmt
+objFn = do
+    fname  <- identifier
+    void (symbol "(")
+    params <- expr `sepBy` comma
+    void (symbol ")")
+    -- params <- sepBy expr comma
+    return (Fn fname params)
+
+avoidObjFn :: Parser Stmt
+avoidObjFn = do
+    rword "avoid"
+    fname  <- identifier
+    lbrac
+    params <- expr `sepBy` comma
+    rbrac
+    return (Avoid fname params)
+
+expr :: Parser Expr
+expr = try objConstructor
+    <|> makeExprParser term operators
+
+term :: Parser Expr
+term = Id <$> identifier
+   <|> number
+
+operators :: [[Operator Parser Expr]]
+operators = [ [ InfixL (BinOp Access <$ symbol ".")] ]
+
+objConstructor :: Parser Expr
+objConstructor = do
+    typ <- styObj
+    lbrac >> newline'
+    stmts <- stmtSeq
+    rbrac  -- NOTE: not consuming the space because stmt already does
+    return $ Cons typ stmts
+
+number :: Parser Expr
+number = IntLit <$> integer <|> FloatLit <$> float
+
+attribute :: Parser String
+attribute = many alphaNumChar
+
+--
+--
+--
+-- typeBlock :: Parser Block
+-- typeBlock = do
+--     t     <- typ
+--     stmts <- braces stmtSeq
+--     return $ TypeBlock t stmts
+--
+-- objBlock :: Parser Block
+-- objBlock = do
+--     i     <- identifier
+--     stmts <- braces stmtSeq
+--     return $ ObjBlock i stmts
+
+
+---- Lexer functions
 sc :: Parser ()
-sc = L.space (void spaceChar) lineCmnt blockCmnt
-  where lineCmnt  = L.skipLineComment "--"
-        blockCmnt = L.skipBlockComment "/*" "*/"
+sc = L.space (void separatorChar) lineCmnt blockCmnt
+  where lineCmnt  = L.skipLineComment "--" >> newline'
+        blockCmnt = L.skipBlockComment "/*" "*/" >> newline'
+
+newline' :: Parser ()
+newline' = void sc >> void (some newline) >> void sc
 
 braces :: Parser a -> Parser a
 braces = between (symbol "{") (symbol "}")
 
-emptyProg :: Parser [Block]
-emptyProg = return []
+lparen, rparen, lbrac, rbrac :: Parser ()
+lbrac = void (symbol "{")
+rbrac = void (symbol "}")
+lparen = void (symbol "(")
+rparen = void (symbol ")")
 
--- noStmt :: Parser Stmt
--- noStmt = return (StmtSeq [])
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
@@ -100,43 +250,8 @@ symbol = L.symbol sc
 integer :: Parser Integer
 integer = lexeme L.integer
 
--- float :: Parser Float
--- float = realToFrac $ lexeme L.float -- TODO: parsing without sign?
-
--- Reserved words
-rword :: String -> Parser ()
-rword w = string w *> notFollowedBy alphaNumChar *> sc
-
-rws, attribs, shapes, types :: [String] -- list of reserved words
-rws =     ["global"]
-types =   ["Set", "Map", "Point"]
-attribs = ["shape", "color", "label", "scale", "position"]
-shapes =  ["None", "Circle", "Box", "SolidArrow", "SolidDot", "HollowDot", "Cross"]
-colors =  ["Random", "Black", "Red", "Blue", "Yellow"]
-
-getShape :: String -> SubShape
-getShape str = case str of
-    "None"       -> NoShape
-    "Circle"     -> SS SetCircle
-    "Box"        -> SS Box
-    "SolidArrow" -> SM SolidArrow
-    "SolidDot"   -> SP SolidDot
-    "HollowDot"  -> SP HollowDot
-    "Cross"      -> SP Cross
-
-getColor :: String -> Color
-getColor str = case str of
-    "Red"    -> Red
-    "Yellow" -> Yellow
-    "Blue"   -> Blue
-    "Black"  -> Black
-    "Random" -> Random
-
-getType :: String -> SubType
-getType str = case str of
-    "Set"   -> Set
-    "Map"   -> Map
-    "Point" -> Pt
+float :: Parser Float
+float = realToFrac <$> lexeme L.float -- TODO: parsing without sign?
 
 identifier :: Parser String
 identifier = (lexeme . try) (p >>= check)
@@ -146,95 +261,88 @@ identifier = (lexeme . try) (p >>= check)
                 then fail $ "keyword " ++ show x ++ " cannot be an identifier"
                 else return x
 
-attribute :: Parser String
-attribute = (lexeme . try) (p >>= check)
-  where
-    p       = (:) <$> letterChar <*> many alphaNumChar
-    check x = if x `elem` attribs
-                then return x
-                else fail $ "keyword " ++ show x ++ " is not a valid attribute"
+-- Reserved words
+rword :: String -> Parser ()
+rword w = string w *> notFollowedBy alphaNumChar *> sc
 
-subshape :: Parser Expr
-subshape = (lexeme . try) (p >>= check)
-  where
-    p       = some letterChar
-    check x = if x `elem` shapes
-                then return (Shape $ getShape x)
-                else fail $ "Shape " ++ show x ++ " is not a valid shape"
+rws, attribs, attribVs, shapes, types :: [String] -- list of reserved words
+rws =     ["avoid", "global"]
+-- ++ types ++ attribs ++ shapes ++ colors
 
-color :: Parser Expr
-color = (lexeme . try) (p >>= check)
-  where
-    p       = some letterChar
-    check x = if x `elem` colors
-                then return (Color $ getColor x)
-                else fail $ "Color " ++ show x ++ " is not a valid color"
+types =   ["Set", "Map", "Point"]
+attribs = ["shape", "color", "label", "scale", "position"]
+attribVs = shapes ++ colors
+shapes =  ["None", "Circle", "Box", "SolidArrow", "SolidDot", "HollowDot", "Cross"]
+colors =  ["Random", "Black", "Red", "Blue", "Yellow"]
+--
+-- getType :: String -> SubType
+-- getType str = case str of
+--     "Set"   -> Set
+--     "Map"   -> Map
+--     "Point" -> Pt
+--     "Subset" -> Subset
+--
+--
+-- getAttribute :: String -> Attribute
+-- getAttribute str = case str of
+--     "shape" -> Shape
+--     "color" -> Color
+--     "bordor" -> LineType
+--
+--
+-- getAttributeV :: String -> AttributeV
+-- getAttributeV str = case str of
+--     "None"       -> ShapeV   NoShape
+--     "Circle"     -> ShapeV $ SS SetCircle
+--     "Box"        -> ShapeV $ SS Box
+--     "SolidArrow" -> ShapeV $ SM SolidArrow
+--     "SolidDot"   -> ShapeV $ SP SolidDot
+--     "HollowDot"  -> ShapeV $ SP HollowDot
+--     "Cross"      -> ShapeV $ SP Cross
+--     "Red"        -> ColorV Red
+--     "Yellow"     -> ColorV Yellow
+--     "Blue"       -> ColorV Blue
+--     "Black"      -> ColorV Black
+--     "Random"     -> ColorV Random
+--
+-- attribute :: Parser Attribute
+-- attribute = (lexeme . try) (p >>= check)
+--   where
+--     p       = many letterChar
+--     check x = if x `elem` attribs
+--                 then return (getAttribute x)
+--                 else fail $ "keyword " ++ show x ++ " cannot be an attribute"
+--
+-- attributeValue :: Parser AttributeV
+-- attributeValue = (lexeme . try) (p >>= check)
+--   where
+--     p       = many letterChar
+--     check x = if x `elem` attribVs
+--                 then return (getAttributeV x)
+--                 else fail $ "keyword " ++ show x ++ " cannot be an attribute value"
+--
+-- typ :: Parser SubType
+-- typ = (lexeme . try) (p >>= check)
+--   where
+--     p       = some letterChar
+--     check x = if x `elem` types
+--                 then return (getType x)
+--                 else fail $ "Type " ++ show x ++ " is not a valid type"
+--
+-- -- access :: Parser Expr
+-- -- access = do
+-- --     i <- identifier
+-- --     void (symbol ".")
+-- --     a <- attributeName
+-- --     return (Access i a)
+--
+--
+--
+--
 
-typ :: Parser SubType
-typ = (lexeme . try) (p >>= check)
-  where
-    p       = some letterChar
-    check x = if x `elem` types
-                then return (getType x)
-                else fail $ "Type " ++ show x ++ " is not a valid type"
-
-term :: Parser Expr
-term = expr
-  <|> subshape
-  <|> color
-  -- <|> integer
-  -- <|> float
-
-expr :: Parser Expr
-expr = makeExprParser term operators
-
-operators :: [[Operator Parser Expr]]
-operators = []
-
-assignStmt :: Parser Stmt
-assignStmt = do
-  var  <- attribute
-  void (symbol "=")
-  e    <- subshape <|> color
-  return (Assign var e)
-
-stmtSeq :: Parser [Stmt]
-stmtSeq = sepBy1 stmt comma
-
-stmt :: Parser Stmt
-stmt = assignStmt
-
-globalBlock :: Parser Block
-globalBlock = do
-    void (symbol "global")
-    stmts <- braces stmtSeq
-    return $ GlobalBlock stmts
-
-typeBlock :: Parser Block
-typeBlock = do
-    t     <- typ
-    stmts <- braces stmtSeq
-    return $ TypeBlock t stmts
-
-objBlock :: Parser Block
-objBlock = do
-    i     <- identifier
-    stmts <- braces stmtSeq
-    return $ ObjBlock i stmts
-
--- TODO: required global and/or style block or not?
--- TODO: How can I write something like noop???
--- NOTE: sequence matters here
-styProg :: Parser [Block]
-styProg = (some $ globalBlock <|> typeBlock <|> objBlock)
-        <|> emptyProg
-
-styleParser :: Parser [Block]
-styleParser = between sc eof styProg
-
--- TODO
-styPrettyPrint :: Block -> String
-styPrettyPrint b = "HAHAHAHA"
+-- -- TODO
+-- styPrettyPrint :: Block -> String
+-- styPrettyPrint b = "HAHAHAHA"
 
 parseFromFile p file = runParser p file <$> readFile file
 
