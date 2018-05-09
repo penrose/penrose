ca9a4d5ee3153cfbc52794f21f3034fffa6e04ca
dormaayn <dor.d.ma@gmail.com>
2018-05-09 14:19:04 -0400

    Implemented Parser, compile but pre testing (Issue #72)

--- src/Dsll.hs
+++ src/Dsll.hs
@@ -34,16 +34,19 @@ type Prop = String
 
 type Type = String
 
-data T = Constr String [A] 
-    | TypeVar
+type ConstructorInvoker = (String,[Arg])
+
+
+data T = TTypeVar TypeVar --TODO: figure out a better name for that...
+    | TConstr ConstructorInvoker
     deriving (Show, Eq, Typeable)
 
-data A = Ta T
-    | TVar Var
+data Arg = AVar Var
+    | AT T
     deriving (Show, Eq, Typeable)
 
-data K = Ktype Type
-    | Kt T
+data K = Ktype Type --TODO: figure out a better name for that...
+    | KT T
     deriving (Show, Eq, Typeable)
 
 data Cd = Constructor String [K] Type 
@@ -88,13 +91,25 @@ symbol = L.symbol sc
 newline' :: Parser ()
 newline' = newline >> scn
 
+-- Pill out the left and right parens from the input and roll-out the parsing
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+listOut :: Parser a -> Parser a
+listOut = between (symbol "[") (symbol "]")
+
+
+
 -- Define characters wich are part of the syntax
-colon, arrow, comma, rsbrac, lsbrac :: Parser ()
+colon, arrow, comma, rsbrac, lsbrac, lparen, rparen :: Parser ()
 colon  = void (symbol ":")
 comma  = void (symbol ",")
 arrow  = void (symbol "->")
 lsbrac = void (symbol "[")
 rsbrac = void (symbol "]")
+lparen = void (symbol "(")
+rparen = void (symbol ")")
+
 
 -- Reserverd word handling
 rword :: String -> Parser()
@@ -116,40 +131,136 @@ identifier = (lexeme . try) (p >>= check)
 -- --------------------------------------- DSLL Parser -------------------------------------
 
 
--- -- | 'DSLLParser' is the top-level parser function. The parser contains a list of functions
--- --    that parse small parts of the language. When parsing a source program, these functions are invoked in a top-down manner.
--- dsllParser :: Parser ([Cd], [Od], [Pd])
--- dsllParser = between scn eof dsllProg -- Parse all the statemnts between the spaces to the end of the input file
+-- | 'DSLLParser' is the top-level parser function. The parser contains a list of functions
+--    that parse small parts of the language. When parsing a source program, these functions are invoked in a top-down manner.
+dsllParser :: Parser ([Cd], [Od], [Pd])
+dsllParser = between scn eof dsllProg -- Parse all the statemnts between the spaces to the end of the input file
 
--- -- |'dsllProg' parses the entire actual DSLL program which is a collection of constructors followed by a collection of
--- --   operations followed by a collection of predicates
--- dsllProg :: Parser ([Cd],[Od],[Pd])
+-- |'dsllProg' parses the entire actual DSLL program which is a collection of constructors followed by a collection of
+--   operations followed by a collection of predicates
+dsllProg :: Parser ([Cd],[Od],[Pd])
+dsllProg = do
+    listCd <- cdParser `sepEndBy` newline'
+    listOd <- odParser `sepEndBy` newline'
+    listPd <- pdParser `sepEndBy` newline'
+    return (listCd, listOd, listPd)
 
 
--- cd = do
---   rword "constructor"
---   name <- identifier
---   colon
---   --k' <- parse that list somehow
---   arrow
---   -- t' <- parse the resulting type somehow
---   -- return (Cd name k' t')
+typeParser :: Parser Type
+typeParser = do
+    i <- identifier
+    return i
 
--- od = do
---   rword "operator"
---   name <- identifier
---   colon
---   -- check how to handle with the facts that parts of these might not appear
---   rword "forvars"
+varParser :: Parser Var
+varParser = do
+    i <- identifier
+    return i
 
--- pd = do
---   rword "predicate"
---   name <- identifier
---   -- Figure out how to continue
+typeVarParser :: Parser TypeVar
+typeVarParser = do
+    i <- identifier
+    return i
+
+propParser :: Parser Prop
+propParser = do
+    i <- identifier
+    return i
+
+
+tParser, tConstructorInvokerParser, typeVarParser' :: Parser T
+tParser = try tConstructorInvokerParser <|> typeVarParser'
+tConstructorInvokerParser = do
+    i         <- identifier
+    arguments <- parens  ( argParser `sepBy1` comma)
+    return (TConstr (i,arguments))
+typeVarParser' = do
+    i <- typeVarParser
+    return (TTypeVar i)
+
+
+
+argParser, varParser', tParser'  :: Parser Arg
+argParser = try varParser' <|> try tParser'
+varParser' = do
+     i <- varParser
+     return (AVar i)
+tParser' = do
+    t <- tParser
+    return (AT t)
+
+
+kParser, kTypeParser, tParser'' :: Parser K
+kParser = try kTypeParser <|> try tParser''
+kTypeParser = do
+     i <- typeParser
+     return (Ktype i)
+tParser'' = do
+    t <- tParser
+    return (KT t)
+
+cdParser :: Parser Cd
+cdParser = do
+    rword "constructor"
+    name <- identifier
+    colon
+    k' <- listOut( kParser `sepBy1` comma)
+    arrow
+    t' <- typeParser
+    return (Constructor name k' t')
+
+odParser :: Parser Od
+odParser = do
+  rword "operator"
+  name <- identifier
+  colon
+  rword "forvars"
+  varsList <- listOut (varParser `sepBy1` comma)
+  colon
+  tList <- listOut (tParser `sepBy1` comma)
+  comma
+  rword "fortypes"
+  typeVarList <- listOut (typeVarParser `sepBy1` comma)
+  colon
+  typeList <- listOut (typeParser `sepBy1` comma)
+  comma
+  outerTList <- listOut (tParser `sepBy1` comma)
+  arrow
+  t <- tParser
+  return (Operator name varsList tList typeVarList typeList outerTList t)
+
+
+pdParser, pd1, pd2 :: Parser Pd
+pdParser = try pd1 <|> pd2
+pd1 = do
+  rword "predicate"
+  name <- identifier
+  colon
+  propList <- listOut (propParser `sepBy1` comma)
+  arrow
+  prop <- propParser 
+  return (Predicate name [] [] [] [] [] propList prop)
+pd2 = do
+  rword "predicate"
+  name <- identifier
+  colon
+  rword "forvars"
+  varsList <- listOut (varParser `sepBy1` comma)
+  colon
+  tList <- listOut (tParser `sepBy1` comma)
+  comma
+  rword "fortypes"
+  typeVarList <- listOut (typeVarParser `sepBy1` comma)
+  colon
+  typeList <- listOut (typeParser `sepBy1` comma)
+  comma
+  outerTList <- listOut (tParser `sepBy1` comma)
+  arrow
+  prop <- propParser 
+  return (Predicate name varsList tList typeVarList typeList outerTList [] prop)
 
 -- --------------------------------------- Test Driver -------------------------------------
 -- | For testing: first uncomment the module definition to make this module the
- -- Main module. Usage: ghc Dsll.hs; ./Substance <substance-file>
+-- Main module. Usage: ghc Dsll.hs; ./Substance <substance-file>
 
 parseFromFile p file = runParser p file <$> readFile file
 
