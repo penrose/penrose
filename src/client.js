869d5a09838dce50b96717b2c56bcd45f0b8f527
wodeni <wn2155@columbia.edu>
2018-06-14 22:29:32 -0400

    [label-dsl] starting to refactor JS code

--- src/client.js
+++ src/client.js
@@ -1,29 +1,6 @@
-/*
- * Penrose web front-end main script
- * @author: Wode "Nimo" Ni
- * @version: 06/23/2017
- */
-
-var canvasWidth  = 800
-var canvasHeight = 700
-
  $.getScript('snap.svg.js', function()
  {
-    // Helper functions
 
-    function createSocket() {
-        return new WebSocket('ws://localhost:9160/');
-    }
-
-    // For (r,g,b) -> hex conversion
-    function componentToHex(c) {
-        var hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
-    }
-
-    function rgbToHex(r, g, b) {
-        return "#" + componentToHex(Math.round(255 * r)) + componentToHex(Math.round(255 * g)) + componentToHex(Math.round(255 * b));
-    }
 
     function renderPoints(canvas, point_list, dx, dy) {
         for(var i = 0; i < point_list.length; i++) {
@@ -43,18 +20,6 @@ var canvasHeight = 700
     }
 
 
-        /*
-     * Translate coordinates from polar to cartesian
-     * Adopted from: https://codepen.io/AnotherLinuxUser/pen/QEJmkN
-     * (Added by Dor)
-     */
-     function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-        var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
-        return {
-         x: centerX + (radius * Math.cos(angleInRadians)),
-         y: centerY + (radius * Math.sin(angleInRadians))
-     };
- }
 
     /*
      * Given center point, radius, and angels return an arc path
@@ -91,78 +56,7 @@ var canvasHeight = 700
     }
 
 
-    /*
-     * Translate a list of points ([[x1, y1], [x2, y2] ...]) to a
-     * Catmull-Rom Spline curve represented as an SVG path string
-     * Adopted from: https://codepen.io/osublake/pen/BowJed
-     */
-     function catmullRomSpline(list, k) {
-        // flatten the point list for simplicity
-        var data = [].concat.apply([], list);
-        if (k == null) k = 1;
-        var size = data.length;
-        var last = size - 4;
-        var path = "M" + [data[0], data[1]];
-        for (var i = 0; i < size - 2; i += 2) {
-            var x0 = i ? data[i - 2] : data[0];
-            var y0 = i ? data[i - 1] : data[1];
-            var x1 = data[i + 0];
-            var y1 = data[i + 1];
-            var x2 = data[i + 2];
-            var y2 = data[i + 3];
-            var x3 = i !== last ? data[i + 4] : x2;
-            var y3 = i !== last ? data[i + 5] : y2;
-            var cp1x = x1 + (x2 - x0) / 6 * k;
-            var cp1y = y1 + (y2 - y0) / 6 * k;
-            var cp2x = x2 - (x3 - x1) / 6 * k;
-            var cp2y = y2 - (y3 - y1) / 6 * k;
-            path += "C" + [cp1x, cp1y, cp2x, cp2y, x2, y2];
-        }
-        return path;
-    }
 
-    /*
-     * Translate a list of points ([[x1, y1], [x2, y2] ...]) to a standard
-     * SVG Path String.
-     * TODO: rigorous error handling. Should this be done at the frontend or backend?
-     */
-     function toPathString(orig_list, dx, dy) {
-        var str = "";
-        var chunk = 2;
-        var list = new Array(orig_list.length);
-
-        // transform all points to screen space
-        for(var i = 0; i < list.length; i++) {
-            list[i] = toScreen(orig_list[i], dx, dy);
-        }
-        // First point is the starting point
-        str += "M " + list[0][0] + " " + list[0][1] + " ";
-        // if we have only two points, simply draw a line
-        if(list.length == 2) {
-            str += "L " + list[1][0] + " " + list[1][1] + " ";
-            return str;
-        } else {
-            var res = catmullRomSpline(list, 1)
-            return res
-        }
-        // TODO: does not work for curve with 3 points
-        // NOTE: the following code treats points from Runtime as control
-        // points for a Cubic Bezier Curve, which will NOT pass through
-        // the control points
-
-        // Second through fourth points are for the first Bezier Curve
-        // str += "C " + list[1][0] + " " + list[1][1] + ", " +
-        //               list[2][0] + " " + list[2][1] + ", " +
-        //               list[3][0] + " " + list[3][1] + " ";
-        // for(var i = 4; i < list.length; i += chunk) {
-        //     points = list.slice(i, i + chunk);
-        //     str += "S "
-        //     for(var j = 0; j < points.length; j++) {
-        //         str += points[j][0] + " " + points[j][1] + ", ";
-        //     }
-        // }
-        // return str.substring(0, str.length - 2);
-    }
 
 
     /*
@@ -193,52 +87,9 @@ var canvasHeight = 700
        ].join(" ");
    }
 
-   function allToScreen(orig_list, dx, dy) {
-    var list = new Array(orig_list.length);
-
-        // transform all points to screen space
-        for(var i = 0; i < list.length; i++) {
-            list[i] = toScreen(orig_list[i], dx, dy);
-        }
-
-        return list;
-    }
-
-    function toScreen(xy, dx, dy) {
-        return [parseInt(dx + xy[0], 10), parseInt(dy - xy[1], 10)]
-    }
 
     // Main rendering function
     function renderScene(ws, s, data, firstrun) {
-        // Handlers for dragging events
-        var move = function(dx, dy) {
-            this.attr({
-                transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
-            });
-            // Increment distance traveled
-            this.data("ox", +dx );
-            this.data("oy", +dy );
-            // console.log(dx + " " + dy)
-        }
-
-        var start = function(dx, dy) {
-            this.data('origTransform', this.transform().local );
-            this.data("ox", 0);
-            this.data("oy", 0);
-            this.attr({opacity: 0.5});
-        }
-        var stop = function() {
-            // console.log('finished dragging');
-            // console.log('distance: ' + this.data("ox") + " " + this.data("oy"));
-            this.attr({opacity: 1});
-            var dict = { "tag" : "Drag",
-            "contents" : { "name" : this.data("name"),
-            "xm" : this.data("ox"),
-            "ym" : this.data("oy")} }
-            var json = JSON.stringify(dict)
-            // console.log(json)
-            ws.send(json)
-        }
         s.clear()
         // NOTE: just using clientWidth/Height does not work on Firefox
         // see https://stackoverflow.com/questions/13122790/how-to-get-svg-element-dimensions-in-firefox
@@ -360,14 +211,7 @@ var canvasHeight = 700
                 pt.drag(move, start, stop)
                 break
                 case 'C': // circle
-                var circ = s.circle(dx + obj.xc, dy - obj.yc, obj.r);
-                circ.data("name", obj.namec)
-                var color = obj.colorc
-                circ.attr({
-                    fill: rgbToHex(color.r, color.g, color.b),
-                    "fill-opacity": color.a,
-                });
-                circ.drag(move, start, stop)
+                    renderCircle(s, obj);
                 break
                 case 'E': // ellipse
                 var ellip = s.ellipse(dx + obj.xe, dy - obj.ye, obj.rx, obj.ry);
@@ -401,16 +245,16 @@ var canvasHeight = 700
                 var rotationar = obj.rotationar
 
                 if(isRightar == "true"){
-                        //Draw PrepMark (for right angle)                        
+                        //Draw PrepMark (for right angle)
 
                         var myMatrix = new Snap.Matrix();
                         var origX = dx + xar, origY = dy + yar
-                        
+
                         myMatrix.translate(origX, origY);
                         myMatrix.rotate(rotationar, 0, 0);
-                        
+
                         var path = "M " + 0 + " " + (-sizear)+ " L "
-                        + sizear + " " +  (-sizear) + " L " 
+                        + sizear + " " +  (-sizear) + " L "
                         + sizear + " " + yar
                         var p = s.path(path).transform(myMatrix.toTransformString())
 
@@ -425,11 +269,11 @@ var canvasHeight = 700
                             p.drag(move, start, stop)
 
                         } else if (style == "wedge") {
-                            
+
                             var rectPpath = "M " + 0 + " " + (-sizear)+ " L "
-                            + sizear + " " +  (-sizear) + " L " 
+                            + sizear + " " +  (-sizear) + " L "
                             + sizear + " " + 0 + " L " + 0 + " " + 0
-                            
+
                             var f = s.path(rectPpath).transform(myMatrix.toTransformString());
                             f.attr({
                                 fill: rgbToHex(0, 0, 205),
@@ -446,8 +290,8 @@ var canvasHeight = 700
                         var anglear = obj.anglear > 360.0 ? 360.0 - obj.anglear : obj.anglear
                         anglear = anglear < 0 ? 360 + anglear : anglear
                         var radiusar = obj.radiusar
-                        var arcPath = describeArc(dx + xar, dy + yar, radiusar, rotationar, 
-                            anglear < 0 ? (rotationar - anglear) : (rotationar + anglear));                          
+                        var arcPath = describeArc(dx + xar, dy + yar, radiusar, rotationar,
+                            anglear < 0 ? (rotationar - anglear) : (rotationar + anglear));
                         var arc = s.path(arcPath);
                         arc.attr({
                             "fill-opacity": 0,
@@ -458,8 +302,8 @@ var canvasHeight = 700
                             arc.data("name", obj.namear)
                             arc.drag(move, start, stop)
                         } else if (style == "wedge") {
-                            var pf = describeFullArc(dx + xar, dy + yar, radiusar, rotationar, 
-                                anglear < 0 ? (rotationar - anglear) : (rotationar + anglear));                          
+                            var pf = describeFullArc(dx + xar, dy + yar, radiusar, rotationar,
+                                anglear < 0 ? (rotationar - anglear) : (rotationar + anglear));
                             var f = s.path(pf);
                             f.attr({
                                 fill: rgbToHex(0, 0, 205),
@@ -496,12 +340,12 @@ var canvasHeight = 700
         var rotation = obj.rotationpa
         var path = ""
         if(angle <= 90.0){
-          path = "M " + (-(sizeX/2) - sizeY/getTanFromDegrees(angle)) + " " + (-(sizeY/2)) +  " L " + 
+          path = "M " + (-(sizeX/2) - sizeY/getTanFromDegrees(angle)) + " " + (-(sizeY/2)) +  " L " +
           (-(sizeX/2)) + " " + (sizeY/2) +  " L " +  (sizeX/2) + " " + (sizeY/2) +  " L " +
           ((sizeX/2) - (sizeY/getTanFromDegrees(angle))) + " " + (-(sizeY/2))
       }
       else{
-          path = "M " + (-(sizeX/2)) + " " + (sizeY/2) +  " L " + 
+          path = "M " + (-(sizeX/2)) + " " + (sizeY/2) +  " L " +
           (sizeX/2) + " " + (sizeY/2) +  " L " + ((sizeX/2) + sizeY/getTanFromDegrees(angle)) + " " + (-(sizeY/2))
           +  " L " + (-(sizeX/2) + (sizeY/getTanFromDegrees(angle))) + " " + (-(sizeY/2))
       }
@@ -535,7 +379,7 @@ var canvasHeight = 700
                 var color = obj.colorsa
                 if(style == "straight"){
                     var line = s.polygon(body_path).transform(myMatrix.toTransformString())
-                }                    
+                }
                 if(style == "curved"){
                     line = s.path(describeArc(len/2-2.4*t,0,len/2,-90,90)).transform(myMatrix.toTransformString())
                     line.attr({
@@ -544,7 +388,7 @@ var canvasHeight = 700
                         strokeWidth: 2
                     })
                 }
-                
+
                 if(style == "length"){
                     //document.write(toPathString([[0,(-(4*t))],[0,(4*t)]],0,0))
                     var p = "M " + 0 + " " + (-(4*t)) + " L " + 0 + " " + (4*t) +  " L " + 0 + " " + 0 + " L " + (len-(5*t)) + " " + 0;
@@ -582,61 +426,4 @@ var canvasHeight = 700
         }
     }
 
-    // Main function
-    $(document).ready(function () {
-        // var s = Snap(800, 700);
-        // TODO: set the width and height here?
-        var s = Snap("#svgdiv");
-        $("#svgdiv").css("width", canvasWidth);
-        $("#svgdiv").css("height", canvasHeight);
-
-        var firstRun = true
-        ws = createSocket();
-        // Only sample in 10 ms intervals, regardless of Penrose's sample speed
-        var sampleInterval = 20
-        var lastTime = new Date().getTime()
-        ws.onopen = function() {
-            // Register handlers for buttons on connection
-            $("#resample").click(function() {
-                var dict = { "tag" : "Cmd", "contents" : { "command" : "resample" } }
-                var json = JSON.stringify(dict)
-                ws.send(json)
-            });
-            $("#step").click(function() {
-                var dict = { "tag" : "Cmd", "contents" : { "command" : "step" } }
-                var json = JSON.stringify(dict)
-                ws.send(json)
-            });
-            $("#autostep").click(function() {
-                var $this = $(this);
-                $this.toggleClass('On');
-                if($this.hasClass('On')){
-                    $this.text('Disable Autostep');
-                } else {
-                    $this.text('Enable Autostep');
-                }
-                var dict = { "tag" : "Cmd", "contents" : {"command" : "autostep" } }
-                var json = JSON.stringify(dict)
-                ws.send(json)
-            });
-        };
-        ws.onmessage = function(event) {
-            //console.log(event.data)
-            var now  = new Date().getTime()
-            var diff = (now - lastTime);
-            var obj = jQuery.parseJSON(event.data)
-
-            // the server only sends `Frame` type data for the __last__ frame
-            if(obj.flag == "final") {
-                renderScene(ws, s, obj.objs, firstRun)
-            } else {
-                // if not the last frame, we refresh the frontend on a time interval
-                if(firstRun || diff > sampleInterval) {
-                    renderScene(ws, s, obj, firstRun)
-                    lastTime = now
-                    firstRun = false
-                }
-            }
-        }
-    });
 });
