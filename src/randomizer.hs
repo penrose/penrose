3282afaccc761a53d98258982e26425f6ea133a3
dormaayn <dor.d.ma@gmail.com>
2018-07-05 15:41:35 -0400

    More progress issue #107

--- src/randomizer.hs
+++ src/randomizer.hs
@@ -1,4 +1,4 @@
--- Get as an input the a DSLL, and the number of programs it should produce
+-- Automatic random generator for Substance programs
 -- Author: Dor Ma'ayan
 -- Version: 07/03/2018
 
@@ -13,42 +13,85 @@ import Env
 
 import qualified Dsll as D
 import qualified Namegen as NG
+import qualified Data.Map.Strict as M
+
+
+-----------------------------  Generator Env -----------------------------------
+-- The goal of the local env is to track the
+newtype LocalEnv = LocalEnv {
+                       declaredTypes    :: M.Map String [String] -- Map from type to list of object from that type
+                     }
+              deriving (Show, Eq)
+
+-- | Add declared type statement to the local env of the generator
+addDeclaredType :: LocalEnv -> String -> String -> LocalEnv
+addDeclaredType localEnv t typeName = case M.lookup t (declaredTypes localEnv) of
+  Nothing -> localEnv { declaredTypes =  M.insert t [typeName] (declaredTypes localEnv) }
+  Just v -> localEnv { declaredTypes = M.insert t (typeName : v) (declaredTypes localEnv) }
+
+
+
+-- | The main function of the genrator.
+--   arguments: Dsll file, name file
+--   parse the input Dsll file, preform type checking and get the environment
+--   load the names from the names file
+
+------------------------------ Substance Generator -----------------------------
 
 main = do
     args <- getArgs
-    when (length args /= 1) $ die "Usage: ./Main prog.dsl"
-    let dsllFile = head args
+    when (length args /= 2) $ die "Usage: ./Main prog.dsl namesFile"
+    let (dsllFile, namesFile) = (head args,args !! 1)
     dsllIn <- readFile dsllFile
     dsllEnv <- D.parseDsll dsllFile dsllIn
-    generateProgram dsllEnv
+    samples <- NG.loadSamples namesFile
+    let language = NG.fromSamples samples
+    generateProgram dsllEnv language
     return ()
 
-generateProgram dsllEnv = do
-    putStrLn "\n hi we here let's generate: \n"
-    generateTypes dsllEnv
-    generateOperations dsllEnv
-    generatePredicates dsllEnv
 
+generateProgram dsllEnv language = do
+    let prog = ""
+        (prog1, localEnv1) = generateTypes dsllEnv initLocalEnv language prog 10 --(unsafePerformIO $ getStdRandom (randomR (1,6)))
+        (prog2, localEnv2) = generateOperations dsllEnv localEnv1 language prog1 3 --(unsafePerformIO $ getStdRandom (randomR (1,6)))
+        (prog3, localEnv3) = generatePredicates dsllEnv localEnv2 language prog2
+    putStrLn prog3
+    putStrLn "\n And the local env is: \n"
+    putStrLn (show localEnv3)
     return ()
-
-generateTypes dsllEnv = do
-     samples <- NG.loadSamples "names"
-     let language = NG.fromSamples samples
-         myGeneratedName = NG.generateName language (unsafePerformIO $ getStdRandom (randomR (1,1000))) -- seed could be ontained from a Random Generator
-     putStrLn myGeneratedName
-     return ()
-
---putStrLn "\n ...generateTypes... \n"
-
-generateOperations dsllEnv = do
-     samples <- NG.loadSamples "names"
-     let language = NG.fromSamples samples
-         myGeneratedName = NG.generateName language (unsafePerformIO $ getStdRandom (randomR (1,1000))) -- seed could be ontained from a Random Generator
-     putStrLn myGeneratedName
-     return ()
-
-generatePredicates dsllEnv = putStrLn "\n ...generatePredicates... \n"
+    where initLocalEnv = LocalEnv {declaredTypes = M.empty}
 
 
 
---seed <- randomIO :: IO Int -- Currently randomizes the seed to...
+-- | Generate random type statements
+generateTypes dsllEnv localEnv language prog n =
+    let (nProg , nLocalEnv) = generateType dsllEnv localEnv language
+        prog1 = prog ++ nProg
+    in if n /= 1 then generateTypes dsllEnv nLocalEnv language prog1 (n - 1)
+                   else (prog1, nLocalEnv)
+
+generateType dsllEnv localEnv language =
+    let types = M.toAscList (typeConstructors dsllEnv)
+        t = (unsafePerformIO $ getStdRandom (randomR (0,length types-1)))
+        name = NG.generateName language (unsafePerformIO $ getStdRandom (randomR (1,1000)))
+        tName = fst (types !! t)
+        nLocalEnv = addDeclaredType localEnv tName name
+    in  (tName ++ " " ++ name ++ " \n" , nLocalEnv)
+
+
+-- | Generate random operation statements
+generateOperations dsllEnv localEnv language prog n =
+    let prog1 = prog ++ generateOperation dsllEnv language
+    in if n /= 1 then generateOperations dsllEnv localEnv language prog1 (n - 1)
+                   else (prog1, localEnv)
+
+generateOperation dsllEnv language =
+    let operations = M.toAscList (operators dsllEnv)
+        o = (unsafePerformIO $ getStdRandom (randomR (0,length operations -1)))
+        operation = snd (operations !! o)
+        name = NG.generateName language (unsafePerformIO $ getStdRandom (randomR (1,1000)))
+    in nameop operation ++ "(" ++ show (tlsop operation) ++ ")\n"
+
+
+-- | Generate random predicate statements
+generatePredicates dsllEnv localEnv language prog = (prog,localEnv)
