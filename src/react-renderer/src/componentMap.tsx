36ce8943224495909fd43604caefd792f7b7a60a
Max Krieger <git+themaxaxis@gmail.com>
2018-10-26 15:28:42 -0400

    [end-user] rename folder

--- src/react-renderer/src/componentMap.tsx
+++ src/react-renderer/src/componentMap.tsx
@@ -2,6 +2,7 @@ import Circle from "./Circle";
 import Label from "./Label";
 import Rectangle from "./Rectangle";
 // Map between "tag" and corresponding component
+// TODO: turn off prettifier for this
 const componentMap = {
   Circle,
   Rectangle,
