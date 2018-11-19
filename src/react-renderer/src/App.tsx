e5231b2949b35ddeff8d18eb10f5c5b86310a284
Max Krieger <git+themaxaxis@gmail.com>
2018-11-19 16:32:49 -0500

    [end-user] #141 buggily prerendering labels

--- src/react-renderer/src/App.tsx
+++ src/react-renderer/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Canvas from "./Canvas";
-import { hydrated } from "./Util";
+import { collectLabels } from "./Util";
 import Log from "./Log";
 import { LockContext } from "./contexts";
 
@@ -24,7 +24,7 @@ class App extends React.Component<IProps, IState> {
       this.canvas.current.download();
     }
   };
-  public onMessage = (e: MessageEvent) => {
+  public onMessage = async (e: MessageEvent) => {
     let myJSON = JSON.parse(e.data);
     // For final frame
     if (myJSON.flag !== null && myJSON.flag === "final") {
@@ -32,12 +32,14 @@ class App extends React.Component<IProps, IState> {
       this.setState({ converged: true });
       Log.info("Fully optimized.");
     }
-    this.setState({ data: myJSON });
+    // TODO: only if initial build
+    const results = await collectLabels(myJSON);
+    this.sendUpdate(results);
   };
   public step = () => {
     const packet = { tag: "Cmd", contents: { command: "step" } };
     this.ws.send(JSON.stringify(packet));
-  }
+  };
   public resample = () => {
     const packet = { tag: "Cmd", contents: { command: "resample" } };
     this.setState({ converged: !this.state.autostep });
@@ -58,18 +60,21 @@ class App extends React.Component<IProps, IState> {
       }
       return [name, oldShape];
     });
-    this.setState({
-      data: shapes
-    });
-    if (hydrated(shapes)) {
-      const packet = {
-        tag: "Update",
-        contents: { shapes }
-      };
-      const packetString = JSON.stringify(packet);
-      Log.info("Sending an Update packet to the server...");
-      this.ws.send(packetString);
-    }
+    this.sendUpdate(shapes);
+  };
+  public sendUpdate = (updatedShapes: any[]) => {
+    const packet = {
+      tag: "Update",
+      contents: {
+        shapes: updatedShapes.map(([name, obj]: [string, any]) => {
+          return [name, { ...obj, rendered: undefined }];
+        })
+      }
+    };
+    const packetString = JSON.stringify(packet);
+    Log.info("Sending an Update packet to the server...");
+    this.ws.send(packetString);
+    this.setState({ data: updatedShapes });
   };
   public dragEvent = (id: string, dy: number, dx: number) => {
     // TODO: save intermediate state so no snapback
