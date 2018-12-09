5363b0e008493ae8fc046c7c4167fd78dd76da53
Max Krieger <git+themaxaxis@gmail.com>
2018-12-08 20:26:26 -0500

    [end-user] unregistered service worker

--- src/ide-frontend/src/index.tsx
+++ src/ide-frontend/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "@reach/menu-button/styles.css";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
 
@@ -11,4 +11,4 @@ if (process.env.NODE_ENV !== "production") {
   localStorage.setItem("debug", "ide:*,renderer:*");
 }
 
-registerServiceWorker();
+// registerServiceWorker();
