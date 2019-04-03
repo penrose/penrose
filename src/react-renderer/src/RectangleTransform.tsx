f92017c2947c59f0440e089a9f0ffa40d27f61ee
katherineye <kye@princeton.edu>
2019-04-03 13:36:53 -0400

    [transform] change frontend transforms to apply Penrose transform, then screen space transform, so we can properly rotate about a shape's center

--- src/react-renderer/src/RectangleTransform.tsx
+++ src/react-renderer/src/RectangleTransform.tsx
@@ -1,52 +1,64 @@
 import * as React from "react";
-import { toScreen, toHex } from "./Util";
+import { toHex } from "./Util";
 import draggable from "./Draggable";
 import { IGPIPropsDraggable } from "./types";
 
+const penroseToSVG = (canvasSize: [number, number]) => {
+    const [width, height] = canvasSize;
+    const flipYStr = "matrix(1 0 0 -1 0 0)";
+    const translateStr = "translate(" + width / 2 + ", " + height / 2 + ")";
+    // Flip Y direction, then translate shape to origin mid-canvas
+    return [translateStr, flipYStr].join(" "); 
+};
+
 class RectangleTransform extends React.Component<IGPIPropsDraggable> {
-  public render() {
-    const { shape } = this.props;
-    const { canvasSize } = this.props;
-    const { onClick } = this.props;
+    public render() {
+	const { shape } = this.props;
+	const { canvasSize } = this.props;
+	const { onClick } = this.props;
 
-    // TODO: write toScreen as a transform that is applied before or after the H transform matrix
-    const [x, y] = toScreen([shape.x.contents, shape.y.contents], canvasSize);
-    const fillColor = toHex(shape.color.contents);
-    const fillAlpha = shape.color.contents[3];
-    const strokeColor = toHex(shape.strokeColor.contents);
-    const strokeAlpha = shape.strokeColor.contents[3];
-    const thickness = shape.strokeWidth.contents;
+	const [x, y] = [shape.x.contents, shape.y.contents];
+	const fillColor = toHex(shape.color.contents);
+	const fillAlpha = shape.color.contents[3];
+	const strokeColor = toHex(shape.strokeColor.contents);
+	const strokeAlpha = shape.strokeColor.contents[3];
+	const thickness = shape.strokeWidth.contents;
 
-      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform
-      // TODO: check that the transform respects screen space, SVG directionality, and the Style writer's intent
-      // Right now increasing the y will move downward. Does that affect the skew factors?
-      const tf = shape.transform.contents;
-      console.log("transform", tf);
-      const transformList = [tf.x_scale, tf.yx_fac, tf.xy_fac,
-			      tf.y_scale, tf.dx, tf.dy];
-      const transformStr = "matrix(" + transformList.join(" ") + ")";
+	// https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform
+	// TODO: check that the transform respects screen space, SVG directionality, and the Style writer's intention
+	// Right now increasing the y will move downward. Does that affect the skew factors?
 
-    return (
-      <rect
-        x={x - shape.sizeX.contents / 2}
-        y={y - shape.sizeY.contents / 2}
-        width={shape.sizeX.contents}
-        height={shape.sizeY.contents}
-        fill={fillColor}
-        fillOpacity={fillAlpha}
+	const tf = shape.transform.contents;
+	console.log("transform", tf);
+	const transformList = [tf.x_scale, tf.yx_fac, tf.xy_fac,
+			       tf.y_scale, tf.dx, tf.dy];
+	const penroseTransform = "matrix(" + transformList.join(" ") + ")";
 
-        stroke={strokeColor}
-        strokeOpacity={strokeAlpha}
-        strokeDasharray={ shape.strokeStyle.contents === "dashed" ? "7, 5" : "" }
-        strokeWidth={thickness}
+	// Do Penrose transform, then SVG
+	const transformStr = [penroseToSVG(canvasSize), penroseTransform].join(" ");
+	console.log("transformStr", transformStr);
 
-	transform={transformStr}
+	return (
+		<rect
+            x={x - shape.sizeX.contents / 2}
+            y={y - shape.sizeY.contents / 2}
+            width={shape.sizeX.contents}
+            height={shape.sizeY.contents}
+            fill={fillColor}
+            fillOpacity={fillAlpha}
 
-        onMouseDown={onClick}
-      >
-        <title>{shape.name.contents}</title>
-      </rect>
-    );
-  }
+            stroke={strokeColor}
+            strokeOpacity={strokeAlpha}
+            strokeDasharray={ shape.strokeStyle.contents === "dashed" ? "7, 5" : "" }
+            strokeWidth={thickness}
+
+	    transform={transformStr}
+
+            onMouseDown={onClick}
+		>
+		<title>{shape.name.contents}</title>
+		</rect>
+	);
+    }
 }
 export default draggable(RectangleTransform);
