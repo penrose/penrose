344b47655824da50cbc2f9a8ae9208d8528ed9dc
katherineye <kye@princeton.edu>
2019-04-04 17:16:55 -0400

    [transform] start adding transform tests

--- test/Functions/Tests.hs
+++ test/Functions/Tests.hs
@@ -14,36 +14,31 @@ tests = testGroup "Functions tests" [properties, unitTests]
 properties :: TestTree
 properties = testGroup "Properties" [scProps, qcProps]
 
+--------
+
+id_compose_id :: Assertion
+id_compose_id = composeTransform idH idH @?= idH
+
+-- TODO: sample random matrix
+-- then test A * id = id * A = A
+
+transformsGroup :: TestTree
+transformsGroup = testGroup "Transforms"
+                [ testCase "idH * idH = idH" id_compose_id
+                ]
+
+--------
+
 scProps = testGroup "(checked by SmallCheck)" 
           [ 
           ]
 
-{-
--- These tests could be more useful. Just an example.
-sameSize_idCirc :: Circ' a -> Circ' a -> Bool
-sameSize_idCirc c1 c2 = True
-
-sameSize_idCirc_unit :: Assertion
-sameSize_idCirc_unit = (sameSize [defcirc, defcirc] [] == 0) @?= True
-                       where defcirc = addGrad $ defaultCirc "c"
-
--- This should really be on the gradient of repel evaluated at 0
-repel_CS_unit_NaN :: Assertion
-repel_CS_unit_NaN = (isNaN $ repel [defcirc, defsquare] []) @?= False
-                       where defcirc = addGrad $ defaultCirc "c"
-                             defsquare = addGrad $ defaultCirc "s"
--}
-
--- Perhaps "for any two randomly initialized objects, an objective function that can be applied to them
--- does not result in NaN"? TODO derive Arbitrary
--- TODO test that repel never/seldom gives NaNs (given that its derivative has a denominator)
 qcProps = testGroup "(checked by QuickCheck)" 
-          [ -- QC.testProperty "constraint function: same size identity" sameSize_idCirc
-            
+          [ 
           ]
 
 -- Module: topic: function: property
+unitTests :: TestTree
 unitTests = testGroup "Unit tests" 
-          [ -- testCase "constraint function: same size identity" sameSize_idCirc_unit,
-            -- testCase "objective function: repel no NaN" sameSize_idCirc_unit
+          [ transformsGroup
           ]
