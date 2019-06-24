4e168222b76d0ed102642cf99d7b36ddc39dd73b
maxkrieger <themaxaxis@gmail.com>
2019-06-24 12:36:12 -0400

    [interface] Accept blank data objects for processData

--- src/react-renderer/src/Canvas.tsx
+++ src/react-renderer/src/Canvas.tsx
@@ -120,6 +120,7 @@ class Canvas extends React.Component<IProps> {
     };
   };
   public static processData = async (data: any) => {
+    if (!data.shapesr) return {};
     const shapes = data.shapesr;
     const labeledShapes = await collectLabels(shapes);
     const labeledShapesWithImgs = await loadImages(labeledShapes);
