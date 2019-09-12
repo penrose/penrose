008b7a5582d03d919a04f0e7ba73b940ba97951d
maxkrieger <themaxaxis@gmail.com>
2019-09-11 22:16:19 -0400

    [small-renderer-changes] Export Protocol, etc in module.tsx as well

--- react-renderer/src/module.tsx
+++ react-renderer/src/module.tsx
@@ -1,5 +1,19 @@
 import Canvas from "./Canvas";
 import * as Packets from "./packets";
-import {ILayer} from "./types";
+import { ILayer } from "./types";
+import {
+  Protocol,
+  IRendererEvents,
+  ConnectionStatus,
+  IEditorEvents
+} from "src/Protocol";
 
-export {Canvas, Packets, ILayer};
+export {
+  Canvas,
+  Packets,
+  ILayer,
+  Protocol,
+  IRendererEvents,
+  ConnectionStatus,
+  IEditorEvents
+};
