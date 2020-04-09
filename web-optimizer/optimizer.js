22083ac06bbe67d4a765484e737df1e3943b8404
katherineye <katherine.ye@gmail.com>
2020-04-09 13:58:58 -0400

    [web-opt] make optimizer more generic with a layer that performs fn and arg lookup

--- web-optimizer/optimizer.js
+++ web-optimizer/optimizer.js
@@ -9,7 +9,7 @@
 // Figure out the speed hit for conversion
 // Benchmark tfjs solver (speed/perf) vs. Penrose solver
 
-// TODO: Scope the goal for tomorrow. 
+// TODO: Scope the goal
 // Port objectives/constraints for just the one example and write them in terms of the tfjs syntax, then optimize and lay out diagram
 // Port evalEnergyOn function from GenOptProblem
 // Port Optimizer module
@@ -35,13 +35,11 @@
 // const optimizer2 = tf.train.adam(learningRate);
 
 const evalEnergyOn = (tr, varying) => {
-
     console.log("TODO: evalEnergyOn");
-
 };
 
-// Optimize f over x
-const optimizeTr = (tr, f, x) => {
+// Minimize f over x
+const minimize = (f, x) => {
     let res;
 
     // Use included tf.js optimizer for now
@@ -53,17 +51,49 @@ const optimizeTr = (tr, f, x) => {
     return res;
 };
 
-const v0 = tf.scalar(Math.random()).variable();
-const v1 = tf.scalar(Math.random()).variable();
+// UTILS
+
+// TODO: Figure out how to schedule computations as tensors?
+// objFn :: [a] -> a
+// TODO: Make the input size and type richer
+let objFn_library = { 
+    min2 : (x, y) => x.square().add(y.square()), // x^2 + y^2
+    equal2 : (x, y) => x.sub(y).square() // (x - y)^2
+};
+
+// TODO: Make this more generic (multiple fns and args) and test the compilation behaves correctly programmatically
+// (With test cases)
+let objFns_input = [ { fnName: "min2",
+		       args: ["A.shape.x", "B.shape.y"] }
+		   ];
+
+// Compile the opt problem in `objFns_input` (which uses names) into a multivariate function on floats, and look up the initial state
+let compileOptProblem = (tr, objFns_input, varyingMap) => {
+    // Look up function name in function library
+    let overallFn = objFn_library[objFns_input[0].fnName];
+    // Look up each argument's varying value in varyingMap (currently a TF variable, TODO make this an expr?)
+    let argVals = objFns_input[0].args.map(argName => varyingMap[argName]);
+
+    return { F: overallFn,
+	     x: argVals };
+};
+
+let varyingMap =
+    { "A.shape.x" : tf.scalar(Math.random()).variable(),
+      "B.shape.y" : tf.scalar(Math.random()).variable()
+    };
 
 let tr = {}; // TODO: fill in
-let F = (x, y) => x.square().add(y.square()); // TODO: make generic
-// TODO: Write a realistic `F` and use `x` realistically (and standardize names)
-let varying = [v0, v1];
+let varyingVals = Object.values(varyingMap); // TODO standardize order of values (alphabetical by varyingMap order? use a list?)
+
+// Compile out all names/references? F : [a] -> a, X0 : [a]
+let pure_opt_problem = compileOptProblem(tr, objFns_input, varyingMap)
+console.log(pure_opt_problem);
 
 // TODO: Compose `f` from smaller functions
-let lastEnergy = optimizeTr(tr, F, varying);
+let lastEnergy = minimize(pure_opt_problem.F, pure_opt_problem.x);
 
 // TODO: do generically
-console.log(`v0: ${v0.dataSync()}, v1: ${v1.dataSync()}`);
+// For some reason it comes out as a list of single-elem-list of Float32Array
+console.log("varyingMap", Object.values(varyingMap).map(v => v.dataSync()[0]));
 console.log(`F[v]: ${lastEnergy}`);
