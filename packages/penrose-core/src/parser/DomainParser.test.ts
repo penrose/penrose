89fec9c64a029b1ddc7827a60e45f05a03fdceb2
wodeni <wn2155@columbia.edu>
2021-02-07 18:13:44 -0500

    chore: turn off file output in test suite

--- packages/penrose-core/src/parser/DomainParser.test.ts
+++ packages/penrose-core/src/parser/DomainParser.test.ts
@@ -5,7 +5,7 @@ import * as fs from "fs";
 import { result } from "lodash";
 
 const outputDir = "/tmp/asts";
-const saveASTs = true;
+const saveASTs = false;
 
 let parser: nearley.Parser;
 const sameASTs = (results: any[]) => {
