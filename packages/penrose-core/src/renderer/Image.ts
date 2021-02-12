e1c3e69e9e4bb78d4f548598ef25fcec9d98b9ec
maxkrieger <maxkrieger@users.noreply.github.com>
2021-02-11 23:54:29 -0500

    [vanilla-renderer] Update watch scripts

--- packages/penrose-core/src/renderer/Image.ts
+++ packages/penrose-core/src/renderer/Image.ts
@@ -1,14 +1,18 @@
-import { attrOpacity, attrWH, attrXY } from "./AttrHelper";
+import { attrOpacity, attrTransformCoords, attrWH } from "./AttrHelper";
 import { ShapeProps } from "./Renderer";
+import images from "contrib/images.json";
 
-const Image = ({ shape, canvasSize }: ShapeProps) => {
-  const elem = document.createElementNS("http://www.w3.org/2000/svg", "image");
+const Image = ({ shape, canvasSize }: ShapeProps): SVGGElement => {
+  const elem = document.createElementNS("http://www.w3.org/2000/svg", "g");
   const path = (shape.properties.path as IStrV<string>).contents;
-  // TODO: will this work?
-  elem.setAttribute("href", path);
+  if (!(path in images)) {
+    console.error(`Could not find image path ${path}`);
+    return elem;
+  }
+  elem.innerHTML = images[path];
   attrOpacity(shape, elem);
-  attrXY(shape, canvasSize, elem);
-  attrWH(shape, elem);
+  attrWH(shape, elem.firstChild as SVGElement);
+  attrTransformCoords(shape, canvasSize, elem);
   return elem;
 };
 export default Image;
