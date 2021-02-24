7386c10e6dba49172db889fe04eba2983fedcfae
Max Krieger <maxkrieger@users.noreply.github.com>
2021-02-24 16:45:37 -0500

    fix: newlines in errors (wrap now) and newlines in mathjax (#493)
    * fix: newlines in errors (wrap now) and newlines in mathjax
    
    * fix: pointer events bounding box

--- packages/browser-ui/src/inspector/views/viewMap.tsx
+++ packages/browser-ui/src/inspector/views/viewMap.tsx
@@ -1,7 +1,7 @@
 import Frames from "./Frames";
 import ShapeView from "./ShapeView";
 import Mod from "./Mod";
-import Errors from "inspector/Errors";
+import Errors from "inspector/views/Errors";
 const viewMap = {
   frames: Frames,
   errors: Errors,
