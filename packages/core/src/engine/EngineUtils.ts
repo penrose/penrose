3ade4de0a8d47b44ce9cdee0e5b9e0f1abd3e961
wodeni <wn2155@columbia.edu>
2021-02-24 16:55:24 -0500

    fix: skip static check for access paths

--- packages/core/src/engine/EngineUtils.ts
+++ packages/core/src/engine/EngineUtils.ts
@@ -858,7 +858,8 @@ export const findExpr = (
           // COMBAK: This deals with accessing elements of path aliases. Maybe there is a nicer way to do it.
           return findExpr(trans, { ...path, path: res2 });
         } else {
-          return { tag: "InvalidAccessPathError", path };
+          // NOTE: we cannot check because we cannot evaluate right now. Returning the result directly instead
+          return res;
         }
       } else if (res.tag === "Done") {
         if (res.contents.tag === "VectorV") {
