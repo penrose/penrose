f8da9f42ca65b5c38baf50c879a75bbdc7676a7c
wodeni <wn2155@columbia.edu>
2021-03-03 15:50:40 -0500

    fix: compute label dimensions without DOMParser

--- packages/core/src/__tests__/overall.test.ts
+++ packages/core/src/__tests__/overall.test.ts
@@ -11,17 +11,17 @@ import {
 describe("Render", () => {
   const triple = [
     "../../examples/set-theory-domain/setTheory.dsl",
-    "../../examples/set-theory-domain/twosets-simple.sub",
-    "../../examples/set-theory-domain/venn.sty",
+    "../../examples/set-theory-domain/tree.sub",
+    "../../examples/set-theory-domain/venn-3d.sty",
   ].map((p) => fs.readFileSync(p).toString()) as [string, string, string];
   test("Diagram", async () => {
     const res = compileTrio(...triple);
     if (res.isOk()) {
-      // const state = await prepareState(res.value);
-      // const optimized = stepUntilConvergence(state);
-      // const rendered = RenderStatic(optimized);
-      // console.log(rendered.outerHTML);
-      // fs.writeFileSync("/tmp/output.svg", rendered.outerHTML, "utf8");
+      const state = await prepareState(res.value);
+      const optimized = stepUntilConvergence(state);
+      const rendered = RenderStatic(optimized);
+      console.log(rendered.outerHTML);
+      fs.writeFileSync("/tmp/output.svg", rendered.outerHTML, "utf8");
     } else {
       fail(res.error);
     }
--- packages/core/src/utils/CollectLabels.ts
+++ packages/core/src/utils/CollectLabels.ts
@@ -37,9 +37,9 @@ const convert = (input: string, fontSize: string) => {
   // https://github.com/mathjax/MathJax-src/blob/master/ts/adaptors/liteAdaptor.ts#L523
   adaptor.setStyle(node, "font-size", fontSize);
   const inner = adaptor.innerHTML(node);
-  const doc = new window.DOMParser().parseFromString(inner, "text/html").body
-    .firstChild as any;
-  return doc as SVGSVGElement;
+  // console.log(inner, node.firstChild);
+  // const doc = new DOMParser().parseFromString(inner, "text/html").body .firstChild as any;
+  return node.firstChild as SVGSVGElement;
 };
 
 /**
@@ -57,7 +57,11 @@ const tex2svg = memoize(
           resolve({ output: undefined, width: 0, height: 0 });
           return;
         }
-        const { width, height } = output.viewBox.baseVal;
+        // console.log(output);
+        // console.log(output.viewBox.baseVal);
+        const viewBox = output.getAttribute("viewBox")!.split(" ");
+        const width = parseFloat(viewBox[2]);
+        const height = parseFloat(viewBox[3]);
 
         // rescaling according to
         // https://github.com/mathjax/MathJax-src/blob/32213009962a887e262d9930adcfb468da4967ce/ts/output/svg.ts#L248
