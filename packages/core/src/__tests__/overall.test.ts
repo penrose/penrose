7c168ac52be86750b6a8d5706530f765c90120df
wodeni <wn2155@columbia.edu>
2021-03-05 15:58:43 -0500

    test: add first cross-energy-eval test

--- packages/core/src/__tests__/overall.test.ts
+++ packages/core/src/__tests__/overall.test.ts
@@ -54,55 +54,62 @@ describe("End-to-end testing of existing diagrams", () => {
 });
 
 describe("Energy API", () => {
-  test("eval overall energy - init", async () => {
+  test("eval overall energy - init vs. optimized", async () => {
     const twoSubsets = `Set A, B\nIsSubset(B, A)\nAutoLabel All`;
     const res = compileTrio(setDomain, twoSubsets, vennStyle);
     if (res.isOk()) {
       const stateEvaled = await prepareState(res.value);
       const stateOptimized = stepUntilConvergence(stateEvaled);
-      console.log(evalEnergy(stateEvaled));
-      console.log(evalEnergy(stateOptimized));
+      const initEng = evalEnergy(stateEvaled);
+      const optedEng = evalEnergy(stateOptimized);
+      expect(initEng).toBeGreaterThan(optedEng);
+    } else {
+      console.log(showError(res.error));
+    }
+  });
+  test("filtered constraints", async () => {
+    const twoSubsets = `Set A, B\nIsSubset(B, A)\nAutoLabel All`;
+    const res = compileTrio(setDomain, twoSubsets, vennStyle);
+    if (res.isOk()) {
+      // NOTE: delibrately not cache the overall objective and re-generate for original and filtered states
+      const state = res.value;
+      const smallerThanFns = state.constrFns.filter(
+        (c) => c.fname === "smallerThan"
+      );
+      const stateFiltered = { ...state, constrFns: smallerThanFns };
+      expect(evalEnergy(state)).toBeGreaterThan(evalEnergy(stateFiltered));
     } else {
       console.log(showError(res.error));
     }
   });
 });
 
-// describe("Energy API", () => {
-//   test("filter constraints", () => {
-//     const twoSubsets = `Set A, B\nIsSubset(B, A)\nAutoLabel All`;
-//     const res = compileTrio(setDomain, twoSubsets, vennStyle);
-//     if (res.isOk()) {
-//       const state = res.value;
-//       const containFns = state.constrFns.filter((c) => c.fname === "contains");
-//       const xs = state.varyingValues;
-
-//       console.log(e(xs));
-
-//       console.log(containFns);
-//     } else {
-//       console.log(showError(res.error));
-//     }
-//   });
-// });
-
 describe("Cross-instance energy eval", () => {
   test("correct - subsets", async () => {
     const twosets = `Set A, B\nAutoLabel All`;
     const twoSubsets = `Set A, B\nIsSubset(B, A)\nAutoLabel All`;
+    // compile and optimize both states
     const state1 = compileTrio(setDomain, twosets, vennStyle);
     const state2 = compileTrio(setDomain, twoSubsets, vennStyle);
-    // if (res.isOk()) {
-    //   const state = await prepareState(res.value);
-    //   const optimized = stepUntilConvergence(state);
-    //   const rendered = RenderStatic(optimized);
-    //   fs.writeFileSync(
-    //     path.join(OUTPUT, `${name}.svg`),
-    //     rendered.outerHTML,
-    //     "utf8"
-    //   );
-    // } else {
-    //   fail(res.error);
-    // }
+    if (state1.isOk() && state2.isOk()) {
+      const state1Done = stepUntilConvergence(await prepareState(state1.value));
+      const state2Done = stepUntilConvergence(await prepareState(state2.value));
+      const crossState21 = {
+        ...state2Done,
+        constrFns: state1Done.constrFns,
+        objFns: state1Done.objFns,
+      };
+      expect(evalEnergy(await prepareState(crossState21))).toBeCloseTo(0);
+      const crossState12 = {
+        ...state1Done,
+        constrFns: state2Done.constrFns,
+        objFns: state2Done.objFns,
+      };
+      expect(evalEnergy(await prepareState(crossState12))).toBeGreaterThan(0);
+      // console.log(RenderStatic(crossState12).outerHTML);
+      // console.log(RenderStatic(crossState21).outerHTML);
+    } else {
+      fail("compilation failed");
+    }
   });
 });
