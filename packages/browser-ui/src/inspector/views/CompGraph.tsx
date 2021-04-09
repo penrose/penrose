35a40424cd845631969a0fe6f258e191b1a01c42
katherineye <kye@princeton.edu>
2021-04-09 13:32:27 -0400

    [opt-vis] attempt to add cytoscape, which renders (with pan) but doesn't update with layout--react problem?

--- packages/browser-ui/src/inspector/views/CompGraph.tsx
+++ packages/browser-ui/src/inspector/views/CompGraph.tsx
@@ -4,6 +4,8 @@ import { prettyPrintFn, evalFns } from "@penrose/core";
 import { zip, uniqBy } from "lodash";
 import DataTable from "react-data-table-component";
 import Graph from 'react-graph-vis';
+import cytoscape from 'cytoscape';
+
 // import { VarAD } from "@penrose/core/";
 
 // TODO: Verify this is correct on small examples
@@ -16,34 +18,35 @@ import Graph from 'react-graph-vis';
 // import "./network.css";
 
 // Flatten a list of lists into a single list of elements
-const merge = (arr: any) => [].concat.apply([], arr);
+// const merge = (arr: any) => [].concat.apply([], arr);
 
-// Given a parent node, returns the graph corresponding to nodes and edges of children
-// May contain duplicate nodes
-// TODO: Move this to utils?
-// TODO: Add type for graph and VarAD
-const traverseGraphTopDown = (par: any): any => {
-  const parNode = { id: par.id, label: par.op };
-  const edges = par.children.map(edge => ({ from: parNode.id, to: edge.node.id }));
+// // Given a parent node, returns the graph corresponding to nodes and edges of children
+// // May contain duplicate nodes
+// // TODO: Move this to utils?
+// // TODO: Add type for graph and VarAD
+// const traverseGraphTopDown = (par: any): any => {
+//   const parNode = { id: par.id, label: par.op };
+//   const edges = par.children.map(edge => ({ from: parNode.id, to: edge.node.id }));
 
-  const subgraphs = par.children.map(edge => traverseGraphTopDown(edge.node));
-  const subnodes = merge(subgraphs.map(g => g.nodes));
-  const subedges = merge(subgraphs.map(g => g.edges));
+//   const subgraphs = par.children.map(edge => traverseGraphTopDown(edge.node));
+//   const subnodes = merge(subgraphs.map(g => g.nodes));
+//   const subedges = merge(subgraphs.map(g => g.edges));
 
-  return {
-    nodes: [parNode].concat(subnodes),
-    edges: edges.concat(subedges)
-  };
-};
+//   return {
+//     nodes: [parNode].concat(subnodes),
+//     edges: edges.concat(subedges)
+//   };
+// };
 
-// Return unique nodes after all nodes are merged
-const traverseUnique = (par: any): any => {
-  const g = traverseGraphTopDown(par);
-  return {
-    ...g,
-    nodes: uniqBy(g.nodes, e => e.id)
-  };
-};
+// // Return unique nodes after all nodes are merged
+// const traverseUnique = (par: any): any => {
+//   const g = traverseGraphTopDown(par);
+//   console.log("??");
+//   return {
+//     ...g,
+//     nodes: uniqBy(g.nodes, e => e.id)
+//   };
+// };
 
 const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
   if (!frame) {
@@ -55,44 +58,103 @@ const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
   }
 
   // TODO: Visualize the gradient too (multi inputs)
-  const top = frame.params.energyGraph;
-  const graph = traverseUnique(top);
+  // const top = frame.params.energyGraph;
+  // const graph = traverseUnique(top);
 
-  console.log("graph", graph);
+  // console.log("graph", graph);
   // TODO: Figure out how to get the graph to render nicely in the inspector w/o being offscreen and appearing only on pan
   // .focus("2"); // Not sure how to use the 'focus' function in this lib?
   // https://visjs.github.io/vis-network/docs/network/#methodCanvas
 
-  const options = {
-    layout: {
-      hierarchical: true
-    },
-    edges: {
-      color: "#000000"
-    },
-    height: "1000px"
-    // height: "100%",
-    // width: "100%",
-    // autoResize: true
-  };
+  // const options = {
+  //   layout: {
+  //     hierarchical: true
+  //   },
+  //   edges: {
+  //     color: "#000000"
+  //   },
+  //   height: "1000px"
+  //   // height: "100%",
+  //   // width: "100%",
+  //   // autoResize: true
+  // };
 
-  const events = {
-    select: function(event) {
-      var { nodes, edges } = event;
+  // const events = {
+  //   select: function(event) {
+  //     var { nodes, edges } = event;
+  //   }
+  // };
+
+  const graphRef = React.useRef<HTMLElement>();
+  React.useEffect(e => {
+    if (graphRef.current !== null) {
+      console.log("GR", graphRef.current);
+
+      var cy = cytoscape({
+
+        // container: document.getElementById('cy'), // container to render in
+        container: graphRef.current, // container to render in
+
+        elements: [ // list of graph elements to start with
+          { // node a
+            data: { id: 'a' }
+          },
+          { // node b
+            data: { id: 'b' }
+          },
+          { // edge ab
+            data: { id: 'ab', source: 'a', target: 'b' }
+          }
+        ],
+
+        style: [ // the stylesheet for the graph
+          {
+            selector: 'node',
+            style: {
+              'background-color': '#666',
+              'label': 'data(id)'
+            }
+          },
+
+          {
+            selector: 'edge',
+            style: {
+              'width': 3,
+              'line-color': '#ccc',
+              'target-arrow-color': '#ccc',
+              'target-arrow-shape': 'triangle',
+              'curve-style': 'bezier'
+            }
+          }
+        ],
+
+        layout: {
+          name: 'grid',
+          rows: 1
+        },
+
+        pan: { x: 100, y: 100 }
+
+      });
+
+      // var layout = cy.layout({
+      //   // name: 'random'
+      //   name: 'grid',
+      //   fit: true,
+      //   avoidOverlap: true
+      // });
+
+      // layout.run();
+      console.log("cy", cy);
+      return () => { cy.destroy(); };
+
     }
-  };
+  }, []);
 
   return (
-    <div>
+    <div style={{ height: "100%" }}>
       <h1>Computation graph of the energy (atomic ops only)</h1>
-      <Graph
-        graph={graph}
-        options={options}
-        events={events}
-        getNetwork={network => {
-          //  if you want access to vis.js network api you can set the state in a parent component using this property
-        }}
-      />
+      <div id="cy" ref={graphRef} style={{ width: "100%", height: "100%" }} />
     </div>
   );
 };
