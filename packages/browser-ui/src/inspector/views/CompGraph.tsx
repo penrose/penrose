1c209fb083ad866f4f717ff8c0cb39f8cbb7e384
katherineye <kye@princeton.edu>
2021-04-09 16:34:45 -0400

    [opt-vis] layout with cytoscape-dagre, which works well with small-med examples so far

--- packages/browser-ui/src/inspector/views/CompGraph.tsx
+++ packages/browser-ui/src/inspector/views/CompGraph.tsx
@@ -1,48 +1,46 @@
 import * as React from "react";
 import IViewProps from "./IViewProps";
 import cytoscape from "cytoscape";
+import { uniqBy } from "lodash";
+import dagre from 'cytoscape-dagre';
+
+cytoscape.use(dagre);
 
 // import { VarAD } from "@penrose/core/";
 
 // TODO: Verify this is correct on small examples
-
 // TODO: Only render the graph once. Otherwise, it's rerendered on each frame, which makes things substantially slower
-
-// TODO Not sure what to do with this?
-// import "./styles.css";
-// // need to import the vis network css in order to show tooltip
-// import "./network.css";
+// TODO: Visualize the gradient too (multi inputs)
 
 // Flatten a list of lists into a single list of elements
-// const merge = (arr: any) => [].concat.apply([], arr);
+const merge = (arr: any) => [].concat.apply([], arr);
 
-// // Given a parent node, returns the graph corresponding to nodes and edges of children
-// // May contain duplicate nodes
-// // TODO: Move this to utils?
-// // TODO: Add type for graph and VarAD
-// const traverseGraphTopDown = (par: any): any => {
-//   const parNode = { id: par.id, label: par.op };
-//   const edges = par.children.map(edge => ({ from: parNode.id, to: edge.node.id }));
+// Given a parent node, returns the graph corresponding to nodes and edges of children
+// May contain duplicate nodes
+// TODO: Move this to utils?
+// TODO: Add type for graph and VarAD
+const traverseGraphTopDown = (par: any): any => {
+  const parNode = { id: par.id, label: par.op };
+  const edges = par.children.map(edge => ({ from: parNode.id, to: edge.node.id }));
 
-//   const subgraphs = par.children.map(edge => traverseGraphTopDown(edge.node));
-//   const subnodes = merge(subgraphs.map(g => g.nodes));
-//   const subedges = merge(subgraphs.map(g => g.edges));
+  const subgraphs = par.children.map(edge => traverseGraphTopDown(edge.node));
+  const subnodes = merge(subgraphs.map(g => g.nodes));
+  const subedges = merge(subgraphs.map(g => g.edges));
 
-//   return {
-//     nodes: [parNode].concat(subnodes),
-//     edges: edges.concat(subedges)
-//   };
-// };
+  return {
+    nodes: [parNode].concat(subnodes),
+    edges: edges.concat(subedges)
+  };
+};
 
-// // Return unique nodes after all nodes are merged
-// const traverseUnique = (par: any): any => {
-//   const g = traverseGraphTopDown(par);
-//   console.log("??");
-//   return {
-//     ...g,
-//     nodes: uniqBy(g.nodes, e => e.id)
-//   };
-// };
+// Return unique nodes after all nodes are merged
+const traverseUnique = (par: any): any => {
+  const g = traverseGraphTopDown(par);
+  return {
+    ...g,
+    nodes: uniqBy(g.nodes, e => e.id)
+  };
+};
 
 const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
   if (!frame) {
@@ -53,33 +51,27 @@ const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
     );
   }
 
-  // TODO: Visualize the gradient too (multi inputs)
-  // const top = frame.params.energyGraph;
-  // const graph = traverseUnique(top);
+  const top = frame.params.energyGraph;
+  const graph = traverseUnique(top);
 
-  // console.log("graph", graph);
-  // TODO: Figure out how to get the graph to render nicely in the inspector w/o being offscreen and appearing only on pan
-  // .focus("2"); // Not sure how to use the 'focus' function in this lib?
-  // https://visjs.github.io/vis-network/docs/network/#methodCanvas
+  // convert between schemas
+  const { nodes, edges } = graph;
+  const nodes2 = nodes.map(e => ({
+    data: {
+      id: String(e.id),
+      label: e.label
+    }
+  }));
+  const edges2 = edges.map(e => ({
+    data: {
+      id: String(e.from) + " " + String(e.to), // NOTE: No duplicate edges
+      source: String(e.from),
+      target: String(e.to)
+      // No label
+    }
+  }));
 
-  // const options = {
-  //   layout: {
-  //     hierarchical: true
-  //   },
-  //   edges: {
-  //     color: "#000000"
-  //   },
-  //   height: "1000px"
-  //   // height: "100%",
-  //   // width: "100%",
-  //   // autoResize: true
-  // };
-
-  // const events = {
-  //   select: function(event) {
-  //     var { nodes, edges } = event;
-  //   }
-  // };
+  console.log("graph # nodes", nodes2.length, "# edges", edges2.length);
 
   const graphRef = React.useRef<HTMLDivElement>(null);
   React.useEffect(() => {
@@ -87,21 +79,10 @@ const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
       const cy = cytoscape({
         container: graphRef.current, // container to render in
 
-        elements: [
-          // list of graph elements to start with
-          {
-            // node a
-            data: { id: "a" }
-          },
-          {
-            // node b
-            data: { id: "b" }
-          },
-          {
-            // edge ab
-            data: { id: "ab", source: "a", target: "b" }
-          }
-        ],
+        elements: {
+          nodes: nodes2,
+          edges: edges2,
+        },
 
         style: [
           // the stylesheet for the graph
@@ -109,7 +90,7 @@ const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
             selector: "node",
             style: {
               "background-color": "#666",
-              label: "data(id)"
+              label: "data(label)"
             }
           },
 
@@ -125,12 +106,16 @@ const CompGraph: React.FC<IViewProps> = ({ frame, history }: IViewProps) => {
           }
         ],
 
-        layout: {
-          name: "grid",
-          rows: 1,
-          fit: true
-        }
+        // layout: {
+        //   name: "grid",
+        //   rows: 1,
+        //   fit: true
+        // }
       });
+
+      cy.layout({
+        name: "dagre"
+      }).run();
 
       return () => {
         cy.destroy();
