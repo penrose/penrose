c731b4a48f0b5f7c78adf6e74bdd7a662020d2a8
Josh Pollock <jopo@mit.edu>
2021-06-14 15:37:42 -0400

    try again to reduce

--- packages/browser-ui/src/inspector/makeViewBoxes.tsx
+++ packages/browser-ui/src/inspector/makeViewBoxes.tsx
@@ -35,35 +35,37 @@ const makeViewBoxes = (
           right: 0,
         }}
       >
-        {shapes.map(({ properties, shapeType }, key: number) => {
-          // If the inspector is crashing around here, then probably the shape doesn't have the width/height properties, so add a special case as below
-          // console.log("properties, shapeType", properties, shapeType, properties.w, properties.h);
-          const [w, h] = bBoxDims(properties, shapeType);
-          return (
-            <ShapeItem
-              key={`shapePreview-${key}`}
-              selected={selectedShape === key}
-              onClick={() => setSelectedShape(key)}
-            >
-              <div>
-                <svg
-                  viewBox={`0 0 ${w} ${h}`}
-                  width="50"
-                  height="50"
-                  dangerouslySetInnerHTML={{
-                    __html: RenderShape(
-                      { properties, shapeType, canvasSize: undefined },
-                      [w, h]
-                    ).outerHTML,
-                  }}
-                />
-              </div>
-              <div style={{ margin: "0.5em" }}>
-                <span>{properties.name.contents}</span>
-              </div>
-            </ShapeItem>
-          );
-        })}
+        {shapes.map(
+          ({ properties, shapeType }: ShapeTypes.Shape, key: number) => {
+            // If the inspector is crashing around here, then probably the shape doesn't have the width/height properties, so add a special case as below
+            // console.log("properties, shapeType", properties, shapeType, properties.w, properties.h);
+            const [w, h] = bBoxDims(properties, shapeType);
+            return (
+              <ShapeItem
+                key={`shapePreview-${key}`}
+                selected={selectedShape === key}
+                onClick={() => setSelectedShape(key)}
+              >
+                <div>
+                  <svg
+                    viewBox={`0 0 ${w} ${h}`}
+                    width="50"
+                    height="50"
+                    dangerouslySetInnerHTML={{
+                      __html: RenderShape(
+                        { properties, shapeType, canvasSize: undefined },
+                        [w, h]
+                      ).outerHTML,
+                    }}
+                  />
+                </div>
+                <div style={{ margin: "0.5em" }}>
+                  <span>{properties.name.contents}</span>
+                </div>
+              </ShapeItem>
+            );
+          }
+        )}
       </ul>
     </div>
   );
