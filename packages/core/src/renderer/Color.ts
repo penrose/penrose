47b9c0800b5d4fe8d6c474efea91cf936c6b2966
heleaf <hfyang544@gmail.com>
2021-06-21 19:27:14 -0400

    organized fns a bit in Color.ts

--- packages/core/src/renderer/Color.ts
+++ packages/core/src/renderer/Color.ts
@@ -3,6 +3,10 @@ import { State } from "types/state";
 import { Shape } from "types/shape";
 import { Color } from "types/value";
 
+/************************************************************************/
+/*                   Color Function Type Declarations                   */
+/************************************************************************/
+
 /**
  * @type Stores RGB color as [R,G,B]; each number is a decimal between 0 & 1
  */
@@ -13,52 +17,63 @@ export type RGB = [number, number, number];
  */
 export type Graph = number[][];
 
-// Adjacency List data structure type
-// given adj : Ajlist,
-// adj[i] is a list of the nodes that have edges connected to node i
+/**
+ * @type Adjacency List representation of a graph.
+ * Given adj : Ajlist, adj[i] is the list of nodes that are connected to node i
+ */
 export type Ajlist = number[][];
 
-// initializes a matrix of all 0.s
-const createMatrix = (rows: number, cols: number): Graph => {
-  var matrix: number[][] = [];
-  for (var i = 0; i < rows; i++) {
-    var row: number[] = [];
-    for (var j = 0; j < cols; j++) {
-      row.push(0);
-    }
-    matrix.push(row);
-  }
-  return matrix;
-};
+/************************************************************************/
+/*                        Main Coloring Functions                       */
+/************************************************************************/
 
-// euclidean distance between two vectors
-const dist = (v1: number[], v2: number[]): number => {
-  if (v1.length != v2.length) {
-    throw new Error("Vector inputs are not of the same dimension");
-  }
-  var squaredSum = 0;
-  for (var i = 0; i < v1.length; i++) {
-    squaredSum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
-  }
-  return Math.sqrt(squaredSum);
-};
+// assign colors to the shapes w/ uninitialized colors
+export const colorUninitShapes = (state: State): State => {
+  // get the fn that checks if a shape has an uninitialized color
+  const hasUninitializedColor = getUninitializedColorCheckerFn(state);
 
-// excluding shapes that don't have a center or color attribute, or
-// shapes that alredy have appropriate default colors, like text (black)
-// or this includes shapes that i haven't gotten too yet
-const includeShapesOnly = (shape: Shape): boolean => {
-  return !(
-    shape.shapeType === "FreeformPolygon" ||
-    shape.shapeType === "Polygon" ||
-    shape.shapeType === "Line" ||
-    shape.shapeType === "Arrow" ||
-    shape.shapeType === "Path" ||
-    shape.shapeType === "Text" ||
-    shape.shapeType === "Image" ||
-    shape.shapeType === "PathString" ||
-    shape.shapeType === "Polyline"
+  // make a new (stricter) fn that also checks
+  // if a shape obj satisfies includeShapesOnly
+  const isUninitializedColorShape = (shape: Shape): boolean => {
+    return hasUninitializedColor(shape) && includeShapesOnly(shape);
+  };
+
+  return getNewlyColoredState(
+    state,
+    isUninitializedColorShape,
+    random_palette(),
+    0.5
   );
 };
+
+// to be called after shape colors have all been assigned
+export const colorUninitText = (state: State): State => {
+  // fn that checks if a shape has an uninitialized color path
+  const hasUninitializedColor = getUninitializedColorCheckerFn(state);
+
+  // fn that checks if a shape is a text & has an uninit color path
+  const isUninitializedColorText = (shape: Shape): boolean => {
+    return hasUninitializedColor(shape) && shape.shapeType === "Text";
+  };
+
+  // get the text shapes that we need to assign colors to
+  const textToAssignColors = state.shapes.filter(isUninitializedColorText);
+
+  const shapeNameAndShapeList = getShapeNameAndShapePairList(state);
+
+  // get the colorlist for the text objects
+  const colorList = createTextColorList(
+    textToAssignColors,
+    shapeNameAndShapeList
+  );
+
+  // assign the colors to the text objects in a new state
+  return assignNewColors(state, colorList, isUninitializedColorText, 1);
+};
+
+/************************************************************************/
+/*       Transition Functions between different coloring states         */
+/************************************************************************/
 
 // given a list of shapes, create a matrix that records distance between objects
 // ex. graph[i][j] === distance between shape i and shape j
@@ -98,62 +113,6 @@ const shapeListToDistanceGraph = (shapeList: Shape[]): Graph => {
   }
 
   return object_graph;
-};
-
-// converts a matrix that stores distance between objects
-// to one that stores "repellant energy" between objects
-// ex. graph[i][j] === *how much* objs i and j should repel each other
-// (repel each other in terms of color)
-const distanceGraphToEnergyGraph = (graph: Graph): Graph => {
-  var newGraph = graph;
-  for (var i = 0; i < graph.length; i++) {
-    for (var j = i + 1; j < graph.length; j++) {
-      // super hacky, uses some guidelines fron Constraints.ts (repel fxns)
-
-      const epsilon = 20; // prevent division by 0
-      const weight = 10e4; // scaling factor, since values are typically small
-
-      // invert the distance to get a "repellant energy" value
-      newGraph[i][j] = (1 / (graph[i][j] + epsilon)) * weight;
-
-      // symmetric graph
-      newGraph[j][i] = newGraph[i][j];
-    }
-  }
-  return newGraph;
-};
-
-// create a new state with newly assigned colors to some shapes
-// only shapes that satisfy includeInColorAdjustmentFn have their colors revised
-// the colors are selected from colorList, in the order they appear
-
-// ex. the first shape s for which includeInColorAdjustmentFn(s) === true
-// will be assigned colorList[0] as its color
-
-// the last shape sLast for which includeInColorAdjustmentFn(sLast) === true
-// will be assigned colorList[colorList.length - 1] as its color
-const assignNewColors = (
-  state: State,
-  colorList: RGB[],
-  includeInColorAdjustmentFn: (s: Shape) => boolean = includeShapesOnly,
-  alpha: number = 0.8
-): State => {
-  // assumes all colors map to the order of appropriate objects in state
-  var newState = state;
-  var j = 0;
-  for (var i = 0; i < newState.shapes.length; i++) {
-    if (includeInColorAdjustmentFn(newState.shapes[i])) {
-      newState.shapes[i].properties.color = {
-        tag: "ColorV",
-        contents: {
-          tag: "RGBA",
-          contents: [colorList[j][0], colorList[j][1], colorList[j][2], alpha],
-        },
-      };
-      j += 1;
-    }
-  }
-  return newState;
 };
 
 // build KNN graph (an adjacency list)
@@ -197,30 +156,6 @@ const distGraphToKNNGraph = (distGraph: Graph, k: number): Ajlist => {
   }
 
   return ajlist;
-};
-
-// returns a list of colors from the palette, sampled uniformly
-const sampleUniformPalette = (
-  numColorsRequested: number,
-  palette: RGB[] = viridis_data
-): RGB[] => {
-  // handle 0 case, to prevent division by 0 later on
-  if (numColorsRequested === 0) return [];
-
-  // this fn doesn't work if num colors requested > palette length
-  if (numColorsRequested > palette.length) {
-    throw new Error("More colors requested than available in palette");
-  }
-
-  const stepSize = Math.floor(palette.length / numColorsRequested);
-
-  var rgbList: RGB[] = [];
-
-  for (var i = 0; i < numColorsRequested; i++) {
-    rgbList.push(palette[i * stepSize]);
-  }
-
-  return rgbList;
 };
 
 // takes in a KNN graph (adjacency list)
@@ -297,6 +232,140 @@ const KNNGraphToColorList = (
   return colorList;
 };
 
+// create a new state with newly assigned colors to some shapes
+// only shapes that satisfy includeInColorAdjustmentFn have their colors revised
+// the colors are selected from colorList, in the order they appear
+
+// ex. the first shape s for which includeInColorAdjustmentFn(s) === true
+// will be assigned colorList[0] as its color
+
+// the last shape sLast for which includeInColorAdjustmentFn(sLast) === true
+// will be assigned colorList[colorList.length - 1] as its color
+const assignNewColors = (
+  state: State,
+  colorList: RGB[],
+  includeInColorAdjustmentFn: (s: Shape) => boolean = includeShapesOnly,
+  alpha: number = 0.8
+): State => {
+  // assumes all colors map to the order of appropriate objects in state
+  var newState = state;
+  var j = 0;
+  for (var i = 0; i < newState.shapes.length; i++) {
+    if (includeInColorAdjustmentFn(newState.shapes[i])) {
+      newState.shapes[i].properties.color = {
+        tag: "ColorV",
+        contents: {
+          tag: "RGBA",
+          contents: [colorList[j][0], colorList[j][1], colorList[j][2], alpha],
+        },
+      };
+      j += 1;
+    }
+  }
+  return newState;
+};
+
+const getNewlyColoredState = (
+  state: State,
+  includeInColorAdjustmentFn: (s: Shape) => boolean,
+  palette = random_palette(),
+  alpha = 0.5
+): State => {
+  const shapesToAssignColors = state.shapes.filter(includeInColorAdjustmentFn);
+  const distanceGraph = shapeListToDistanceGraph(shapesToAssignColors);
+  if (distanceGraph.length <= 3) {
+    var k = distanceGraph.length - 1; // number of neighbors
+  } else {
+    var k = 3;
+  }
+  if (k <= 0) {
+    // figure something out here, refactor the previous functions
+    // this happens if distanceGraph.length <= 1, in which case
+    // we don't need to do special coloring (a single object is uncolored)
+    return state;
+  } else {
+    const KNNGraph = distGraphToKNNGraph(distanceGraph, k);
+    const colorList = KNNGraphToColorList(KNNGraph, k, palette);
+    const newState = assignNewColors(
+      state,
+      colorList,
+      includeInColorAdjustmentFn,
+      alpha
+    );
+    return newState;
+  }
+};
+
+/************************************************************************/
+/*                       Helper (Utility) Functions                     */
+/************************************************************************/
+
+// initializes a matrix of all 0.s
+const createMatrix = (rows: number, cols: number): Graph => {
+  var matrix: number[][] = [];
+  for (var i = 0; i < rows; i++) {
+    var row: number[] = [];
+    for (var j = 0; j < cols; j++) {
+      row.push(0);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+};
+
+// euclidean distance between two vectors
+const dist = (v1: number[], v2: number[]): number => {
+  if (v1.length != v2.length) {
+    throw new Error("Vector inputs are not of the same dimension");
+  }
+  var squaredSum = 0;
+  for (var i = 0; i < v1.length; i++) {
+    squaredSum += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+  }
+  return Math.sqrt(squaredSum);
+};
+
+// excluding shapes that don't have a center or color attribute, or
+// shapes that alredy have appropriate default colors, like text (black)
+// or this includes shapes that i haven't gotten too yet
+const includeShapesOnly = (shape: Shape): boolean => {
+  return !(
+    shape.shapeType === "FreeformPolygon" ||
+    shape.shapeType === "Polygon" ||
+    shape.shapeType === "Line" ||
+    shape.shapeType === "Arrow" ||
+    shape.shapeType === "Path" ||
+    shape.shapeType === "Text" ||
+    shape.shapeType === "Image" ||
+    shape.shapeType === "PathString" ||
+    shape.shapeType === "Polyline"
+  );
+};
+
+// returns a list of colors from the palette, sampled uniformly
+const sampleUniformPalette = (
+  numColorsRequested: number,
+  palette: RGB[] = viridis_data
+): RGB[] => {
+  // handle 0 case, to prevent division by 0 later on
+  if (numColorsRequested === 0) return [];
+
+  // this fn doesn't work if num colors requested > palette length
+  if (numColorsRequested > palette.length) {
+    throw new Error("More colors requested than available in palette");
+  }
+
+  const stepSize = Math.floor(palette.length / numColorsRequested);
+
+  var rgbList: RGB[] = [];
+
+  for (var i = 0; i < numColorsRequested; i++) {
+    rgbList.push(palette[i * stepSize]);
+  }
+
+  return rgbList;
+};
+
 // given a state, returns a (shape => bool) function f:
 // f(s) === true if shape s has an uninitialized color path
 // f(s) === false if shape s has a (user) initialized color path
@@ -331,54 +400,48 @@ const getUninitializedColorCheckerFn = (
   return hasUninitializedColor;
 };
 
-// assign colors to the shapes w/ uninitialized colors
-export const colorUninitShapes = (state: State): State => {
-  // get the fn that checks if a shape has an uninitialized color
-  const hasUninitializedColor = getUninitializedColorCheckerFn(state);
+const getShapeNameAndShapePairList = (state: State): [string, Shape][] => {
+  // get all color-able shapes (circle, square, rect, ellipse, callout)
+  const colorShapes = state.shapes.filter(includeShapesOnly);
 
-  // make a new (stricter) fn that also checks
-  // if a shape obj satisfies includeShapesOnly
-  const isUninitializedColorShape = (shape: Shape): boolean => {
-    return hasUninitializedColor(shape) && includeShapesOnly(shape);
+  const colorShapeNames = colorShapes.map((elem) => {
+    return elem.properties.name.contents;
+  });
+
+  // get the list of color-able shape names, in order in which they are drawn
+  // shapes that appear at the beginning of the shapeLayeringOrderAll list
+  // are the shapes that are drawn first (and would appear on the "bottom")
+  const shapeLayeringOrderAll = state.shapeOrdering.filter((elem) => {
+    return colorShapeNames.includes(elem);
+  });
+
+  // reverse the list to
+  // get the shapes that are drawn most recently at the front of the list
+  const reversedShapeLayeringOrderAll = shapeLayeringOrderAll.reverse();
+
+  // takes in a shapeName, and returns a fn that checks of a shape has that name
+  const isMatchingShape = (shapeName: string) => {
+    const fn = (shape: Shape) => {
+      return shapeName === shape.properties.name.contents;
+    };
+    return fn;
   };
 
-  return getNewlyColoredState(
-    state,
-    isUninitializedColorShape,
-    random_palette(),
-    0.5
-  );
-};
+  // maps the list of shape names to a list of
+  // [shapeName, shape] : [string, Shape] objects
+  const shapeNameAndShapePairsAll: [
+    string,
+    Shape
+  ][] = reversedShapeLayeringOrderAll.map((shapeName) => {
+    const matchesName = isMatchingShape(shapeName);
+    const shape = colorShapes.find(matchesName);
+    if (typeof shape === "undefined") {
+      throw new Error("im sad");
+    }
+    return [shapeName, shape];
+  });
 
-const getNewlyColoredState = (
-  state: State,
-  includeInColorAdjustmentFn: (s: Shape) => boolean,
-  palette = random_palette(),
-  alpha = 0.5
-): State => {
-  const shapesToAssignColors = state.shapes.filter(includeInColorAdjustmentFn);
-  const distanceGraph = shapeListToDistanceGraph(shapesToAssignColors);
-  if (distanceGraph.length <= 3) {
-    var k = distanceGraph.length - 1; // number of neighbors
-  } else {
-    var k = 3;
-  }
-  if (k <= 0) {
-    // figure something out here, refactor the previous functions
-    // this happens if distanceGraph.length <= 1, in which case
-    // we don't need to do special coloring (a single object is uncolored)
-    return state;
-  } else {
-    const KNNGraph = distGraphToKNNGraph(distanceGraph, k);
-    const colorList = KNNGraphToColorList(KNNGraph, k, palette);
-    const newState = assignNewColors(
-      state,
-      colorList,
-      includeInColorAdjustmentFn,
-      alpha
-    );
-    return newState;
-  }
+  return shapeNameAndShapePairsAll;
 };
 
 // detects if an (x,y) point is contained within a (limited) number of shapes
@@ -448,66 +511,6 @@ const pointInShape = (shape: Shape, point: [number, number]): boolean => {
        point detection inside polygons
       */
   }
-};
-
-// to be called after shape colors have all been assigned
-export const colorUninitText = (state: State): State => {
-  // fn that checks if a shape has an uninitialized color path
-  const hasUninitializedColor = getUninitializedColorCheckerFn(state);
-
-  // fn that checks if a shape is a text & has an uninit color path
-  const isUninitializedColorText = (shape: Shape): boolean => {
-    return hasUninitializedColor(shape) && shape.shapeType === "Text";
-  };
-
-  // get the text shapes that we need to assign colors to
-  const textToAssignColors = state.shapes.filter(isUninitializedColorText);
-
-  // get all the shapes
-  const colorShapes = state.shapes.filter(includeShapesOnly);
-
-  const colorShapeNames = colorShapes.map((elem) => {
-    return elem.properties.name.contents;
-  });
-
-  // shapes at the beginning of the list are drawn first
-  const shapeLayeringOrderAll = state.shapeOrdering.filter((elem) => {
-    return colorShapeNames.includes(elem);
-  });
-
-  // get the shapes that are drawn most recently at the front of the list
-  const reversedShapeLayeringOrderAll = shapeLayeringOrderAll.reverse();
-
-  // takes in a shapeName, and returns a fn that checks of a shape has that name
-  const isMatchingShape = (shapeName: string) => {
-    const fn = (shape: Shape) => {
-      return shapeName === shape.properties.name.contents;
-    };
-    return fn;
-  };
-
-  // maps the list of shape names to a list of
-  // [shapeName, shape] : [string, Shape] objects
-  const shapeNameAndShapePairsAll: [
-    string,
-    Shape
-  ][] = reversedShapeLayeringOrderAll.map((shapeName) => {
-    const matchesName = isMatchingShape(shapeName);
-    const shape = colorShapes.find(matchesName);
-    if (typeof shape === "undefined") {
-      throw new Error("im sad");
-    }
-    return [shapeName, shape];
-  });
-
-  // get the colorlist for the text objects
-  const colorList = createTextColorList(
-    textToAssignColors,
-    shapeNameAndShapePairsAll
-  );
-
-  // assign the colors to the text objects in a new state
-  return assignNewColors(state, colorList, isUninitializedColorText, 1);
 };
 
 const createTextColorList = (
@@ -596,4 +599,29 @@ const createTextColorList = (
     colorList.push(color as RGB);
   }
   return colorList;
+};
+
+/**@deprecated */
+// not currently in use
+// converts a matrix that stores distance between objects
+// to one that stores "repellant energy" between objects
+// ex. graph[i][j] === *how much* objs i and j should repel each other
+// (repel each other in terms of color)
+const distanceGraphToEnergyGraph = (graph: Graph): Graph => {
+  var newGraph = graph;
+  for (var i = 0; i < graph.length; i++) {
+    for (var j = i + 1; j < graph.length; j++) {
+      // super hacky, uses some guidelines fron Constraints.ts (repel fxns)
+
+      const epsilon = 20; // prevent division by 0
+      const weight = 10e4; // scaling factor, since values are typically small
+
+      // invert the distance to get a "repellant energy" value
+      newGraph[i][j] = (1 / (graph[i][j] + epsilon)) * weight;
+
+      // symmetric graph
+      newGraph[j][i] = newGraph[i][j];
+    }
+  }
+  return newGraph;
 };
