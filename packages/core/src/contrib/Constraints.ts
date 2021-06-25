be57242a72e22debdc3594d5948bc6c9dfa85182
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2021-06-25 13:35:53 -0700

    addressing feedback

--- packages/core/src/contrib/Constraints.ts
+++ packages/core/src/contrib/Constraints.ts
@@ -258,17 +258,12 @@ export const objDict = {
       const l1 = ops.vsub(c0, c1);
       const l2 = ops.vsub(c2, c1);
 
+      const cosine = absVal(ops.vdot(ops.vnormalize(l1), ops.vnormalize(l2)));
       // angles between 10 and 170 degrees do not need to be pushed
       return ifCond(
-        lt(
-          absVal(ops.vdot(ops.vnormalize(l1), ops.vnormalize(l2))),
-          varOf(0.985) // cos10 deg = .9848, abs(cos170) = .9848
-        ),
+        lt(cosine, varOf(0.985)), // cos10 deg = .9848, abs(cos170) = .9848
         varOf(0),
-        mul(
-          constOfIf(strength),
-          div(squared(ops.vdot(l1, l2)), mul(ops.vnormsq(l1), ops.vnormsq(l2)))
-        )
+        mul(constOfIf(strength), cosine)
       );
     } else {
       throw new Error(
