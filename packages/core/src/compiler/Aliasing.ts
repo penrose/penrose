0d3b0699794641cd912d49785de8fc19593eb361
heleaf <hfyang544@gmail.com>
2021-07-16 18:04:12 -0400

    merged aliasing functions into style compiler

--- packages/core/src/compiler/Aliasing.ts
+++ packages/core/src/compiler/Aliasing.ts
@@ -13,30 +13,13 @@ import { SelEnv, Subst } from "types/styleSemantics";
 import { Identifier } from "types/ast";
 import _ from "lodash";
 
-// helper for debugging fxn
-const extractAliasFromRelationPattern = (rel: RelationPattern): string[] => {
-  // get rid of
-  if (rel.tag === "RelPred" && rel.alias) {
-    return [rel.alias.value];
-  }
-  return [];
-};
-
-// for debugging
-const getHeaderAliasKeywords = (header: Header): string[] => {
-  // get rid of
-  if (header.tag === "Selector") {
-    const sel = header;
-    const rels = safeContentsList(sel.where);
-    return _.flatMap(rels, extractAliasFromRelationPattern);
-  } else {
-    // namespace has no aliases
-    return [];
-  }
-};
-
+/**
+ * Helper fxn for @function isValidRelSubst.
+ *
+ * Determines if all the variables in @param sebind
+ * exist as valid keys in @param subst.
+ */
 const sebindExistsInSubst = (sebind: SEBind, subst: Subst): boolean => {
-  // finding substs
   if (sebind.contents.tag !== "StyVar") {
     throw new Error("expected sty var");
   }
@@ -44,8 +27,13 @@ const sebindExistsInSubst = (sebind: SEBind, subst: Subst): boolean => {
   return validKeywords.includes(sebind.contents.contents.value);
 };
 
+/**
+ * Helper fxn for @function isValidRelSubst.
+ *
+ * Determines if all the variables in @param expr
+ * exist as valid keys in @param subst.
+ */
 const selExprExistsInSubst = (expr: SelExpr, subst: Subst): boolean => {
-  // finding substs
   if (expr.tag === "SEBind") {
     return sebindExistsInSubst(expr, subst);
   } else if (expr.tag === ("SEFunc" || "SEValCons" || "SEFuncOrValCons")) {
@@ -53,8 +41,13 @@ const selExprExistsInSubst = (expr: SelExpr, subst: Subst): boolean => {
   } else throw new Error("unknown tag");
 };
 
+/**
+ * Helper fxn for @function isValidRelSubst.
+ *
+ * Determines if all the variables in @param arg
+ * exist as valid keys in @param subst.
+ */
 const predArgExistsInSubst = (arg: PredArg, subst: Subst): boolean => {
-  // finding substs
   if (arg.tag === "RelPred") {
     return isValidRelSubst(subst, arg);
   } else if (arg.tag === "SEBind") {
@@ -62,8 +55,14 @@ const predArgExistsInSubst = (arg: PredArg, subst: Subst): boolean => {
   } else throw new Error("unknown tag");
 };
 
+/**
+ * Determines if all the variables in @param rel exist as valid
+ * keys in @param subst.
+ *
+ * Ex, IsSubset(x,y) must have 'x' and 'y' exist as keys in @param subst
+ * for the relation to be valid.
+ */
 const isValidRelSubst = (subst: Subst, rel: RelationPattern): boolean => {
-  // finding substs
   if (rel.tag === "RelPred") {
     return rel.args.every((arg) => predArgExistsInSubst(arg, subst));
   } else if (rel.tag === "RelBind") {
@@ -73,17 +72,35 @@ const isValidRelSubst = (subst: Subst, rel: RelationPattern): boolean => {
   }
 };
 
+/**
+ * Helper fxn for @function getRelPredAliasInstanceName
+ *
+ * Returns the substitution for a bindingform
+ */
+const getBindingFormAliasInstanceName = (
+  bf: BindingForm,
+  subst: Subst
+): string => {
+  if (bf.tag === "SubVar") {
+    return bf.contents.value;
+  } else if (bf.tag === "StyVar") {
+    return subst[bf.contents.value];
+  } else throw new Error("unknown tag");
+};
+
+/**
+ * Returns the substitution for a predicate alias
+ */
 // IsSubset(B,A) --> `IsSubset_B_A`
 // IsSubset(Union(B,C),A) --> `IsSubset_Union_B_C_A`
-// this can be refactored for a more descriptive name for nested cases
+// TODO: this can be refactored for a more descriptive name for nested cases
+// TODO: adding parentheses into the strings messes with GPIs sometimes?
 const getRelPredAliasInstanceName = (
-  // finding substs
   relPred: RelPred,
   subst: Subst
 ): string => {
-  var name = relPred.name.value;
-  // name = name.concat('(') // for some reason this messes with arrow GPIs
-  for (var arg of relPred.args) {
+  let name = relPred.name.value;
+  for (let arg of relPred.args) {
     if (arg.tag === "RelPred") {
       name = name.concat("_").concat(getRelPredAliasInstanceName(arg, subst));
     } else if (arg.tag === "SEBind") {
@@ -95,28 +112,19 @@ const getRelPredAliasInstanceName = (
   return name;
 };
 
-const getBindingFormAliasInstanceName = (
-  // finding substs
-  bf: BindingForm,
-  subst: Subst
-): string => {
-  if (bf.tag === "SubVar") {
-    return bf.contents.value;
-  } else if (bf.tag === "StyVar") {
-    return subst[bf.contents.value];
-  } else throw new Error("unknown tag");
-};
-
-export const addRelPredAliasSubsts = (
-  // finding substs
+/**
+ * Adds predicate alias substitutions to an existing valid subst
+ * @param subst a valid substitution for a given style selector
+ * @param rels a list of relations for the same style selector
+ */
+const addRelPredAliasSubsts = (
   subst: Subst,
   rels: RelationPattern[]
 ): Subst => {
-  // a shallow copy
-  // not really necessary, but w/o this it does alias (still might alias internally)
-  subst = { ...subst };
+  subst = { ...subst }; // a shallow copy
 
-  for (var rel of rels) {
+  // only consider valid predicates in context of each subst
+  for (let rel of rels) {
     if (rel.tag === "RelPred" && rel.alias && isValidRelSubst(subst, rel)) {
       subst[rel.alias.value] = getRelPredAliasInstanceName(rel, subst);
     }
@@ -125,18 +133,26 @@ export const addRelPredAliasSubsts = (
   return subst;
 };
 
+/**
+ * Helper fxn for checking well-formedness of selectors with predicate aliases
+ * @param m : Map<string, any>
+ */
 const getKeyWordsFromMap = (m: any): string[] => {
-  // selector checking
-  let acc: string[] = [];
+  let keywords: string[] = [];
   const iterator = m.keys();
   for (let i = 0; i < m.size; i++) {
-    acc.push(iterator.next().value);
+    keywords.push(iterator.next().value);
   }
-  return acc;
+  return keywords;
 };
 
+/**
+ * Helper fxn for checking that predicate alias names don't conflict with
+ * existing domain keywords
+ *
+ * Returns a list of domain keywords that the aliases cannot match
+ */
 const getDomainKeywords = (varEnv: Env): string[] => {
-  // selector checking
   const keyWordMaps = [
     varEnv.types,
     varEnv.functions,
@@ -154,12 +170,20 @@ const getDomainKeywords = (varEnv: Env): string[] => {
   return keywords.concat(subtypeKeywords);
 };
 
+/**
+ * Helper fxn for checking that predicate alias names don't conflict with
+ * existing selector style variable names
+ *
+ * Returns a list of selector keywords that the aliases cannot match
+ */
 const getSelectorStyVarNames = (selEnv: SelEnv): string[] => {
   return Object.keys(selEnv.sTypeVarMap);
 };
 
-export const aliasConflictsWithDomainOrSelectorKeyword = (
-  // selector checking
+/**
+ * Checks for if an alias name conflicts with domain or selector keywords
+ */
+const aliasConflictsWithDomainOrSelectorKeyword = (
   alias: Identifier,
   varEnv: Env,
   selEnv: SelEnv
