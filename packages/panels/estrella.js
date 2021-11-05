064a0f13950bae5b361a1683f05af392160b31c0
wodeni <wn2155@columbia.edu>
2021-11-05 10:19:58 -0400

    chore: add the panels repo

---
+++ packages/panels/estrella.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { build, cliopts } = require("estrella");
+
+build({
+  entry: "./src/index.tsx",
+  outfile: "build/app.js",
+  bundle: true,
+  sourcemap: "inline",
+  keepNames: true,
+  debug: true,
+  format: "iife",
+  platform: "browser",
+  external: ["path", "crypto", "fs"],
+  define: {
+    "process.env.NODE_ENV": '"development"',
+    global: "window",
+  },
+  loader: {
+    ".ttf": "dataurl",
+  },
+});
+
+cliopts.watch &&
+  require("serve-http").createServer({
+    port: 3000,
+    pubdir: ".",
+    indexFilename: "index-estrella.html",
+  });
---
+++ packages/panels/src/SubPane.tsx
@@ -0,0 +1,80 @@
+import * as React from "react";
+import { Dispatcher } from "./reducer";
+import MonacoEditor, { useMonaco } from "@monaco-editor/react";
+import { monacoOptions } from "./Util";
+import { useCallback, useEffect } from "react";
+import {
+  StyleCompletions,
+  StyleConfig,
+  StyleLanguageTokens,
+  SubstanceCompletions,
+  SubstanceConfig,
+  SubstanceLanguageTokens,
+} from "./languageConfigs";
+import { IRange } from "monaco-editor";
+
+const SubPane = ({
+  value,
+  domainCache,
+  dispatch,
+  numOpen,
+}: {
+  value: string;
+  domainCache: any;
+  dispatch: Dispatcher;
+  numOpen: number;
+}) => {
+  const monaco = useMonaco();
+  const provideCompletion = useCallback(
+    (model, position) => {
+      const word = model.getWordUntilPosition(position);
+      const range: IRange = {
+        startLineNumber: position.lineNumber,
+        endLineNumber: position.lineNumber,
+        startColumn: word.startColumn,
+        endColumn: word.endColumn,
+      };
+      return { suggestions: SubstanceCompletions(range, domainCache) };
+    },
+    [domainCache]
+  );
+  useEffect(() => {
+    if (monaco) {
+      monaco.languages.register({ id: "substance" });
+      monaco.languages.setLanguageConfiguration("substance", SubstanceConfig);
+      if (domainCache) {
+        monaco.languages.setMonarchTokensProvider(
+          "substance",
+          SubstanceLanguageTokens(domainCache)
+        );
+        const dispose = monaco.languages.registerCompletionItemProvider(
+          "substance",
+          {
+            provideCompletionItems: provideCompletion,
+          }
+        );
+        return () => {
+          // prevents duplicates
+          dispose.dispose();
+        };
+      }
+    }
+  }, [monaco, provideCompletion, domainCache]);
+  return (
+    <MonacoEditor
+      value={value}
+      width={`${window.innerWidth / numOpen}px`}
+      onChange={(content) =>
+        dispatch({
+          kind: "CHANGE_CODE",
+          lang: "sub",
+          content: content as string,
+        })
+      }
+      defaultLanguage="substance"
+      options={monacoOptions}
+    />
+  );
+};
+
+export default SubPane;
