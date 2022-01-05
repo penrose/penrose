8785fe335936b9b9b13860cb7175e773575ad3ec
Sam Estep <sam@samestep.com>
2022-01-05 16:59:39 -0500

    Migrate all color properties in examples

--- packages/core/src/shapes/Equation.ts
+++ packages/core/src/shapes/Equation.ts
@@ -1,10 +1,31 @@
-import { INamed, ICenter, IRect, IRotate, IString, IShape } from "types/shapes";
-import { Canvas, sampleVector, sampleZero, StrV } from "./Samplers";
+import {
+  INamed,
+  ICenter,
+  IRect,
+  IRotate,
+  IString,
+  IShape,
+  IFill,
+} from "types/shapes";
+import {
+  Canvas,
+  sampleBlack,
+  sampleVector,
+  sampleZero,
+  StrV,
+} from "./Samplers";
 
-export interface IEquation extends INamed, ICenter, IRect, IRotate, IString {}
+export interface IEquation
+  extends INamed,
+    IFill,
+    ICenter,
+    IRect,
+    IRotate,
+    IString {}
 
 export const sampleEquation = (canvas: Canvas): IEquation => ({
   name: StrV("defaultEquation"),
+  fillColor: sampleBlack(),
   center: sampleVector(canvas),
   width: sampleZero(),
   height: sampleZero(),
