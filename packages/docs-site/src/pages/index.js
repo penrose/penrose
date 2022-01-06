d8d4e99d4c37a7926049323235355445738462f1
Max Krieger <maxkrieger@users.noreply.github.com>
2022-01-06 18:19:26 -0500

    feat: docusaurus docs site init

---
+++ packages/docs-site/src/pages/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/tutorial/welcome"
+          >
+            Tutorial
+          </Link>
+          <Link
+            className="button button--tertiary button--lg"
+            to="https://panes.penrose.ink"
+          >
+            Try
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home`}
+      description="Create beautiful diagrams just by typing math notation in plain text."
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
