6c7f572c31400df21db70fda8d1533ea09a7362f
wodeni <wn2155@columbia.edu>
2022-01-11 16:49:40 -0500

    fix: stop translating the center of text

--- packages/core/src/renderer/Text.ts
+++ packages/core/src/renderer/Text.ts
@@ -20,8 +20,13 @@ const Text = ({ shape, canvasSize, labels }: ShapeProps): SVGTextElement => {
   // Keep track of which input properties we programatically mapped
   const attrToNotAutoMap: string[] = [];
 
+  const center = shape.properties.center as IVectorV<number>;
+  const [x, y] = toScreen(center.contents as [number, number], canvasSize);
+  elem.setAttribute("x", x.toString());
+  elem.setAttribute("y", y.toString());
+
   // Map/Fill the shape attributes while keeping track of input properties mapped
-  attrToNotAutoMap.push(...attrXY(shape, canvasSize, elem));
+  attrToNotAutoMap.push("x", "y");
   attrToNotAutoMap.push(...attrFill(shape, elem));
   attrToNotAutoMap.push(...attrStroke(shape, elem));
   attrToNotAutoMap.push(...attrTitle(shape, elem));
