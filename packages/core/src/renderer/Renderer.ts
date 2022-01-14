018c4cdae280dd37ea4a2312d59e236f6d46c76d
keenancrane <kmcrane@cs.cmu.edu>
2022-01-14 12:06:10 -0500

    [g-tweaks] Put xmlns before view box

--- packages/core/src/renderer/Renderer.ts
+++ packages/core/src/renderer/Renderer.ts
@@ -172,8 +172,8 @@ export const RenderStatic = async (
   const { shapes, labelCache: labels, canvas } = state;
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("version", "1.2");
-  svg.setAttribute("viewBox", `0 0 ${canvas.width} ${canvas.height}`);
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("viewBox", `0 0 ${canvas.width} ${canvas.height}`);
   for (const shape of shapes) {
     svg.appendChild(
       await RenderShape({
