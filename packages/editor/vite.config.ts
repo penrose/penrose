56e46c4bd618d2a7c9846cb8300652f6d7e0cab7
Max Krieger <maxkrieger@users.noreply.github.com>
2022-01-14 13:21:09 -0500

    fix: add inspect

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import Inspect from "vite-plugin-inspect";
 
 // Fun fact: define: {global: {}} overwrites every mention of global, which breaks lots.
 // Don't use with Vite. Use window.global in index.html
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Inspect()],
 });
