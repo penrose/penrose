866c5b4d7a77f87b7173f2d1322b3cc2cef4d1d1
Sam Estep <sam@samestep.com>
2022-02-10 16:19:30 -0500

    Add test stub with points from [HM83]

--- packages/core/src/contrib/__tests__/Minkowski.test.ts
+++ packages/core/src/contrib/__tests__/Minkowski.test.ts
@@ -115,3 +115,50 @@ describe("halfPlaneSDF", () => {
     expect(numOf(result)).toBeCloseTo(1, digitPrecision);
   });
 });
+
+describe("convexPartitions", () => {
+  test("Figure 1 from [HM83]", () => {
+    // https://link.springer.com/content/pdf/10.1007/3-540-12689-9_105.pdf
+    // point locations approximated by tracing in Inkscape
+    const p1 = [-316.39758, -117.40885];
+    const p2 = [-291.04166, -57.877602];
+    const p3 = [-243.63715, -24.804688];
+    const p4 = [-189.61806, -74.414062];
+    const p5 = [-155.4427, 9.921875];
+    const p6 = [-173.63281, 78.823784];
+    const p7 = [-202.29601, 136.15016];
+    const p8 = [-257.41754, 167.01823];
+    const p9 = [-301.51477, 164.81337];
+    const p10 = [-359.94357, 147.72569];
+    const p11 = [-54.570312, 355.53384];
+    const p12 = [288.28558, 232.61285];
+    const p13 = [241.43228, 148.82812];
+    const p14 = [151.5842, 66.145836];
+    const p15 = [186.31076, 8.8194437];
+    const p16 = [269.54428, -59.53125];
+    const p17 = [219.38368, -151.03297];
+    const p18 = [14.882812, -128.98438];
+    const p19 = [-224.34462, -165.36458];
+    const p = [
+      p1,
+      p2,
+      p3,
+      p4,
+      p5,
+      p6,
+      p7,
+      p8,
+      p9,
+      p10,
+      p11,
+      p12,
+      p13,
+      p14,
+      p15,
+      p16,
+      p17,
+      p18,
+      p19,
+    ];
+  });
+});
