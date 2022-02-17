83f7c5145a482a14d26a09e6011f4a29248e17b0
Sam Estep <sam@samestep.com>
2022-02-17 13:01:00 -0500

    Run Prettier

--- packages/synthesizer-ui/src/main.tsx
+++ packages/synthesizer-ui/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
