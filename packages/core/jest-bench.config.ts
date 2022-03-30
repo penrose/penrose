4d25f1f768c09b50be5a3c4a202974a01e355f58
wodeni <wn2155@columbia.edu>
2022-03-30 12:03:59 -0400

    perf: compilation benchmark

---
+++ packages/core/jest-bench.config.ts
@@ -0,0 +1,17 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-bench/environment",
+  reporters: ["default", "jest-bench/reporter"],
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+  },
+  testEnvironmentOptions: {
+    testEnvironment: "jest-environment-jsdom",
+  },
+  testRegex: "(/__benchmarks__/.*|(\\.|/)bench)\\.[jt]sx?$",
+  modulePaths: ["node_modules", "<rootDir>/src/"],
+  testPathIgnorePatterns: ["build/dist/"],
+  // coverageProvider: "v8",
+  // coveragePathIgnorePatterns: ["/node_modules/", "/test/"],
+  // collectCoverageFrom: ["src/**/*.ts"],
+};
