da7995c5efed5e3e1eb3d68ac68c7e2b7eb898b6
Sam Estep <sam@samestep.com>
2022-04-12 09:25:36 -0400

    Fix ConstraintsUtils.test.ts

--- packages/core/src/contrib/__tests__/ConstraintsUtils.test.ts
+++ packages/core/src/contrib/__tests__/ConstraintsUtils.test.ts
@@ -3,6 +3,7 @@ import {
   overlappingPolygons,
 } from "contrib/ConstraintsUtils";
 import { _rectangles } from "contrib/__testfixtures__/TestShapes.input";
+import { genCode, makeGraph } from "engine/Autodiff";
 
 describe("overlappingAABBs should return the same value as overlappingPolygons", () => {
   it.each([0, 10, 100])("padding %p", (padding: number) => {
@@ -17,7 +18,11 @@ describe("overlappingAABBs should return the same value as overlappingPolygons",
         const result1 = overlappingAABBs([t, r1], [t, r2], padding);
         const result2 = overlappingPolygons([t, r1], [t, r2], padding);
 
-        expect(numOf(result1)).toBeCloseTo(numOf(result2), 4);
+        const [result1num, result2num] = genCode(makeGraph([result1, result2]))(
+          new Map()
+        );
+
+        expect(result1num).toBeCloseTo(result2num, 4);
       }
     }
   });
