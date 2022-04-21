542f8d62398fe03da5448ab8a1b6e590aa0b4067
wodeni <wn2155@columbia.edu>
2022-04-21 13:59:35 -0400

    build: switch to manual polyfill for `global`

--- packages/browser-ui/vite.config.js
+++ packages/browser-ui/vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "window",
-  },
   json: {
     stringify: true,
   },
