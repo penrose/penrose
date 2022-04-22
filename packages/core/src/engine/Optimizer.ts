e5519cbf8b45aea9f5fe4ba10ef7cfa1c741a6ad
Sam Estep <sam@samestep.com>
2022-04-22 13:41:44 -0400

    Get rid of unnecessary array clone before zip2

--- packages/core/src/engine/Optimizer.ts
+++ packages/core/src/engine/Optimizer.ts
@@ -378,7 +378,7 @@ export const step = (
 
     newState.translation = insertVaryings(
       state.translation,
-      zip2(state.varyingPaths, [...varyingValues])
+      zip2(state.varyingPaths, varyingValues)
     );
 
     newState.varyingValues = varyingValues;
