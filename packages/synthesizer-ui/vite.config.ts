6a679f59c3686fb18c02d0e64fec115bc4896960
wodeni <wn2155@columbia.edu>
2022-04-25 15:17:47 -0400

    fix: better global polyfill

--- packages/synthesizer-ui/vite.config.ts
+++ packages/synthesizer-ui/vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "globalThis",
-  },
   build: {
     rollupOptions: {
       external: ["react", "react-dom"],
