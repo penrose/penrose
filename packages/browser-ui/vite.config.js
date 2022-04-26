4d41b0cf266d65b4e2837209f1479c1a101c948c
Wode "Nimo" Ni <wn2155@columbia.edu>
2022-04-26 16:18:14 -0400

    build: use vite for live-reload mode in `@penrose/panels` and `@penrose/browser-ui` (#929)
    * chore: bump estrella to 1.4.1
    
    * build: use vite for live-watch mode
    
    * build: use vite for live-watch mode in panels
    
    * build: switch to manual polyfill for `global`
    
    * chore: move index.html out of public and add module paths
    
    * build: add prestart script that builds core
    
    * Build examples in prestart
    
    * build: use estrella for panels to avoid the octokit error
    
    * build: polyfill stream
    
    Co-authored-by: Sam Estep <sam@samestep.com>

---
+++ packages/browser-ui/vite.config.js
@@ -0,0 +1,21 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  json: {
+    stringify: true,
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
---
+++ packages/panels/vite.config.js
@@ -0,0 +1,30 @@
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [NodeModulesPolyfillPlugin()],
+    },
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+});
