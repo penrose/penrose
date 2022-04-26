11ca133629029b3ff15067a2e060e2c30677f210
wodeni <wn2155@columbia.edu>
2022-04-26 15:56:25 -0400

    build: polyfill stream

--- packages/panels/vite.config.js
+++ packages/panels/vite.config.js
@@ -1,14 +1,20 @@
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: "window",
+  resolve: {
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+    },
   },
-  json: {
-    stringify: true,
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [NodeModulesPolyfillPlugin()],
+    },
   },
   build: {
     sourcemap: true,
