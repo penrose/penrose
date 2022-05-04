96d2a9c224ef5972a5b36f11b809e6d41d1a61f4
Matt Davis <22134678+cmumatt@users.noreply.github.com>
2022-05-04 15:54:56 -0400

    chore: Additional typescript checks for automator

--- packages/automator/artifacts.ts
+++ packages/automator/artifacts.ts
@@ -60,9 +60,9 @@ export const renderArtifacts = (artifactsDir: string, outDir: string) => {
       delta = 2,
       left = current - delta,
       right = current + delta + 1,
-      range = [],
-      rangeWithDots = [],
-      l;
+      range:number[] = [],
+      rangeWithDots: any[] = [],
+      l:number = -1;
 
     for (let i = 1; i <= last; i++) {
       if (i == 1 || i == last || (i >= left && i < right)) {
@@ -71,7 +71,7 @@ export const renderArtifacts = (artifactsDir: string, outDir: string) => {
     }
 
     for (let i of range) {
-      if (l) {
+      if (l >= 0) {
         if (i - l === 2) {
           rangeWithDots.push(l + 1);
         } else if (i - l !== 1) {
