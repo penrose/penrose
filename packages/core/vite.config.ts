2fea01dd1a0f3a3b21a8ca03e049953ddc65d271
wodeni <wn2155@columbia.edu>
2022-05-12 15:28:17 -0400

    build: use vite to build core

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,27 +1,21 @@
-import react from "@vitejs/plugin-react";
 import path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths()],
   build: {
     sourcemap: true,
     lib: {
-      name: "@penrose/components",
-      formats: ["es"],
+      name: "@penrose/core",
+      formats: ["umd", "cjs", "es"],
       entry: path.resolve(__dirname, "src/index.ts"),
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
-      plugins: [visualizer("dist/stats.html")],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
+      plugins: [visualizer()],
     },
   },
 });
