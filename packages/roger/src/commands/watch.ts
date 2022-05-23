310819b54b720a19ae132582df27e9b7d5a9f8d1
wodeni <wn2155@columbia.edu>
2022-05-23 11:07:18 -0400

    fix: format

--- packages/roger/src/commands/watch.ts
+++ packages/roger/src/commands/watch.ts
@@ -1,18 +1,18 @@
-import {Command, Flags} from '@oclif/core'
-import chokidar from 'chokidar'
-import {promises as fs} from 'node:fs'
-import WebSocket from 'ws'
+import { Command, Flags } from "@oclif/core";
+import chokidar from "chokidar";
+import { promises as fs } from "node:fs";
+import WebSocket from "ws";
 
 export default class Watch extends Command {
   static description =
-    'Watch the current folder for files & changes (must end in .sub,.sty,.dsl)';
+    "Watch the current folder for files & changes (must end in .sub,.sty,.dsl)";
 
-  static examples = ['<%= config.bin %> <%= command.id %>'];
+  static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
     port: Flags.integer({
-      char: 'p',
-      description: 'websocket port to serve to frontend',
+      char: "p",
+      description: "websocket port to serve to frontend",
       default: 9160,
     }),
   };
@@ -32,89 +32,89 @@ export default class Watch extends Command {
   broadcastFiles() {
     if (this.wss) {
       for (const ws of this.wss.clients) {
-        ws.send(JSON.stringify({kind: 'files', files: this.files}))
+        ws.send(JSON.stringify({ kind: "files", files: this.files }));
       }
     } else {
-      console.warn('Websocket server not defined')
+      console.warn("Websocket server not defined");
     }
   }
 
   async broadcastFileChange(fileName: string) {
     try {
-      const contents = await fs.readFile(fileName, 'utf8')
+      const contents = await fs.readFile(fileName, "utf8");
       if (this.wss) {
         for (const ws of this.wss.clients) {
-          ws.send(JSON.stringify({kind: 'file_change', fileName, contents}))
+          ws.send(JSON.stringify({ kind: "file_change", fileName, contents }));
         }
       } else {
-        console.warn('Websocket server not defined')
+        console.warn("Websocket server not defined");
       }
     } catch (error) {
-      console.warn(error)
+      console.warn(error);
     }
   }
 
   public async run(): Promise<void> {
-    const {flags} = await this.parse(Watch)
-    this.wss = new WebSocket.Server({port: flags.port})
-    console.log(`watching on port ${flags.port}`)
-    this.wss.on('connection', ws => {
-      console.info('client connected')
-      this.broadcastFiles()
-      ws.on('message', async data => {
-        const parsed = JSON.parse(data.toString())
+    const { flags } = await this.parse(Watch);
+    this.wss = new WebSocket.Server({ port: flags.port });
+    console.log(`watching on port ${flags.port}`);
+    this.wss.on("connection", (ws) => {
+      console.info("client connected");
+      this.broadcastFiles();
+      ws.on("message", async (data) => {
+        const parsed = JSON.parse(data.toString());
         switch (parsed.kind) {
-        case 'retrieve_file':
-          this.broadcastFileChange(parsed.fileName)
-          break
-        default:
-          console.error(`unknown message kind: ${parsed.kind}`)
+          case "retrieve_file":
+            this.broadcastFileChange(parsed.fileName);
+            break;
+          default:
+            console.error(`unknown message kind: ${parsed.kind}`);
         }
-      })
-      ws.on('close', () => {
-        console.info('client disconnected')
-      })
-    })
+      });
+      ws.on("close", () => {
+        console.info("client disconnected");
+      });
+    });
 
-    const watcher = chokidar.watch('.', {persistent: true})
-    watcher.on('add', p => {
-      switch (p.split('.').pop()) {
-      case 'sub':
-        this.files.substance.push(p)
-        break
-      case 'sty':
-        this.files.style.push(p)
-        break
-      case 'dsl':
-        this.files.domain.push(p)
-        break
+    const watcher = chokidar.watch(".", { persistent: true });
+    watcher.on("add", (p) => {
+      switch (p.split(".").pop()) {
+        case "sub":
+          this.files.substance.push(p);
+          break;
+        case "sty":
+          this.files.style.push(p);
+          break;
+        case "dsl":
+          this.files.domain.push(p);
+          break;
       }
 
-      this.broadcastFiles()
-    })
-    watcher.on('error', err => {
-      console.error(err)
-    })
-    watcher.on('change', async p => {
-      if (['sub', 'sty', 'dsl'].includes(p.split('.').pop() ?? '')) {
-        console.info(`file ${p} changed`)
-        this.broadcastFileChange(p)
+      this.broadcastFiles();
+    });
+    watcher.on("error", (err) => {
+      console.error(err);
+    });
+    watcher.on("change", async (p) => {
+      if (["sub", "sty", "dsl"].includes(p.split(".").pop() ?? "")) {
+        console.info(`file ${p} changed`);
+        this.broadcastFileChange(p);
       }
-    })
-    watcher.on('unlink', p => {
-      switch (p.split('.').pop()) {
-      case 'sub':
-        this.files.substance = this.files.substance.filter(f => f !== p)
-        break
-      case 'sty':
-        this.files.style = this.files.style.filter(f => f !== p)
-        break
-      case 'dsl':
-        this.files.domain = this.files.domain.filter(f => f !== p)
-        break
+    });
+    watcher.on("unlink", (p) => {
+      switch (p.split(".").pop()) {
+        case "sub":
+          this.files.substance = this.files.substance.filter((f) => f !== p);
+          break;
+        case "sty":
+          this.files.style = this.files.style.filter((f) => f !== p);
+          break;
+        case "dsl":
+          this.files.domain = this.files.domain.filter((f) => f !== p);
+          break;
       }
 
-      this.broadcastFiles()
-    })
+      this.broadcastFiles();
+    });
   }
 }
