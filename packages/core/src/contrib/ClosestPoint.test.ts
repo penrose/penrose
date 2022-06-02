9cb77d80b334515df7084a9ac682216811a0df85
rhit-chois3 <chois3@rose-hulman.edu>
2022-06-02 15:49:49 -0400

    rebuilt dependencies

--- packages/core/src/contrib/ClosestPoint.test.ts
+++ packages/core/src/contrib/ClosestPoint.test.ts
@@ -1,21 +1,23 @@
-import { genCode, makeADInputVars, secondaryGraph } from "engine/Autodiff";
+import { genCode, input, secondaryGraph } from "engine/Autodiff";
 import seedrandom from "seedrandom";
 import { makeCircle } from "shapes/Circle";
-import { floatV, makeCanvas, sampleBlack, vectorV } from "shapes/Samplers";
+import { Context, InputFactory, makeCanvas } from "shapes/Samplers";
 import * as ad from "types/ad";
 import { Shape } from "types/shapes";
 import { VectorV } from "types/value";
+import { black, floatV, vectorV } from "utils/Util";
 import { compDict } from "./Functions";
 
 const canvas = makeCanvas(800, 700);
 
 const compareClosestPoint = (
+  context: Context,
   shapeType: string,
   shape: Shape,
   p: ad.Input[],
   expected: [number, number]
 ) => {
-  const result = getResult(shapeType, shape, p);
+  const result = getResult(context, shapeType, shape, p);
   console.log(
     `result contents 0: ${JSON.stringify(result.contents[0], null, 2)}`
   );
@@ -39,21 +41,12 @@ const compareClosestPoint = (
 };
 
 const getResult = (
+  context: Context,
   shapeType: string,
   s: any,
   p: ad.Input[]
 ): VectorV<ad.Num> => {
-  const result = compDict.closestPoint(
-    { rng: seedrandom("shape") },
-    [shapeType, s],
-    p
-  );
-  //   console.log(
-  //     `closestPoint(${seedrandom("shape")},[${shapeType},${JSON.stringify(
-  //       s
-  //     )}],${p})
-  //   ) = ${JSON.stringify(result, null, 2)}`
-  //   );
+  const result = compDict.closestPoint(context, [shapeType, s], p);
   return result;
 };
 export const testCircle = (
@@ -63,14 +56,14 @@ export const testCircle = (
   pt: number[],
   expected: [number, number]
 ) => {
-  const seed = seedrandom("bbox Rectangle");
-  const shape = makeCircle(seed, canvas, {
+  const { context, p } = makeContext(pt);
+  const shape = makeCircle(context, canvas, {
     center: vectorV(center),
     r: floatV(radius),
     strokeWidth: floatV(strokeWidth),
-    strokeColor: sampleBlack(),
+    strokeColor: black(),
   });
-  compareClosestPoint("Circle", shape, makeADInputVars(pt), expected);
+  compareClosestPoint(context, "Circle", shape, p, expected);
 };
 
 describe("closest point", () => {
@@ -82,3 +75,19 @@ describe("closest point", () => {
     // testCircle([0, 0], 3, 0, [-5, 0], [-3, 0]);
   });
 });
+const makeContext = (pt: number[]): { context: Context; p: ad.Input[] } => {
+  const rng = seedrandom("sdf");
+  const inputs: ad.Input[] = [];
+  const makeInput: InputFactory = (meta) => {
+    const x = input({
+      key: inputs.length,
+      val: "pending" in meta ? meta.pending : meta.sampler(rng),
+    });
+    inputs.push(x);
+    return x;
+  };
+  for (const coord of pt) {
+    makeInput({ sampler: () => coord });
+  }
+  return { context: { makeInput }, p: [...inputs] };
+};
