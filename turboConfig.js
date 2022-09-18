3ad6a0267e892aff5734fcef2221cd8667e3e969
Sam Estep <sam@samestep.com>
2022-09-18 11:40:23 -0400

    build: Skip missing package.json in turboConfig.js

--- turboConfig.js
+++ turboConfig.js
@@ -15,22 +15,21 @@ const turboJSON = {
 };
 const packagesDir = "packages";
 for (const package of fs.readdirSync(packagesDir)) {
-  const { name, turbo } = JSON.parse(
-    fs.readFileSync(path.join(packagesDir, package, "package.json")).toString()
-  );
-  if (turbo) {
-    for (const [script, unparsed] of Object.entries(turbo)) {
-      const config = yaml.load(`{${unparsed}}`);
-      const entry = {
-        dependsOn: [
-          ...(basePipeline[script]?.dependsOn ?? []),
-          ...(config.deps ?? []),
-        ],
-        outputs: config.out ?? [],
-      };
-      if ("cache" in config) entry.cache = config.cache;
-      turboJSON.pipeline[`${name}#${script}`] = entry;
-    }
+  const p = path.join(packagesDir, package, "package.json");
+  if (!fs.existsSync(p)) continue;
+  const { name, turbo } = JSON.parse(fs.readFileSync(p).toString());
+  if (!turbo) continue;
+  for (const [script, unparsed] of Object.entries(turbo)) {
+    const config = yaml.load(`{${unparsed}}`);
+    const entry = {
+      dependsOn: [
+        ...(basePipeline[script]?.dependsOn ?? []),
+        ...(config.deps ?? []),
+      ],
+      outputs: config.out ?? [],
+    };
+    if ("cache" in config) entry.cache = config.cache;
+    turboJSON.pipeline[`${name}#${script}`] = entry;
   }
 }
 fs.writeFileSync("turbo.json", JSON.stringify(turboJSON, null, 2));
