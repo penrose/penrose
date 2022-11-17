84d96ff27a1129ba52a6d24125c5d24e71fe6b8d
wodeni <wn2155@columbia.edu>
2022-11-17 17:06:38 -0500

    chore: resolve relative paths

--- packages/synthesizer-ui/vite.config.ts
+++ packages/synthesizer-ui/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@penrose/core", "@penrose/components"],
