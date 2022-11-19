d57fb0548d594871f69e708df9c531490e4f682f
Sam Estep <sam@samestep.com>
2022-11-19 09:26:08 -0500

    Move makeGraph changes to PR 1150

--- packages/core/src/engine/Autodiff.ts
+++ packages/core/src/engine/Autodiff.ts
@@ -564,11 +564,25 @@ export const makeGraph = (
     );
   }
 
-  // we get the IDs for the input gradients before adding all the secondary
-  // nodes, because some of the inputs may only be reachable from the secondary
-  // outputs instead of the primary output; really, the gradients for all those
-  // inputs are just zero, so the caller needs to substitute zero whenever the
-  // gradient is missing a key
+  // easiest case: final stage, just add all the nodes and edges for the
+  // secondary outputs
+  const secondary = outputs.secondary.map(addNode);
+  while (!queue.isEmpty()) {
+    addNode(queue.dequeue());
+  }
+  while (!edges.isEmpty()) {
+    const [{ child, name }, parent] = edges.dequeue();
+    addEdge(child, parent, name);
+  }
+
+  // we wait until after adding all the nodes before get the IDs for the input
+  // gradients, because some of the inputs may only be reachable from the
+  // secondary outputs instead of the primary output; this isn't really
+  // necessary, because the gradients for all those inputs are just zero, so the
+  // caller could just substitute zero whenever the gradient is missing a key,
+  // but it's probably a bit less surprising if we always include an array
+  // element for the gradient on any input that is reachable even from the
+  // secondary outputs
   const gradient: ad.Id[] = [];
   for (const {
     id,
@@ -580,18 +594,8 @@ export const makeGraph = (
     // note that it's very easy for the set of Input indices to not be
     // contiguous, e.g. if some inputs end up not being used in any of the
     // computations in the graph; but even if that happens, it's actually OK
-    gradient[key] = safe(gradNodes.get(id), "missing gradient")[0];
-  }
-
-  // easiest case: final stage, just add all the nodes and edges for the
-  // secondary outputs
-  const secondary = outputs.secondary.map(addNode);
-  while (!queue.isEmpty()) {
-    addNode(queue.dequeue());
-  }
-  while (!edges.isEmpty()) {
-    const [{ child, name }, parent] = edges.dequeue();
-    addEdge(child, parent, name);
+    // (see the comment in the implementation of genCode below)
+    gradient[key] = (gradNodes.get(id) ?? [addNode(0)])[0];
   }
 
   return { graph, nodes, gradient, primary, secondary };
