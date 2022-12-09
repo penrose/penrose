ab27867d3f45888d19572486cc28ed6ef84652f9
Sam Estep <sam@samestep.com>
2022-12-08 23:37:27 -0500

    Try to install Rust and wasm-bindgen in Cloudflare

--- cloudflare.sh
+++ cloudflare.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
+curl https://sh.rustup.rs -sSf | sh -s -- -yt wasm32-unknown-unknown
+source "$HOME/.cargo/env"
+wget -P ~/.cargo/bin \
+  https://gist.github.com/samestep/2cf703cfd81691f2cb3c23422fce7e56/raw/f27692e7269f798faaf259b9c75942b151d8b69b/wasm-bindgen
+chmod +x ~/.cargo/bin/wasm-bindgen
 yarn build:docs-site
