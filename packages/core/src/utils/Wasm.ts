5dcf9c3a44fe01b9aa12ab8e9ae23d6c05db1e64
Sam Estep <sam@samestep.com>
2022-12-18 12:54:56 -0500

    Document utils/Wasm

--- packages/core/src/utils/Wasm.ts
+++ packages/core/src/utils/Wasm.ts
@@ -1,7 +1,8 @@
 // https://webassembly.github.io/spec/core/binary/
 
-const PREAMBLE_SIZE = 8;
+//#region constants for the binary format
 
+/** Section ids. */
 export const SECTION = {
   TYPE: 1,
   IMPORT: 2,
@@ -10,14 +11,19 @@ export const SECTION = {
   CODE: 10,
 };
 
+/** `importdesc` leading bytes. */
 export const IMPORT = { FUNCTION: 0x00, MEMORY: 0x02 };
 
+/** `exportdesc` leading bytes. */
 export const EXPORT = { FUNCTION: 0x00 };
 
+/** Type bytes. */
 export const TYPE = { FUNCTION: 0x60, f64: 0x7c, i32: 0x7f };
 
+/** `limits` leading bytes. */
 export const LIMITS = { NO_MAXIMUM: 0x00 };
 
+/** Instruction opcodes. */
 export const OP = {
   call: 0x10,
   drop: 0x1a,
@@ -50,12 +56,31 @@ export const OP = {
   local: { get: 0x20, set: 0x21, tee: 0x22 },
 };
 
+/** `end` opcode. */
 export const END = 0x0b;
 
+//#endregion
+
+//#region helper types, functions, and classes
+
+/**
+ * A mutable object to which individual bytes can be written. Exported only for
+ * testing purposes; prefer `Target`.
+ */
 export interface Byter {
+  /**
+   * Write a single byte.
+   * @param b the byte
+   */
   byte(b: number): void;
 }
 
+/**
+ * Writes a 32-bit signed integer to a target. Exported only for testing
+ * purposes; prefer `Count`, `intSize`, or `Module` depending on your use case.
+ * @param t target
+ * @param n integer to write
+ */
 export const int = (t: Byter, n: number): void => {
   if (!(Number.isInteger(n) && -0x80000001 < n && n < 0x80000000))
     throw new Error(`cannot represent ${n} as a 32-bit signed integer`);
@@ -66,12 +91,34 @@ export const int = (t: Byter, n: number): void => {
   t.byte(n & 0x7f);
 };
 
+/**
+ * A mutable object to which bytes, 32-bit signed integers, 64-bit
+ * floating-point numbers, and ASCII strings can be written.
+ */
 export interface Target extends Byter {
+  /**
+   * Write a LEB128-encoded 32-bit signed integer.
+   * @param n the integer
+   */
   int(n: number): void;
+  /**
+   * Write an IEEE 754-2019 little-endian 64-bit floating-point number.
+   * @param x the number
+   */
   f64(x: number): void;
+  /**
+   * Write an ASCII string as a vector of bytes.
+   * @param s the string
+   */
   ascii(s: string): void;
 }
 
+/**
+ * A `Target` that doesn't actually put bytes anywhere, and instead just counts
+ * how many bytes would be written. Useful for calculating the size of a
+ * `Module` before allocating one big buffer to store its bytes, because
+ * JavaScript typed arrays cannot be resized after construction.
+ */
 export class Count implements Target {
   size = 0;
 
@@ -93,18 +140,34 @@ export class Count implements Target {
   }
 }
 
+/**
+ * @param n integer to encode
+ * @returns number of bytes to encode as signed LEB128
+ */
 export const intSize = (n: number): number => {
   const count = new Count();
   count.int(n);
   return count.size;
 };
 
+const PREAMBLE_SIZE = 8;
+
+/**
+ * A `Target` which preallocates a byte buffer of known size (perhaps computed
+ * via `Count`) with the standard 8-byte preamble, and writes data to it,
+ * incrementing an internal byte index as it goes.
+ */
 export class Module implements Target {
   bytes: Uint8Array;
   count: Count;
   floatIn: Float64Array;
   floatOut: Uint8Array;
 
+  /**
+   * Allocates a byte buffer for a Wasm module, automatically adding the
+   * standard 8 bytes for the preamble.
+   * @param sumSectionSizes number of bytes in the module, minus 8 for preamble
+   */
   constructor(sumSectionSizes: number) {
     this.bytes = new Uint8Array(PREAMBLE_SIZE + sumSectionSizes);
     this.count = new Count();
@@ -136,6 +199,10 @@ export class Module implements Target {
   }
 
   f64(x: number): void {
+    // https://stackoverflow.com/a/7870190 this assumes that the underlying
+    // hardware is little-endian, and will break if that assumption is violated;
+    // it may be wiser to check the endianness and then flip the order of these
+    // bytes, but the downside is there isn't an easy way to test that
     this.floatIn[0] = x;
     this.bytes.set(this.floatOut, this.count.size);
     this.count.f64();
@@ -151,3 +218,5 @@ export class Module implements Target {
     }
   }
 }
+
+//#endregion
