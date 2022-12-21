03fe301c66347388ec5b4a740eff277f5817b04e
Sam Estep <sam@samestep.com>
2022-12-20 23:13:36 -0500

    Use KaTeX

--- packages/docs-site/.vitepress/config.ts
+++ packages/docs-site/.vitepress/config.ts
@@ -1,4 +1,94 @@
+import markdownItKatex from "markdown-it-katex";
 import { defineConfig } from "vitepress";
+
+// https://github.com/vuejs/vitepress/issues/529#issuecomment-1151186631
+const customElements = [
+  "math",
+  "maction",
+  "maligngroup",
+  "malignmark",
+  "menclose",
+  "merror",
+  "mfenced",
+  "mfrac",
+  "mi",
+  "mlongdiv",
+  "mmultiscripts",
+  "mn",
+  "mo",
+  "mover",
+  "mpadded",
+  "mphantom",
+  "mroot",
+  "mrow",
+  "ms",
+  "mscarries",
+  "mscarry",
+  "mscarries",
+  "msgroup",
+  "mstack",
+  "mlongdiv",
+  "msline",
+  "mstack",
+  "mspace",
+  "msqrt",
+  "msrow",
+  "mstack",
+  "mstack",
+  "mstyle",
+  "msub",
+  "msup",
+  "msubsup",
+  "mtable",
+  "mtd",
+  "mtext",
+  "mtr",
+  "munder",
+  "munderover",
+  "semantics",
+  "math",
+  "mi",
+  "mn",
+  "mo",
+  "ms",
+  "mspace",
+  "mtext",
+  "menclose",
+  "merror",
+  "mfenced",
+  "mfrac",
+  "mpadded",
+  "mphantom",
+  "mroot",
+  "mrow",
+  "msqrt",
+  "mstyle",
+  "mmultiscripts",
+  "mover",
+  "mprescripts",
+  "msub",
+  "msubsup",
+  "msup",
+  "munder",
+  "munderover",
+  "none",
+  "maligngroup",
+  "malignmark",
+  "mtable",
+  "mtd",
+  "mtr",
+  "mlongdiv",
+  "mscarries",
+  "mscarry",
+  "msgroup",
+  "msline",
+  "msrow",
+  "mstack",
+  "maction",
+  "semantics",
+  "annotation",
+  "annotation-xml",
+];
 
 // https://github.com/vuejs/vitepress/issues/1685#issuecomment-1345542895
 const root = "https://penrose.cs.cmu.edu";
@@ -12,7 +102,30 @@ export default defineConfig({
   ignoreDeadLinks: true,
   outDir: "build",
 
-  head: [["link", { rel: "icon", href: "/img/logo.svg" }]],
+  head: [
+    ["link", { rel: "icon", href: "/img/logo.svg" }],
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href:
+          "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css",
+      },
+    ],
+  ],
+
+  markdown: {
+    config: (md) => {
+      md.use(markdownItKatex);
+    },
+  },
+  vue: {
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => customElements.includes(tag),
+      },
+    },
+  },
 
   themeConfig: {
     logo: "img/favicon.ico",
