9ea7eb60f00b6df69a2ce240958c998a33934bfc
Sam Estep <sam@samestep.com>
2023-01-09 13:43:54 -0500

    Undo Jest config change

--- packages/core/jest.config.cjs
+++ packages/core/jest.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   modulePaths: ["node_modules", "<rootDir>/src/"],
-  testPathIgnorePatterns: ["dist/"],
+  testPathIgnorePatterns: ["build/dist/"],
   extensionsToTreatAsEsm: [".ts"],
   globals: { "ts-jest": { useESM: true } },
   setupFiles: ["./setupJestEnv.js"], // https://stackoverflow.com/a/57943686
