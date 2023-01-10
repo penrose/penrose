757963ed0d57dbcbbdf8c3cb279733088a38602c
jiri.minarcik <jiri.minarcik@resistant.ai>
2023-01-10 20:33:13 +0100

    feat: implement isLocallyConvex

--- packages/core/src/contrib/CurveConstraints.ts
+++ packages/core/src/contrib/CurveConstraints.ts
@@ -1,5 +1,15 @@
 import { ops } from "engine/Autodiff";
-import { add, addN, div, squared, sub } from "engine/AutodiffFunctions";
+import {
+  add,
+  addN,
+  div,
+  ifCond,
+  lte,
+  mul,
+  sign,
+  squared,
+  sub,
+} from "engine/AutodiffFunctions";
 import { Path } from "shapes/Path";
 import { Polygon } from "shapes/Polygon";
 import { Polyline } from "shapes/Polyline";
@@ -18,7 +28,17 @@ export const constrDictCurves = {
   },
 
   isLocallyConvex: ([t, s]: [string, Polyline | Polygon | Path]): ad.Num => {
-    throw new Error("Function not implemented."); // TODO
+    const points = extractPoints([t, s]);
+    const triples = consecutiveNTuples(points, 3, isClosed([t, s]));
+    const angles = triples.map(([p1, p2, p3]: [ad.Num, ad.Num][]) =>
+      ops.angleFrom(ops.vsub(p2, p1), ops.vsub(p3, p2))
+    );
+    const meanSign = sign(addN(angles));
+    return addN(
+      angles.map((angle: ad.Num) =>
+        ifCond(lte(mul(meanSign, angle), 0), 0, angle)
+      )
+    );
   },
 
   isConvex: ([t, s]: [string, Polyline | Polygon | Path]): ad.Num => {
