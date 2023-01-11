aaf593181ad186baa68a00ee7e4d098f353f3137
wodeni <wn2155@columbia.edu>
2023-01-11 11:45:31 -0500

    fix: #1194 don't bundle components

--- packages/synthesizer-ui/vite.config.ts
+++ packages/synthesizer-ui/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: "classic" })],
   optimizeDeps: {
     exclude: ["@penrose/core", "@penrose/components"],
   },
