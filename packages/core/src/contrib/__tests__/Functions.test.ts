cb7da69274bf201d46087bb1c19e01776f451e39
jiri.minarcik <jiri.minarcik@resistant.ai>
2023-01-12 06:51:44 +0100

    chore: test values

--- packages/core/src/contrib/__tests__/Functions.test.ts
+++ packages/core/src/contrib/__tests__/Functions.test.ts
@@ -55,16 +55,20 @@ describe("curvature", () => {
 
 describe("totalCurvature", () => {
   it.each([
-    ["Polyline", _polylines[666], 666],
-    ["Polygon", _polygons[666], 666],
-    ["Path", _closed_paths[666], 666],
-    ["Path", _open_paths[666], 666],
+    ["Polygon", _polygons[0], 2 * Math.PI],
+    ["Path", _closed_paths[1], 2 * Math.PI],
+    ["Polygon", _polygons[7], -2 * Math.PI],
+    ["Path", _closed_paths[7], -2 * Math.PI],
+    ["Polygon", _polygons[8], 4 * Math.PI],
+    ["Path", _closed_paths[8], 4 * Math.PI],
+    ["Polyline", _polylines[10], Math.PI / 2],
+    ["Path", _open_paths[7], (-3 * Math.PI) / 2],
   ])("of %p", (shapeType: string, shapeData: any, expected: number) => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = totalCurvature(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = totalCurvature(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
 
@@ -78,67 +82,77 @@ describe("elasticEnergy", () => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = elasticEnergy(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = elasticEnergy(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
 
 describe("isoperimetricRatio", () => {
   it.each([
-    ["Polyline", _polylines[666], 666],
-    ["Polygon", _polygons[666], 666],
-    ["Path", _closed_paths[666], 666],
-    ["Path", _open_paths[666], 666],
+    ["Polygon", _polygons[6], 16],
+    ["Path", _closed_paths[6], 16],
+    ["Polygon", _polygons[7], 18],
+    ["Path", _closed_paths[7], 18],
+    ["Polygon", _polygons[8], 256 / 7],
+    ["Path", _closed_paths[8], 256 / 7],
   ])("of %p", (shapeType: string, shapeData: any, expected: number) => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = isoperimetricRatio(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = isoperimetricRatio(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
 
 describe("perimeter", () => {
   it.each([
-    ["Polyline", _polylines[666], 666],
-    ["Polygon", _polygons[666], 666],
-    ["Path", _closed_paths[666], 666],
-    ["Path", _open_paths[666], 666],
+    ["Polygon", _polygons[6], 400],
+    ["Path", _closed_paths[6], 400],
+    ["Polygon", _polygons[7], 600],
+    ["Path", _closed_paths[7], 600],
+    ["Polygon", _polygons[8], 4 * 300 + 400],
+    ["Path", _closed_paths[8], 4 * 300 + 400],
+    ["Polyline", _polylines[10], 300],
+    ["Path", _open_paths[10], 300],
   ])("of %p", (shapeType: string, shapeData: any, expected: number) => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = perimeter(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = perimeter(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
 
 describe("turningNumber", () => {
   it.each([
-    ["Polyline", _polylines[666], 666],
-    ["Polygon", _polygons[666], 666],
-    ["Path", _closed_paths[666], 666],
-    ["Path", _open_paths[666], 666],
+    ["Polygon", _polygons[0], 1],
+    ["Path", _closed_paths[1], 1],
+    ["Polygon", _polygons[7], -1],
+    ["Path", _closed_paths[7], -1],
+    ["Polygon", _polygons[8], 2],
+    ["Path", _closed_paths[8], 2],
   ])("of %p", (shapeType: string, shapeData: any, expected: number) => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = turningNumber(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = turningNumber(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
 
 describe("algebraicArea", () => {
   it.each([
-    ["Polyline", _polylines[666], 666],
-    ["Polygon", _polygons[666], 666],
-    ["Path", _closed_paths[666], 666],
-    ["Path", _open_paths[666], 666],
+    ["Polygon", _polygons[6], 100 * 100],
+    ["Path", _closed_paths[6], 100 * 100],
+    ["Polygon", _polygons[7], 200 * 100],
+    ["Path", _closed_paths[7], 200 * 100],
+    ["Polygon", _polygons[8], 300 * 300 - 2 * 100 * 100],
+    ["Path", _closed_paths[8], 300 * 300 - 2 * 100 * 100],
   ])("of %p", (shapeType: string, shapeData: any, expected: number) => {
     const shape: [string, any] = [shapeType, shapeData];
     const points: [ad.Num, ad.Num][] = extractPoints(shape);
     const closed: boolean = isClosed(shape);
-    const result = algebraicArea(shapeData, closed);
-    expect(numOf(result)).toStrictEqual(expected);
+    const result = algebraicArea(points, closed);
+    expect(numOf(result)).toBeCloseTo(expected, 4);
   });
 });
