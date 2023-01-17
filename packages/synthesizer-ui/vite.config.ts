08bb5fb6204caf434ab8213b62c91876a7504974
Wode "Nimo" Ni <wn2155@columbia.edu>
2023-01-17 15:30:55 -0500

    build: don't bundle `components` (#1225)
    Co-authored-by: Sam Estep <sam@samestep.com>

--- packages/synthesizer-ui/vite.config.ts
+++ packages/synthesizer-ui/vite.config.ts
@@ -1,12 +1,9 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["@penrose/core", "@penrose/components"],
-  },
+  plugins: [react({ jsxRuntime: "classic" })],
   build: { target: "esnext" },
 });
