6af731918e78bb4dfa0ec98a7b95c2ae188246e6
wodeni <wn2155@columbia.edu>
2023-01-17 14:48:08 -0500

    build: bump vite and plugin-react

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
--- packages/synthesizer-ui/vite.config.ts
+++ packages/synthesizer-ui/vite.config.ts
@@ -1,12 +1,9 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react({ jsxRuntime: "classic" })],
-  optimizeDeps: {
-    exclude: ["@penrose/core", "@penrose/components"],
-  },
   build: { target: "esnext" },
 });
