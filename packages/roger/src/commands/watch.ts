fe0a2f0a50e71bf6b826e62a9d7fdd498ce33d92
Sam Estep <sam@samestep.com>
2023-01-30 21:24:37 -0500

    Don't breaking-change Roger

--- packages/roger/src/commands/watch.ts
+++ packages/roger/src/commands/watch.ts
@@ -6,7 +6,7 @@ import WebSocket from "ws";
 
 export default class Watch extends Command {
   static description =
-    "Watch the current folder for files & changes (must end in .substance,.style,.domain)";
+    "Watch the current folder for files & changes (must end in .sub,.substance,.sty,.style,.dsl,.domain)";
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
@@ -88,12 +88,15 @@ export default class Watch extends Command {
     const watcher = chokidar.watch(".", { persistent: true });
     watcher.on("add", (p) => {
       switch (p.split(".").pop()) {
+        case "sub":
         case "substance":
           this.files.substance.push(p);
           break;
+        case "sty":
         case "style":
           this.files.style.push(p);
           break;
+        case "dsl":
         case "domain":
           this.files.domain.push(p);
           break;
@@ -105,19 +108,26 @@ export default class Watch extends Command {
       console.error(err);
     });
     watcher.on("change", async (p) => {
-      if (["substance", "style", "domain"].includes(p.split(".").pop() ?? "")) {
+      if (
+        ["sub", "substance", "sty", "style", "dsl", "domain"].includes(
+          p.split(".").pop() ?? ""
+        )
+      ) {
         console.info(`file ${p} changed`);
         this.broadcastFileChange(p);
       }
     });
     watcher.on("unlink", (p) => {
       switch (p.split(".").pop()) {
+        case "sub":
         case "substance":
           this.files.substance = this.files.substance.filter((f) => f !== p);
           break;
+        case "sty":
         case "style":
           this.files.style = this.files.style.filter((f) => f !== p);
           break;
+        case "dsl":
         case "domain":
           this.files.domain = this.files.domain.filter((f) => f !== p);
           break;
