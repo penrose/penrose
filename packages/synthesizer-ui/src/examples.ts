2f1f624118d2c48433c6f888075c8b279ff3e387
Wode "Nimo" Ni <wn2155@columbia.edu>
2023-03-10 16:35:59 -0500

    chore: add Lewis structures examples to `synthesizer-ui` (#1334)

--- packages/synthesizer-ui/src/examples.ts
+++ packages/synthesizer-ui/src/examples.ts
@@ -1,5 +1,6 @@
 import { SynthesizerSetting } from "@penrose/core";
 import geometryDomain from "@penrose/examples/dist/geometry-domain";
+import molecules from "@penrose/examples/dist/molecules";
 
 export interface Preset {
   displayName: string;
@@ -13,7 +14,7 @@ interface PresetCollection {
   [s: string]: Preset;
 }
 
-const defaultParams: SynthesizerSetting = {
+const geometryParams: SynthesizerSetting = {
   mutationCount: [1, 4],
   argOption: "existing",
   argReuse: "distinct",
@@ -48,15 +49,51 @@ const defaultParams: SynthesizerSetting = {
   },
 };
 
+const lewisParams: SynthesizerSetting = {
+  mutationCount: [1, 4],
+  argOption: "existing",
+  argReuse: "distinct",
+  weights: {
+    type: 0.1,
+    predicate: 0.3,
+    constructor: 0.3,
+  },
+  opWeights: {
+    add: 0.01,
+    delete: 0.01,
+    edit: 0.8,
+  },
+  // TODO: need weights for the three ops
+  add: {
+    type: "*",
+    function: "*",
+    constructor: "*",
+    predicate: "*",
+  },
+  delete: {
+    type: "*",
+    function: "*",
+    constructor: "*",
+    predicate: "*",
+  },
+  edit: {
+    type: "*",
+    function: "*",
+    constructor: "*",
+    predicate: "*",
+  },
+};
+
 export const presets: PresetCollection = {
   c01p01: {
     displayName: "c01p01: Collinear Points",
-    prompt: "In which of the following diagrams are points B, D, E collinear?",
+    prompt:
+      "In which of the following diagrams are points $B$, $D$, $E$ collinear?",
     substance: geometryDomain.textbook_problems["c01p01.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"],
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c01p10: {
@@ -67,13 +104,13 @@ export const presets: PresetCollection = {
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"],
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c02p01: {
     displayName: "c02p01: Angle bisector",
     prompt:
-      "In which of the following diagrams is the statement, <ADC=2(m<ADB)?",
+      "In which of the following diagrams is the statement $\\angle ADC=2(m \\angle ADB)$ true?",
     substance: geometryDomain.textbook_problems["c02p01.substance"],
     domain: geometryDomain["geometry.domain"],
     style:
@@ -84,13 +121,13 @@ export const presets: PresetCollection = {
   }
     `,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c03p01: {
     displayName: "c03p01: Alternate interior angles",
     prompt:
-      "Which diagram illustrates <JKM and <KML as alternate interior angles?",
+      "Which diagram illustrates $\\angle JKM$ and $\\angle KML$ as alternate interior angles?",
     substance: geometryDomain.textbook_problems["c03p01.substance"],
     domain: geometryDomain["geometry.domain"],
     style:
@@ -103,13 +140,13 @@ where a := InteriorAngle(p, q, r) {
 }
     `,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c04p01: {
     displayName: "c04p01: Congruent triangles",
     prompt:
-      "In which of the following diagrams are triangles tDEC an tDEA congruent?",
+      "In which of the following diagrams are triangles $\\triangle DEC$ an $\\triangle DEA$ congruent?",
     substance: geometryDomain.textbook_problems["c04p01.substance"],
     domain: geometryDomain["geometry.domain"],
     style:
@@ -129,69 +166,71 @@ forall Point \`D\`, \`E\`, \`A\` {
 }
     `,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c04p12: {
     displayName: "c04p12: Complementary angles",
     prompt:
-      "In which of these diagrams, can you find the value of <BCE given the value of x?",
+      "In which of these diagrams, can you find the value of $\\angle BCE$ given the value of $x$?",
     substance: geometryDomain.textbook_problems["c04p12.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c05p01: {
     displayName: "c05p01: Mid-segement",
-    prompt: "Which diagram shows that HK is a midsegment of tGJF?",
+    prompt:
+      "Which diagram shows that $HK$ is a midsegment of $\\triangle GJF$?",
     substance: geometryDomain.textbook_problems["c05p01.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c05p13: {
     displayName: "c05p13: Incenter",
-    prompt: "Which diagram shows P as the incenter of tJKL?",
+    prompt: "Which diagram shows $P$ as the incenter of $\\triangle JKL$?",
     substance: geometryDomain.textbook_problems["c05p13.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c06p06: {
     displayName: "c06p06: Parallelogram",
     prompt:
-      "Which of the following diagrams shows that JKLM is a parallelogram?",
+      "Which of the following diagrams shows that $JKLM$ is a parallelogram?",
     substance: geometryDomain.textbook_problems["c06p06.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c07p06: {
     displayName: "c07p06: Parallelogram 2",
-    prompt: "In which of the following diagrams is ABCD a parallelogram?",
+    prompt: "In which of the following diagrams is $ABCD$ a parallelogram?",
     substance: geometryDomain.textbook_problems["c07p06.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c07p10: {
     displayName: "c07p10: Orthocenter",
-    prompt: "In which of the following diagrams is G the orthocenter of tFGH?",
+    prompt:
+      "In which of the following diagrams is $G$ the orthocenter of $\\triangle FGH$?",
     substance: geometryDomain.textbook_problems["c07p10.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c07p22: {
@@ -201,17 +240,18 @@ forall Point \`D\`, \`E\`, \`A\` {
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c08p08: {
     displayName: "c08p08: Nested Right Angle Triangles",
-    prompt: "In which of the following diagrams is PQR similar to TSR?",
+    prompt:
+      "In which of the following diagrams is $\\triangle PQR$ similar to $\\triangle TSR$?",
     substance: geometryDomain.textbook_problems["c08p08.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   // NOTE: deprecated due to lack of conceptual prompt
@@ -239,23 +279,24 @@ forall Point \`D\`, \`E\`, \`A\` {
   // },
   c10p08: {
     displayName: "c10p08: Triangle Congruency",
-    prompt: "In which of the following diagrams is tDEF congruent to tABC?",
+    prompt:
+      "In which of the following diagrams is $\\triangle DEF$ congruent to $\\triangle ABC$?",
     substance: geometryDomain.textbook_problems["c10p08.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c11p07: {
     displayName: "c11p07: Circle with Secant",
     prompt:
-      "Which of the following diagrams is the length of PS represented by PQ*PR/PU?",
+      "Which of the following diagrams is the length of $PS$ represented by $\\frac{PQ \\times PR}{PU}$?",
     substance: geometryDomain.textbook_problems["c11p07.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   c11p12: {
@@ -265,7 +306,7 @@ forall Point \`D\`, \`E\`, \`A\` {
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   // NOTE: deprecated due to lack of conceptual prompt
@@ -281,12 +322,12 @@ forall Point \`D\`, \`E\`, \`A\` {
   // },
   c11p25: {
     displayName: "c11p25: Trapezoid inside Circle",
-    prompt: "In which of the following diagrams are AD = BC?",
+    prompt: "In which of the following diagrams are $AD = BC$?",
     substance: geometryDomain.textbook_problems["c11p25.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
     },
   },
   // NOTE: deprecated due to lack of conceptual prompt
@@ -302,12 +343,83 @@ forall Point \`D\`, \`E\`, \`A\` {
   // },
   c12p20: {
     displayName: "c12p20: Triangle Congruency",
-    prompt: "In which of the following diagrams is tABC congruent to tBCD?",
+    prompt:
+      "In which of the following diagrams is $\\triangle ABC$ congruent to $\\triangle BCD$?",
     substance: geometryDomain.textbook_problems["c12p20.substance"],
     domain: geometryDomain["geometry.domain"],
     style: geometryDomain["euclidean.style"] + ``,
     setting: {
-      ...defaultParams,
+      ...geometryParams,
+    },
+  },
+  lewis_0: {
+    displayName: "lewis_0: Hydrogen Cyanide",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{HCN}$.",
+    substance: molecules["hydrogencyanide.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_1: {
+    displayName: "lewis_1: Methane",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{CH_4}$.",
+    substance: molecules["methane.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_2: {
+    displayName: "lewis_2: Phosphorus Trichoride",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{PCl_3}$.",
+    substance: molecules["phosphorustrichloride.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_3: {
+    displayName: "lewis_3: Xenon Tetroxide",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{XeO_4}$.",
+    substance: molecules["xenontetroxide.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_4: {
+    displayName: "lewis_4: Phosgene",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{COCl_2}$.",
+    substance: molecules["phosgene.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_5: {
+    displayName: "lewis_5: Nitrogen",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{N_2}$.",
+    substance: molecules["nitrogen.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
+    },
+  },
+  lewis_6: {
+    displayName: "lewis_5: Hydrazine",
+    prompt: "Choose the correct Lewis structure for $\\mathrm{N_2H_4}$.",
+    substance: molecules["hydrazine.substance"],
+    domain: molecules["molecules.domain"],
+    style: molecules["lewis.style"],
+    setting: {
+      ...lewisParams,
     },
   },
 };
