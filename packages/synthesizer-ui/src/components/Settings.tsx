59b1697f88018676712eb911027e01fa4fcbcd67
wodeni <wn2155@columbia.edu>
2023-04-02 15:43:45 -0400

    chore: bump variation max to 50

--- packages/synthesizer-ui/src/components/Settings.tsx
+++ packages/synthesizer-ui/src/components/Settings.tsx
@@ -417,10 +417,13 @@ export class Settings extends React.Component<SettingsProps, SettingState> {
                 { value: 1, label: "1" },
                 { value: 10, label: "10" },
                 { value: 20, label: "20" },
+                { value: 30, label: "30" },
+                { value: 40, label: "40" },
+                { value: 50, label: "50" },
               ]}
               value={this.state.numPrograms}
               min={1}
-              max={20}
+              max={50}
               onChange={this.onProgCountChange}
             />
           </SettingDiv>
