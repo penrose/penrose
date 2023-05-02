61b2328869499bed11791fb89c69506d43ec5229
wodeni <woden@cs.cmu.edu>
2023-05-02 20:25:37 +0000

    Deploying to gh-pages from @ penrose/penrose@a09d1939e5788e5f6587546fb6aee1f7dcc803a7 🚀

---
+++ storybook/assets/GridBox.stories-b367f377.js
@@ -0,0 +1,43 @@
+var s=Object.defineProperty;var e=(o,a)=>s(o,"name",{value:a,configurable:!0});import{G as r,F as i,p}from"./penroseBlue-1abd9645.js";import{B as m,C as c,D as d}from"./PenrosePrograms-625ebeb0.js";import{j as n}from"./jsx-runtime-9c5bc5e6.js";import"./index-74c5fbfa.js";import"./es.object.get-own-property-descriptor-2f3bcc00.js";import"./index-b79ff3a9.js";import"./Simple-40419beb.js";import"./web.url-1059d872.js";const G={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import { Gridbox } from "../Gridbox";
+import penroseBlue from "../themes/penroseBlue";
+import { continuousMap, error, oneSet } from "./PenrosePrograms";
+
+// const diagram = await getDiagram();
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/GridBox Component",
+  component: Gridbox,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+} as ComponentMeta<typeof Gridbox>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Gridbox> = (args) => (
+  <div style={{ width: "50%", height: "50%" }}>
+    <ThemeProvider theme={penroseBlue}>
+      <Gridbox
+        {...args}
+        metadata={[
+          { name: "Variation", data: args.variation },
+          { name: "Substance Program", data: args.substance },
+        ]}
+      />
+    </ThemeProvider>
+  </div>
+);
+
+export const ContinuousMap = Template.bind({});
+ContinuousMap.args = continuousMap;
+
+export const OneSet = Template.bind({});
+OneSet.args = oneSet;
+
+export const Error = Template.bind({});
+Error.args = error;
+`,locationsMap:{"continuous-map":{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}},"one-set":{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}},error:{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}}}}},title:"Example/GridBox Component",component:r},t=e(o=>n("div",{style:{width:"50%",height:"50%"},children:n(i,{theme:p,children:n(r,{...o,metadata:[{name:"Variation",data:o.variation},{name:"Substance Program",data:o.substance}]})})}),"Template"),l=t.bind({});l.args=m;const u=t.bind({});u.args=c;const g=t.bind({});g.args=d;const T=["ContinuousMap","OneSet","Error"];export{l as ContinuousMap,g as Error,u as OneSet,T as __namedExportsOrder,G as default};
+//# sourceMappingURL=GridBox.stories-b367f377.js.map
