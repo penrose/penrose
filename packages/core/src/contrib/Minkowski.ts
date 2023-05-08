d234a9673a479cfeca5f3192f4289f4a525e27f9
Yiliang Liang <liangyiliang@outlook.com>
2023-05-08 12:07:48 -0400

    fix: `halfPlaneSDF` and padding (#1360)

--- packages/core/src/contrib/Minkowski.ts
+++ packages/core/src/contrib/Minkowski.ts
@@ -3,7 +3,6 @@ import { ops } from "../engine/Autodiff";
 import {
   absVal,
   add,
-  addN,
   div,
   eq,
   ifCond,
@@ -87,7 +86,7 @@ export const halfPlaneSDF = (
   const normal = outwardUnitNormal(lineSegment, insidePoint);
   const alpha = ops.vdot(normal, lineSegment[0]);
   const alphaOther = maxN(otherPoints.map((p) => ops.vdot(normal, p)));
-  return neg(addN([alpha, alphaOther, padding]));
+  return add(neg(add(alpha, alphaOther)), padding);
 };
 
 /**
@@ -228,7 +227,7 @@ export const rectangleSignedDistance = (
  * @param point Testing point.
  * @param padding Padding applied to the polygon.
  */
-const containsConvexPolygonPoints = (
+export const containsConvexPolygonPoints = (
   p1: ad.Num[][],
   p2: ad.Num[],
   padding: ad.Num
