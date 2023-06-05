b36931f05ccfd386cceeb0ae2583a9afdacbbcd3
Sam Estep <sam@samestep.com>
2023-06-05 10:28:09 -0400

    build: update VitePress `cleanUrls` setting

--- packages/docs-site/.vitepress/config.ts
+++ packages/docs-site/.vitepress/config.ts
@@ -95,7 +95,7 @@ export default defineConfig({
   description:
     "Create beautiful diagrams just by typing math notation in plain text.",
 
-  cleanUrls: "without-subfolders",
+  cleanUrls: true,
   ignoreDeadLinks: true,
   outDir: "build",
 
