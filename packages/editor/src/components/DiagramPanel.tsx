0c7b9f0b16a1754ee0fdae8aabf1f0b1386e81cd
Rijul Jain <98900692+rjainrjain@users.noreply.github.com>
2023-06-06 05:04:41 -0700

    feat: `renderer` adds tightest viewbox metadata to SVG (#1444)
    Co-authored-by: Wode "Nimo" Ni <wn2155@columbia.edu>

--- packages/editor/src/components/DiagramPanel.tsx
+++ packages/editor/src/components/DiagramPanel.tsx
@@ -165,13 +165,18 @@ const SVGaddCode = (
   versionStr: string,
   variationStr: string
 ): void => {
-  svg.setAttribute("penrose", "0");
+  // Create custom <penrose> tag to store metadata, or grab it if it already exists
+  const metadataQuery = document.querySelector("penrose");
+  let metadata: Element;
 
-  // Create custom <penrose> tag to store metadata
-  const metadata = document.createElementNS(
-    "https://penrose.cs.cmu.edu/metadata",
-    "penrose"
-  );
+  if (metadataQuery === null) {
+    metadata = document.createElementNS(
+      "https://penrose.cs.cmu.edu/metadata",
+      "penrose"
+    );
+  } else {
+    metadata = metadataQuery!;
+  }
 
   // Create <version> tag for penrose version
   const version = document.createElementNS(
