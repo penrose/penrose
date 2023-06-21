b1a6338c8de72c51bdd1719cd60adf9787cc5a7a
wodeni <woden@cs.cmu.edu>
2023-06-21 17:52:00 +0000

    Deploying to gh-pages from @ penrose/penrose@8581efc9cac3211ca6732e1d7f1d5d542aa4fbc3 🚀

---
+++ edgeworth/8581efc9cac3211ca6732e1d7f1d5d542aa4fbc3/assets/scalar-vector-division.trio-97f859e9.js
@@ -0,0 +1,9 @@
+import{s as o,r,d as t}from"./matrix-ops.domain-ab0fa2b7.js";import"./index-ce4395dc.js";const e=`AutoLabel All
+Scalar c
+Vector u
+
+-- scalar-vector division
+Vector r := vdiv(u,c)
+Label r $\\vec{u}/c$
+
+`,n={substance:e,style:[{contents:o,resolver:r}],domain:t,variation:"DoesNotMatter"};export{n as default};
