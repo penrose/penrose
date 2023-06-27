65841610d3d902c44d4dc84336b0b571d1996fdd
wodeni <wn2155@columbia.edu>
2023-06-27 14:41:36 -0400

    chore: remove unused import

--- packages/docs-site/src/components/StagedDiagram.vue
+++ packages/docs-site/src/components/StagedDiagram.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { defineComponent } from "vue";
 import { applyReactInVue } from "veaury";
 import { StagedDiagram as StagedDiagramReact } from "@penrose/components";
 
