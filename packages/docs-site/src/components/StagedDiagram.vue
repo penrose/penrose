cc5065442e3b139a912c1174519ef214509e3fb3
wodeni <wn2155@columbia.edu>
2023-06-27 22:30:22 -0400

    fix: revert client only loading and remove srcmap

--- packages/docs-site/src/components/StagedDiagram.vue
+++ packages/docs-site/src/components/StagedDiagram.vue
@@ -1,26 +1,16 @@
 <script setup lang="ts">
-import { defineAsyncComponent, defineComponent, h, onMounted } from "vue";
+import { defineAsyncComponent } from "vue";
 
-let StagedDiagram = defineComponent((props) => {
-  return () => {
-    return h("div");
-  };
-});
-
-onMounted(() => {
-  StagedDiagram = defineAsyncComponent(async () => {
-    const { applyPureReactInVue } = await import("veaury");
-    const StagedDiagram = (
-      await import("@penrose/components/dist/StagedDiagram.js")
-    ).default;
-    return applyPureReactInVue(StagedDiagram);
-  });
+const StagedDiagram = defineAsyncComponent(async () => {
+  const { applyPureReactInVue } = await import("veaury");
+  const StagedDiagram = (
+    await import("@penrose/components/dist/StagedDiagram.js")
+  ).default;
+  return applyPureReactInVue(StagedDiagram);
 });
 const props = defineProps(["trio"]);
 </script>
 
 <template>
-  <ClientOnly>
-    <StagedDiagram :trio="props.trio" />
-  </ClientOnly>
+  <StagedDiagram :trio="props.trio" />
 </template>
--- packages/docs-site/vite.config.ts
+++ packages/docs-site/vite.config.ts
@@ -3,6 +3,6 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { target: "esnext", sourcemap: true },
+  build: { target: "esnext" },
   plugins: [topLevelAwait()],
 });
