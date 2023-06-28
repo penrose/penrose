12c2f54f354f025b5bbe0bded8813e2412ddfba4
wodeni <wn2155@columbia.edu>
2023-06-27 21:02:03 -0400

    chore: attempt to work around ssr

--- packages/docs-site/src/components/StagedDiagram.vue
+++ packages/docs-site/src/components/StagedDiagram.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
+import { defineAsyncComponent, defineComponent, h, onMounted } from "vue";
 
-const StagedDiagram = defineAsyncComponent(async () => {
-  const { applyReactInVue } = await import("veaury");
-  const StagedDiagram = (
-    await import("@penrose/components/dist/StagedDiagram.js")
-  ).default;
-  return applyReactInVue(StagedDiagram);
+let StagedDiagram = defineComponent((props) => {
+  return () => {
+    return h("div");
+  };
 });
 
+onMounted(() => {
+  StagedDiagram = defineAsyncComponent(async () => {
+    const { applyReactInVue } = await import("veaury");
+    const StagedDiagram = (
+      await import("@penrose/components/dist/StagedDiagram.js")
+    ).default;
+    return applyReactInVue(StagedDiagram);
+  });
+});
 const props = defineProps(["trio"]);
 </script>
 
 <template>
-  <StagedDiagram :trio="props.trio" />
+  <ClientOnly>
+    <StagedDiagram :trio="props.trio" />
+  </ClientOnly>
 </template>
