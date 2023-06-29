9692094c8961f7b157ec9d1f802becfcf0b7b296
wodeni <woden@cs.cmu.edu>
2023-06-29 18:24:16 +0000

    Deploying to gh-pages from @ penrose/penrose@5c11a6d152d74735f666945852ea57045356e289 🚀

---
+++ edgeworth/5c11a6d152d74735f666945852ea57045356e289/assets/matrix-matrix-multiplication.trio-6876ac2f.js
@@ -0,0 +1,7 @@
+import{s as t,r as a,d as i}from"./matrix-ops.domain-ff5001ab.js";import"./index-6d8b3b93.js";const r=`AutoLabel All
+Matrix X, Y
+
+-- matrix-matrix multiplication
+Matrix Z := mmmul(X,Y)
+
+`,s={substance:r,style:[{contents:t,resolver:a}],domain:i,variation:"DoesNotMatter"};export{s as default};
---
+++ edgeworth/5c11a6d152d74735f666945852ea57045356e289/assets/matrix-vector-left-multiplication.trio-3ed9cb7e.js
@@ -0,0 +1,9 @@
+import{s as t,r as o,d as e}from"./matrix-ops.domain-ff5001ab.js";import"./index-6d8b3b93.js";const r=`AutoLabel All
+Matrix A
+Vector u
+
+-- matrix-vector left multiplication
+Vector Au := mvmul(A,u)
+Label Au $Au$
+
+`,i={substance:r,style:[{contents:t,resolver:o}],domain:e,variation:"DoesNotMatter"};export{i as default};
