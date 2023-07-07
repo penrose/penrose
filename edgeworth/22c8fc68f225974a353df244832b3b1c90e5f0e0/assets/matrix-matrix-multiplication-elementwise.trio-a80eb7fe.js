a179ddfef1a1ab929e9c7d91af1221095be79dbb
wodeni <woden@cs.cmu.edu>
2023-07-07 01:54:58 +0000

    Deploying to gh-pages from @ penrose/penrose@22c8fc68f225974a353df244832b3b1c90e5f0e0 🚀

---
+++ edgeworth/22c8fc68f225974a353df244832b3b1c90e5f0e0/assets/matrix-matrix-multiplication-elementwise.trio-a80eb7fe.js
@@ -0,0 +1,7 @@
+import{s as t,r as e,d as i}from"./matrix-ops.domain-7fe699ab.js";import"./index-3083d8fe.js";const a=`AutoLabel All
+Matrix X, Y
+
+-- elementwise matrix-matrix multiplication
+Matrix Z := ewmmmul(X,Y)
+
+`,s={substance:a,style:[{contents:t,resolver:e}],domain:i,variation:"DoesNotMatter",excludeWarnings:[]};export{s as default};
---
+++ edgeworth/22c8fc68f225974a353df244832b3b1c90e5f0e0/assets/triangle-centers.trio-9fd20c87.js
@@ -0,0 +1,23 @@
+import{s as e,r as n,d as i}from"./triangle-mesh-2d.domain-21f5b2b7.js";import"./index-3083d8fe.js";const a=`-- create a triangle with vertices i, j, k
+Vertex i, j, k
+Edge ij := MakeEdge(i,j)
+Edge jk := MakeEdge(j,k)
+Edge ki := MakeEdge(k,i)
+Triangle ijk := MakeTriangle(i,j,k)
+
+-- draw two different triangle centers
+Point a, b
+a := Circumcenter(ijk)
+b := Incenter(ijk)
+
+Circle Ca := Circumcircle(ijk)
+Circle Cb := Incircle(ijk)
+
+AutoLabel All
+NoLabel ij
+NoLabel jk
+NoLabel ki
+Label i "i"
+Label j "j"
+Label k "k"
+`,l={substance:a,style:[{contents:e,resolver:n}],domain:i,variation:"ClamshellKookabura228",excludeWarnings:[]};export{l as default};
