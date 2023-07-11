b4fdb022284f8ad00b05cfa286984dc07c756f65
samestep <sam@samestep.com>
2023-07-11 00:12:26 +0000

    Deploying to gh-pages from @ penrose/penrose@99b7b1cdbc3ae1a3cf87b3003977cf393310d1d8 🚀

---
+++ edgeworth/99b7b1cdbc3ae1a3cf87b3003977cf393310d1d8/assets/c05p13.trio-137605f4.js
@@ -0,0 +1,14 @@
+import{a as e,i as n,j as t,k as o,l as r}from"./index-3083d8fe.js";const a=e("geometry-domain/textbook_problems"),s=`global {
+    shape background = Rectangle {
+        center: (0, 0)
+        width: canvas.width
+        height: canvas.height
+        fillColor: #DDEDF4
+    } 
+}
+
+forall Point p
+with Triangle T; Point t1, t2, t3
+where T := Triangle(t1, t2, t3); Incenter(p, T) {
+  override T.incenterIcon.strokeColor = setOpacity(Colors.darkpurple, 0.8)
+}`,l={substance:n,style:[{contents:t,resolver:o},{contents:s,resolver:a}],domain:r,variation:"RationalityZebra567",excludeWarnings:["BBoxApproximationWarning"]};export{l as default};
---
+++ edgeworth/99b7b1cdbc3ae1a3cf87b3003977cf393310d1d8/assets/vector-vector-multiplication-elementwise.trio-37885565.js
@@ -0,0 +1,7 @@
+import{s as e,r as t,d as o}from"./matrix-ops.domain-7fe699ab.js";import"./index-3083d8fe.js";const n=`AutoLabel All
+Vector u, v
+
+-- elementwise vector-vector multiplication
+Vector a := ewvvmul(u,v)
+Label a $u*v$
+`,a={substance:n,style:[{contents:e,resolver:t}],domain:o,variation:"DoesNotMatter",excludeWarnings:[]};export{a as default};
