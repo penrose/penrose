cc37429ecbf45ca6bcdf4c5365330e0a0d8ddcbe
keenancrane <Keenan.Crane@gmail.com>
2023-07-12 12:35:47 +0000

    Deploying to gh-pages from @ penrose/penrose@d09b3d64f49710c0ee58b96f79f1c26561caa4f8 🚀

---
+++ edgeworth/d09b3d64f49710c0ee58b96f79f1c26561caa4f8/assets/triangles-a533c225.js
@@ -0,0 +1,1 @@
+import{r as t,T as a}from"./triangles-a671e370.js";import"./index-392b0829.js";const i=async()=>{const e=document.createElement("div");return await new Promise(n=>{const r=()=>{n(void 0)};t(()=>a({seed:"foobar",theta:0,onFinish:r}),e)}),e.innerHTML};export{i as default};
