5dbf9414f13f12005924be418950932aca216f21
wodeni <wn2155@columbia.edu>
2023-07-17 21:52:04 -0400

    chore: remove log

--- packages/roger/bin/run.js
+++ packages/roger/bin/run.js
@@ -1,4 +1,2 @@
 #!/usr/bin/env node
-console.log("process.cwd()", process.cwd());
-console.log("process.env.INIT_CWD", process.env.INIT_CWD);
-import("../dist/index.js");
+import "../dist/index.js";
