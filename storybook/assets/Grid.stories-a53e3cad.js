a1dd8a77d63846a13f97d7e1222216e18887bbc4
wodeni <woden@cs.cmu.edu>
2023-07-20 18:00:28 +0000

    Deploying to gh-pages from @ penrose/penrose@1bd1e2d23c52239ef34977956db73c249edc9d31 🚀

---
+++ storybook/assets/Grid.stories-a53e3cad.js
@@ -0,0 +1,48 @@
+var h=Object.defineProperty;var s=(t,e)=>h(t,"name",{value:e,configurable:!0});import{l}from"./svg-86621b92.js";import{s as a,F as f}from"./styled-components.browser.esm-92ee2156.js";import{i as y}from"./Simple-586e92d6.js";import{R as x,r as C,j as o}from"./jsx-runtime-c9a26150.js";import{G as v,p as G}from"./penroseBlue-b23171a3.js";import{c as S}from"./PenrosePrograms-8d9944b8.js";import"./iframe-1cb58f23.js";import"./index-5b359ca1.js";import"./hoist-non-react-statics.cjs-d16626a5.js";import"./vector-wedge.substance-71ece7b3.js";const b=a.main`
+  flex-grow: 1;
+  margin-left: "4rem";
+`,M=a.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+`,P=a.div``,_=a.h3`
+  font-family: "Roboto Mono";
+  color: ${t=>t.theme.primary};
+`;class n extends x.Component{constructor(e){super(e),this.state={optimized:Array(e.diagrams.length)}}innerContent(){return this.props.diagrams.map(({substance:e,domain:u,style:c,variation:g},r)=>C.createElement(v,{...this.props.gridBoxProps,key:`grid-${r}`,name:`grid-${r}`,header:this.props.header(r),metadata:this.props.metadata(r),domain:u,style:c,gridIndex:r,substance:e,variation:g,excludeWarnings:[],onSelected:this.props.onSelected,onStateUpdate:(d,p)=>{this.setState(m=>{const i=[...m.optimized];return i[d]=y(p),this.props.onComplete&&l.every(i)&&this.props.onComplete(),{...m,optStatuses:i}}),this.props.onStateUpdate(d,p)},imageResolver:this.props.imageResolver,selected:this.props.selected&&this.props.selected.includes(r)}))}render(){const e=this.props.diagrams.length===0?o(P,{children:o(_,{children:"(Generated diagrams will appear here)"})}):this.innerContent();return o(b,{children:o(M,{children:e})})}}s(n,"Grid");try{n.displayName="Grid",n.__docgenInfo={description:"",displayName:"Grid",props:{diagrams:{defaultValue:null,description:"",name:"diagrams",required:!0,type:{name:"DiagramSource[]"}},metadata:{defaultValue:null,description:"",name:"metadata",required:!0,type:{name:"(i: number) => { name: string; data: string; }[]"}},header:{defaultValue:null,description:"",name:"header",required:!0,type:{name:"(i: number) => string"}},onSelected:{defaultValue:null,description:"",name:"onSelected",required:!1,type:{name:"((n: number) => void)"}},onComplete:{defaultValue:null,description:"",name:"onComplete",required:!1,type:{name:"(() => void)"}},onStateUpdate:{defaultValue:null,description:"",name:"onStateUpdate",required:!0,type:{name:"(n: number, s: State) => void"}},imageResolver:{defaultValue:null,description:"",name:"imageResolver",required:!1,type:{name:"PathResolver"}},gridBoxProps:{defaultValue:null,description:"",name:"gridBoxProps",required:!1,type:{name:"Partial<GridboxProps>"}},selected:{defaultValue:null,description:"",name:"selected",required:!1,type:{name:"number[]"}}}}}catch{}const F={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { range } from "lodash";
+import { ThemeProvider } from "styled-components";
+import { Grid } from "../Grid.js";
+import penroseBlue from "../themes/penroseBlue.js";
+import { continuousMap } from "./PenrosePrograms.js";
+
+// const diagram = await getDiagram();
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/Grid Component",
+  component: Grid,
+} as ComponentMeta<typeof Grid>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Grid> = (args) => (
+  <ThemeProvider theme={penroseBlue}>
+    <Grid
+      {...args}
+      header={(i) => \`Diagram \${i}\`}
+      metadata={(i) => [
+        { name: "Variation", data: args.diagrams[i].variation },
+      ]}
+    />
+  </ThemeProvider>
+);
+
+export const ContinuousMap = Template.bind({});
+ContinuousMap.args = {
+  diagrams: range(10).map((n) => ({
+    ...continuousMap,
+    variation: \`\${n}\`,
+  })),
+};
+`,locationsMap:{"continuous-map":{startLoc:{col:46,line:17},endLoc:{col:1,line:27},startBody:{col:46,line:17},endBody:{col:1,line:27}}}}},title:"Example/Grid Component",component:n},V=s(t=>o(f,{theme:G,children:o(n,{...t,header:e=>`Diagram ${e}`,metadata:e=>[{name:"Variation",data:t.diagrams[e].variation}]})}),"Template"),q=V.bind({});q.args={diagrams:l.range(10).map(t=>({...S,variation:`${t}`}))};const I=["ContinuousMap"];export{q as ContinuousMap,I as __namedExportsOrder,F as default};
+//# sourceMappingURL=Grid.stories-a53e3cad.js.map
