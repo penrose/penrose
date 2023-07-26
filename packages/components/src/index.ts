ef81afaf0c034ba915af0b950e5d0203bfe03f98
Sam Estep <sam@samestep.com>
2023-07-26 13:38:59 -0400

    Run `yarn format`

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -12,17 +12,17 @@ import { SetupSubstanceMonaco } from "./editing/languages/SubstanceConfig.js";
 import fetchResolver from "./fetchPathResolver.js";
 import penroseBlue from "./themes/penroseBlue.js";
 export {
-  Simple,
-  Embed,
-  Grid,
-  MultipleChoiceProblem,
-  StagedDiagram,
-  Listing,
   Demo,
   EditorPane,
+  Embed,
+  Grid,
+  Listing,
+  MultipleChoiceProblem,
   SetupDomainMonaco,
-  SetupSubstanceMonaco,
   SetupStyleMonaco,
+  SetupSubstanceMonaco,
+  Simple,
+  StagedDiagram,
   fetchResolver,
   penroseBlue,
 };
