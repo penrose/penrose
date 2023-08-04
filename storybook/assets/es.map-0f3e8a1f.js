9c78153dcbbde763d99c1a84c71c94bda235ecfd
liangyiliang <liangyiliang@outlook.com>
2023-08-04 20:06:44 +0000

    Deploying to gh-pages from @ penrose/penrose@83f386950415a0d839ce95ba080c9ed36c4413b7 🚀

---
+++ storybook/assets/es.map-0f3e8a1f.js
@@ -0,0 +1,2 @@
+var r=Object.defineProperty;var o=(n,t)=>r(n,"name",{value:t,configurable:!0});import{am as e,an as a}from"./iframe-7d4f7288.js";var c=e,i=a;c("Map",function(n){return o(function(){return n(this,arguments.length?arguments[0]:void 0)},"Map")},i);
+//# sourceMappingURL=es.map-0f3e8a1f.js.map
