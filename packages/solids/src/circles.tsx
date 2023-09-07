4e3ba3bcfb7f4f40d394e681ab358311f378237c
Hwei-Shin Harriman <hweishih@GS23545.SP.CS.CMU.EDU>
2023-09-07 15:28:50 -0400

    dragging and resampling works, a bit of code cleanup

--- packages/solids/src/circles.tsx
+++ packages/solids/src/circles.tsx
@@ -13,28 +13,23 @@ import {
   textBBox,
   variable,
 } from "@penrose/core";
-import { greaterThan, inRange } from "@penrose/core/dist/contrib/Constraints";
+import { greaterThanSq, inRange } from "@penrose/core/dist/contrib/Constraints";
 import { toPt } from "@penrose/core/dist/contrib/Utils";
-import { Num, Pt2 } from "@penrose/core/dist/types/ad";
+import { Num, Problem, Pt2 } from "@penrose/core/dist/types/ad";
 import { createEffect, createResource, on } from "solid-js";
 import { createMutable } from "solid-js/store";
 import { num, signalNum } from "./util.js";
 
+// TODO constraints vs encourages? disjoint is working ehhh, no label overlap
 interface SetCircleProps {
   center: Var[];
   r: number;
 }
 
-interface SetsProps {
-  sets: SetCircle[];
-  labels: Label[];
-  onFinish?: () => void;
-}
-
-const canvasWidth = 800;
-const canvasHeight = 800;
-let svg: SVGSVGElement; // this should be the reference to the circle
-const ids = new Set(["s1-circle", "s2-circle", "s3-circle", "s1", "s2", "s3"]);
+const [CANVAS_WIDTH, CANVAS_HEIGHT] = [800, 800];
+const frozen = new Set();
+let svg: SVGSVGElement;
+let constraints: Num[];
 
 class Label {
   pt: Var[];
@@ -42,8 +37,11 @@ class Label {
   private fontSize = "40px";
   private fontFamily = "STIXGeneral-Italic";
 
-  constructor(x: number, y: number, txt: string) {
-    this.pt = [createMutable(variable(x)), createMutable(variable(y))];
+  constructor(txt: string) {
+    this.pt = [
+      createMutable(variable(Math.random() * CANVAS_WIDTH)),
+      createMutable(variable(Math.random() * CANVAS_HEIGHT)),
+    ];
     this.txt = txt;
   }
 
@@ -77,7 +75,6 @@ class Label {
   };
 
   render = () => {
-    // const [x, y] = toCanvas([num(pt[0]), num(pt[1])]);
     return (
       <g onMouseDown={(e) => onMouseDown(e, svg, this.pt)} id={`${this.txt}`}>
         {/* {this.getCorners().map((c) => this.renderPt(c))} */}
@@ -109,10 +106,17 @@ class Label {
 class SetCircle {
   center: Var[];
   r: number;
-  constructor(cx: number, cy: number, r: number) {
-    this.center = [createMutable(variable(cx)), createMutable(variable(cy))];
+  color: string;
+  label: string;
+  constructor(r: number, color: string, label: string) {
+    this.center = [
+      createMutable(variable(Math.random() * CANVAS_WIDTH)),
+      createMutable(variable(Math.random() * CANVAS_HEIGHT)),
+    ];
     // this.r = createMutable(variable(r));
     this.r = r;
+    this.color = color;
+    this.label = label;
   }
 
   getSet = (): SetCircleProps => {
@@ -122,17 +126,17 @@ class SetCircle {
     };
   };
 
-  render = (fill: string, label: string) => {
+  render = () => {
     return (
       <g
         onMouseDown={(e) => onMouseDown(e, svg, this.center)}
-        id={`${label}-circle`}
+        id={`${this.label}-circle`}
       >
         <circle
           cx={num(this.center[0])}
           cy={num(this.center[1])}
           r={this.r}
-          fill={fill}
+          fill={this.color}
         ></circle>
       </g>
     );
@@ -157,20 +161,14 @@ const getPosition = (
   return { x: 0, y: 0 };
 };
 
-//keep this
 const onMouseDown = (e: MouseEvent, parent: SVGSVGElement, val: Var[]) => {
-  const target = e.target as SVGSVGElement;
-  console.log(target.innerHTML);
   const { clientX, clientY } = e;
   let { x: tempX, y: tempY } = getPosition({ clientX, clientY }, parent);
-  let dx = 0,
-    dy = 0;
+  let [dx, dy] = [0, 0];
   const onMouseMove = (e: MouseEvent) => {
     const { x, y } = getPosition(e, parent);
-    dx = x - tempX;
-    dy = y - tempY;
-    tempX = x;
-    tempY = y;
+    [dx, dy] = [x - tempX, y - tempY];
+    [tempX, tempY] = [x, y];
     // set the actual values
     val[0].val = val[0].val + dx;
     val[1].val = val[1].val + dy;
@@ -178,10 +176,11 @@ const onMouseDown = (e: MouseEvent, parent: SVGSVGElement, val: Var[]) => {
   const onMouseUp = (e: MouseEvent) => {
     document.removeEventListener("mouseup", onMouseUp);
     document.removeEventListener("mousemove", onMouseMove);
-    // remove transform
-    ids.delete(target.id);
-    target.setAttribute(`transform`, "");
+
+    frozen.add(val);
     //trigger re-optimization
+    optimizeLayout();
+    frozen.delete(val);
   };
   document.addEventListener("mouseup", onMouseUp);
   document.addEventListener("mousemove", onMouseMove);
@@ -200,14 +199,14 @@ class Constraints {
 
   static onCanvas = (s: SetCircle, padding: number = 30) => {
     return [
-      inRange(s.center[0], s.r + padding, canvasWidth - (s.r + padding)),
-      inRange(s.center[1], s.r + padding, canvasHeight - (s.r + padding)),
+      inRange(s.center[0], s.r + padding, CANVAS_WIDTH - (s.r + padding)),
+      inRange(s.center[1], s.r + padding, CANVAS_HEIGHT - (s.r + padding)),
     ];
     // const canvasCorners = [
-    //   toPt([canvasWidth, 0]),
+    //   toPt([CANVAS_WIDTH, 0]),
     //   toPt([0, 0]),
-    //   toPt([0, canvasHeight]),
-    //   toPt([canvasWidth, canvasHeight]),
+    //   toPt([0, CANVAS_HEIGHT]),
+    //   toPt([CANVAS_WIDTH, CANVAS_HEIGHT]),
     // ];
     // return containsRectCircle(canvas, toPt(s.center), num(s.r), num(padding));
   };
@@ -222,133 +221,94 @@ class Constraints {
     padding: number = 30,
   ) => {
     const toNumArr = (c: Var[]) => c.map((v) => v.val);
-    const minDist = Math.max(s1.r, s2.r);
-    return greaterThan(
+    const minDist = s1.r + s2.r + padding;
+    console.log(minDist);
+    return greaterThanSq(
       dist(toNumArr(s2.center), toNumArr(s1.center)),
       minDist,
-      padding,
     );
+  };
+
+  static build = (sets: SetCircle[], labels: Label[]) => {
+    const [s1, s2, s3] = sets;
+    const [l1, l2, l3] = labels;
+    // subsets
+    const s1containss2 = Constraints.isSubset(s1, s2);
+    const s1containss3 = Constraints.isSubset(s1, s3);
+
+    // labels
+    const s1l1 = Constraints.containsText(s1, l1);
+    const s2l2 = Constraints.containsText(s2, l2);
+    const s3l3 = Constraints.containsText(s3, l3);
+
+    const s1onCanvas = Constraints.onCanvas(s1);
+
+    const s2s3disjoint = Constraints.disjointCircles(s2, s3);
+    return [
+      s1containss2,
+      s1containss3,
+      s1onCanvas[0],
+      s1onCanvas[1],
+      s2s3disjoint,
+      s1l1,
+      s2l2,
+      s3l3,
+    ];
   };
 }
 
-export const Sets = (props: SetsProps) => {
-  const s1color = "#3498db";
-  const s2color = "#ab91eb";
-  const s3color = "#cbbaf5";
-  const [s1, s2, s3] = props.sets;
-  const [l1, l2, l3] = props.labels;
+const resample = (problem?: Problem) => {
+  if (problem !== undefined) {
+    const run = problem.start({ freeze: (x) => frozen.has(x) }).run({});
+    console.log(run);
+    for (const [v, x] of run.vals) v.val = x;
+  }
+};
 
-  // subsets
-  const s1containss2 = Constraints.isSubset(s1, s2);
-  const s1containss3 = Constraints.isSubset(s1, s3);
-
-  // labels
-  const s1l1 = Constraints.containsText(s1, l1);
-  const s2l2 = Constraints.containsText(s2, l2);
-  const s3l3 = Constraints.containsText(s3, l3);
-
-  const s1onCanvas = Constraints.onCanvas(s1);
-  // const s2onCanvas = Constraints.onCanvas(s2);
-  // const s3onCanvas = Constraints.onCanvas(s3);
-
-  const s2s3disjoint = Constraints.disjointCircles(s2, s3);
-
+const optimizeLayout = () => {
   const waiting: Promise<void>[] = [];
   const [compiling] = createResource(() => {
     const prob = problem({
-      constraints: [
-        s1containss2,
-        s1containss3,
-        s1onCanvas[0],
-        s1onCanvas[1],
-        s2s3disjoint,
-        // s2onCanvas[0],
-        // s2onCanvas[1],
-        // s3onCanvas[0],
-        // s3onCanvas[1],
-        s1l1,
-        s2l2,
-        s3l3,
-      ],
+      constraints: constraints,
     });
     waiting.push(prob.then(() => {}));
     return prob;
   });
 
-  // TODO need to create effect to RUN the optimization for something related to labels here?
-  const centers = () => props.sets;
   createEffect(
-    on([centers, compiling], ([ps, prob]) => {
-      // TODO need this to be triggered by mouseup and onload conditions
-      if (prob !== undefined) {
-        // TODO need a map between id and penrose object for freeze? :\
-        // const run = prob.start({ freeze: (x) => !ids.has(x) }).run({});
-        const run = prob.start({}).run({});
-        console.log(run);
-        for (const [v, x] of run.vals) v.val = x;
-      }
+    on(compiling, (prob) => {
+      resample(prob);
     }),
-  );
-
-  createEffect(async () => {
-    const f = props.onFinish;
-    if (f) {
-      await Promise.all(waiting);
-      f();
-    }
-  });
-
-  return (
-    <div style={{ display: "flex", "align-items": "center" }}>
-      <svg
-        ref={svg}
-        width={canvasWidth}
-        height={canvasHeight}
-        style={{ "background-color": "lightgray" }}
-      >
-        <g>
-          {s1.render(s1color, "s1")}
-          {s2.render(s2color, "s2")}
-          {s3.render(s3color, "s3")}
-          {l1.render()}
-          {l2.render()}
-          {l3.render()}
-        </g>
-      </svg>
-    </div>
   );
 };
 
 export default () => {
-  const s1 = new SetCircle(
-    Math.random() * canvasWidth,
-    Math.random() * canvasHeight,
-    300,
-  );
-  const s2 = new SetCircle(
-    Math.random() * canvasWidth,
-    Math.random() * canvasHeight,
-    100,
-  );
-  const s3 = new SetCircle(230, 200, 100);
-  const l1 = new Label(
-    Math.random() * canvasWidth,
-    Math.random() * canvasHeight,
-    "s1",
-  );
-  const l2 = new Label(
-    Math.random() * canvasWidth,
-    Math.random() * canvasHeight,
-    "s222",
-  );
-  const l3 = new Label(300, 300, "s3");
-
+  const sets = [
+    new SetCircle(300, "#3498db", "s1"),
+    new SetCircle(100, "#ab91eb", "s2"),
+    new SetCircle(100, "#cbbaf5", "s3"),
+  ];
+  const labels = [new Label("s1"), new Label("s222"), new Label("s3")];
+  constraints = Constraints.build(sets, labels);
+  optimizeLayout();
   return (
     <div
       style={{ display: "flex", width: "100%", "justify-content": "center" }}
     >
       <div style={{ "font-family": "Lato", margin: "0 150px" }}>
-        <Sets sets={[s1, s2, s3]} labels={[l1, l2, l3]} />
+        <div style={{ display: "flex", "align-items": "center" }}>
+          <svg
+            ref={svg}
+            width={CANVAS_WIDTH}
+            height={CANVAS_HEIGHT}
+            style={{ "background-color": "lightgray" }}
+          >
+            <g>
+              {sets.map((set) => set.render())}
+              {labels.map((label) => label.render())}
+            </g>
+          </svg>
+        </div>
       </div>
     </div>
   );
