e0207aff773856edcf4478ee71cb186f835a4a2a
Yiliang Liang <liangyiliang@outlook.com>
2023-09-20 11:36:02 -0400

    fix: style selector on invalid function names (#1635)

--- packages/core/src/compiler/Style.ts
+++ packages/core/src/compiler/Style.ts
@@ -486,7 +486,7 @@ const checkRelPattern = (
       const { type: vtype } = res1.value; // ignore env
 
       // G |- E : T2
-      const res2 = checkExpr(toSubExpr(varEnv, rel.expr), varEnv);
+      const res2 = checkExpr(toSubExpr(rel.expr), varEnv);
 
       // TODO(error)
       if (isErr(res2)) {
@@ -856,45 +856,22 @@ export const substituteRel = (
 //#endregion (subregion? TODO fix)
 
 // Convert Style expression to Substance expression (for ease of comparison in matching)
-// Note: the env is needed to disambiguate SEFuncOrValCons
-const toSubExpr = <T>(env: Env, e: SelExpr<T>): SubExpr<T> => {
+const toSubExpr = <T>(e: SelExpr<T>): SubExpr<T> => {
   switch (e.tag) {
     case "SEBind": {
       return e.contents.contents;
     }
-    case "SEFunc": {
-      return {
-        ...e, // Puts the remnants of e's ASTNode info here -- is that ok?
-        tag: "ApplyFunction",
-        name: e.name,
-        args: e.args.map((e) => toSubExpr(env, e)),
-      };
-    }
-    case "SEValCons": {
-      return {
-        ...e,
-        tag: "ApplyConstructor",
-        name: e.name,
-        args: e.args.map((e) => toSubExpr(env, e)),
-      };
-    }
+    case "SEFunc":
+    case "SEValCons":
     case "SEFuncOrValCons": {
-      let tag: "ApplyFunction" | "ApplyConstructor";
-      if (env.constructors.has(e.name.value)) {
-        tag = "ApplyConstructor";
-      } else if (env.functions.has(e.name.value)) {
-        tag = "ApplyFunction";
-      } else {
-        // TODO: return TypeNotFound instead
-        throw new Error(
-          `Style internal error: expected '${e.name.value}' to be either a constructor or function, but was not found`,
-        );
-      }
+      // keep everything as generic Func
+      // since the Substance checker would automatically distinguish
+      // between ValCons and Func.
       const res: SubExpr<T> = {
         ...e,
-        tag,
+        tag: "Func",
         name: e.name,
-        args: e.args.map((e) => toSubExpr(env, e)),
+        args: e.args.map((e) => toSubExpr(e)),
       };
       return res;
     }
