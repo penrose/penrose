cd49c379a505c4dd8c6f8d225f7c538aacc46a6b
Sam Estep <sam@samestep.com>
2023-09-21 14:29:45 -0400

    Make `fetch` work in Node 16

--- packages/examples/setupVitest.js
+++ packages/examples/setupVitest.js
@@ -1,26 +1,3 @@
 import * as fs from "fs/promises";
 
-global.fetch = async (url) => {
-  if (url.href.startsWith("file:")) {
-    // Extract the local file path from the URL
-    const localFilePath = url.href.replace("file://", "");
-
-    try {
-      // Read the local file
-      const data = await fs.readFile(localFilePath);
-
-      // Return a response-like object
-      return new Response(data, {
-        status: 200,
-        headers: { "Content-Type": "application/wasm" },
-      });
-    } catch (error) {
-      return {
-        ok: false,
-        status: 404,
-        text: () => Promise.resolve("Not Found"),
-        // ... handle other errors as needed
-      };
-    }
-  } else throw Error("Only file:// URLs are supported");
-};
+global.fetch = (url) => fs.readFile(url.href.replace("file://", ""));
