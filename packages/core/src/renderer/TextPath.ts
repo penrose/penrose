7c18deecb62635477eb9e37808c3ca9810a9ac34
jiri.minarcik <jiri.minarcik@resistant.ai>
2023-10-14 23:32:28 +0200

    fix: path svg rendering

--- packages/core/src/renderer/TextPath.ts
+++ packages/core/src/renderer/TextPath.ts
@@ -9,36 +9,64 @@ import {
 import { toPathString } from "./Path.js";
 import { RenderProps } from "./Renderer.js";
 
-const RenderTextPath = (
+export const RenderTextPath = (
   shape: TextPath<number>,
   { canvasSize }: RenderProps,
-): SVGTextPathElement => {
-  const elem = document.createElementNS(
+): SVGGElement => {
+  // Create the group element
+  const groupElem = document.createElementNS("http://www.w3.org/2000/svg", "g");
+
+  // Generate a unique ID for linking textPath and path
+  const uniqueID = `path_${Math.random().toString(36).substring(2, 15)}`;
+
+  // Create the path element
+  const pathElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path",
+  );
+  pathElem.setAttribute("id", uniqueID);
+  pathElem.setAttribute("d", toPathString(shape.path.contents, canvasSize));
+  pathElem.setAttribute("style", "visibility: hidden");
+
+  // Append the path to the group
+  groupElem.appendChild(pathElem);
+
+  // Create the text element
+  const textElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "text",
+  );
+
+  // Create the textPath element
+  const textPathElem = document.createElementNS(
     "http://www.w3.org/2000/svg",
     "textPath",
   );
-  // Keep track of which input properties we programatically mapped
+  textPathElem.setAttributeNS(
+    "http://www.w3.org/1999/xlink",
+    "xlink:href",
+    `#${uniqueID}`,
+  );
+
+  textPathElem.setAttribute(
+    "startOffset",
+    shape.startOffset.contents.toString(),
+  );
+  textPathElem.setAttribute("method", shape.method.contents);
+  textPathElem.setAttribute("spacing", shape.spacing.contents);
+
   const attrToNotAutoMap: string[] = [];
-
-  // Map/Fill the shape attributes while keeping track of input properties mapped
-  attrToNotAutoMap.push(...attrFill(shape, elem));
-  attrToNotAutoMap.push(...attrStroke(shape, elem));
-  attrToNotAutoMap.push(...attrTitle(shape, elem));
-  attrToNotAutoMap.push(...attrString(shape, elem));
-  //   attrToNotAutoMap.push(...attrFont(shape, elem));
-
-  // Assuming TextPath has a `path` property for path definition
-  elem.setAttribute("startOffset", shape.startOffset.contents.toString());
-  elem.setAttribute("method", shape.method.contents);
-  elem.setAttribute("spacing", shape.spacing.contents);
-  elem.setAttribute("path", toPathString(shape.path.contents, canvasSize));
-
+  attrToNotAutoMap.push(...attrFill(shape, textPathElem));
+  attrToNotAutoMap.push(...attrStroke(shape, textPathElem));
+  attrToNotAutoMap.push(...attrTitle(shape, textPathElem));
+  attrToNotAutoMap.push(...attrString(shape, textPathElem));
   attrToNotAutoMap.push("startOffset", "method", "spacing");
 
-  // Directly Map across any "unknown" SVG properties
-  attrAutoFillSvg(shape, elem, attrToNotAutoMap);
+  attrAutoFillSvg(shape, textPathElem, attrToNotAutoMap);
 
-  return elem;
+  textElem.appendChild(textPathElem);
+  groupElem.appendChild(textElem);
+
+  return groupElem;
 };
-
 export default RenderTextPath;
