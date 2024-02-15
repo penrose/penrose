8535e3c8cbdbd008621427b8db1f7150a1b2767c
wodeni <woden@cs.cmu.edu>
2024-02-15 17:24:20 +0000

    Deploying to gh-pages from @ penrose/penrose@0f96b30b3d1e628024327eb53f68a2b625054c31 🚀

---
+++ storybook/assets/GridBox.stories-a2fc5597.js
@@ -0,0 +1,43 @@
+var s=Object.defineProperty;var e=(o,a)=>s(o,"name",{value:a,configurable:!0});import{F as i}from"./styled-components.browser.esm-aa8d2b2e.js";import{G as r,p}from"./penroseBlue-24645062.js";import{c as m,o as c,e as d}from"./PenrosePrograms-625c82ac.js";import{j as n}from"./jsx-runtime-3ab769c8.js";import"./index-5b359ca1.js";import"./hoist-non-react-statics.cjs-9cb6bb35.js";import"./iframe-69ce7d08.js";import"./Simple-0a704065.js";import"./svg-097c35ba.js";import"./vector-wedge.substance-c1850d65.js";const j={parameters:{storySource:{source:`import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import { Gridbox } from "../Gridbox.js";
+import penroseBlue from "../themes/penroseBlue.js";
+import { continuousMap, error, oneSet } from "./PenrosePrograms.js";
+
+// const diagram = await getDiagram();
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/GridBox Component",
+  component: Gridbox,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
+} as ComponentMeta<typeof Gridbox>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Gridbox> = (args) => (
+  <div style={{ width: "50%", height: "50%" }}>
+    <ThemeProvider theme={penroseBlue}>
+      <Gridbox
+        {...args}
+        metadata={[
+          { name: "Variation", data: args.variation },
+          { name: "Substance Program", data: args.substance },
+        ]}
+      />
+    </ThemeProvider>
+  </div>
+);
+
+export const ContinuousMap = Template.bind({});
+ContinuousMap.args = continuousMap;
+
+export const OneSet = Template.bind({});
+OneSet.args = oneSet;
+
+export const Error = Template.bind({});
+Error.args = error;
+`,locationsMap:{"continuous-map":{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}},"one-set":{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}},error:{startLoc:{col:49,line:20},endLoc:{col:1,line:32},startBody:{col:49,line:20},endBody:{col:1,line:32}}}}},title:"Example/GridBox Component",component:r},t=e(o=>n("div",{style:{width:"50%",height:"50%"},children:n(i,{theme:p,children:n(r,{...o,metadata:[{name:"Variation",data:o.variation},{name:"Substance Program",data:o.substance}]})})}),"Template"),l=t.bind({});l.args=m;const u=t.bind({});u.args=c;const g=t.bind({});g.args=d;const v=["ContinuousMap","OneSet","Error"];export{l as ContinuousMap,g as Error,u as OneSet,v as __namedExportsOrder,j as default};
+//# sourceMappingURL=GridBox.stories-a2fc5597.js.map
--- storybook/assets/javascript-2a62d001.js
+++
@@ -1,7 +0,0 @@
-import{conf as t,language as e}from"./typescript-12df777e.js";import"./Listing.stories-bd2c6c1a.js";import"./jsx-runtime-7cbe1778.js";import"./iframe-8830d19c.js";import"./index-212454b8.js";import"./svg-fa10b444.js";import"./PenrosePrograms-e6acd945.js";import"./vector-wedge.substance-00c05a9a.js";/*!-----------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Version: 0.31.1(337587859b1c171314b40503171188b6cea6a32a)
- * Released under the MIT license
- * https://github.com/microsoft/monaco-editor/blob/main/LICENSE.txt
- *-----------------------------------------------------------------------------*/var p=t,d={defaultToken:"invalid",tokenPostfix:".js",keywords:["break","case","catch","class","continue","const","constructor","debugger","default","delete","do","else","export","extends","false","finally","for","from","function","get","if","import","in","instanceof","let","new","null","return","set","super","switch","symbol","this","throw","true","try","typeof","undefined","var","void","while","with","yield","async","await","of"],typeKeywords:[],operators:e.operators,symbols:e.symbols,escapes:e.escapes,digits:e.digits,octaldigits:e.octaldigits,binarydigits:e.binarydigits,hexdigits:e.hexdigits,regexpctl:e.regexpctl,regexpesc:e.regexpesc,tokenizer:e.tokenizer};export{p as conf,d as language};
-//# sourceMappingURL=javascript-2a62d001.js.map
