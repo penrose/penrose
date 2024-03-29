099d1d0c817271cdd3e1d15fd5c9d1f888f05a62
wodeni <wn2155@columbia.edu>
2024-03-29 10:25:44 -0400

    build: update vite config to accommodate roseg

--- packages/solids/vite.config.ts
+++ packages/solids/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), topLevelAwait()],
   build: { target: "esnext" },
+  optimizeDeps: {
+    esbuildOptions: { target: "esnext" },
+    exclude: ["rose"],
+  },
 });
