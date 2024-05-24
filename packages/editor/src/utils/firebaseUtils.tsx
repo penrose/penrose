a791b11b942e237127e3c87197937f2f39fe5fcc
Kyle Lee <kylelee.sea@gmail.com>
2024-05-24 12:15:37 -0400

    cleaned up utils file

--- packages/editor/src/utils/firebaseUtils.tsx
+++ packages/editor/src/utils/firebaseUtils.tsx
@@ -1,9 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signOut } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import toast from "react-hot-toast";
-import { useResetRecoilState } from "recoil";
-import { diagramState } from "../state/atoms.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA_WZrM0sWpOt3oKmPp_D77rS7TsqaTC-w",
@@ -18,16 +15,3 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 export const authObject = getAuth(firebaseApp);
 export const db = getFirestore(firebaseApp);
-
-export const signOutWrapper = () => {
-  signOut(authObject)
-    .then(() => {
-      toast.success("logged out");
-    })
-    .catch((error) => {
-      toast.error("Error: Could not log out");
-    });
-
-  const resetDiagramState = useResetRecoilState(diagramState);
-  resetDiagramState();
-};
