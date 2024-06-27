88d3e647f0f41a645778ab9ea44a97d72e5484b5
Kyle Lee <kylelee.sea@gmail.com>
2024-06-27 16:06:51 -0400

    ran yarn format

--- packages/components/src/editing/parser/substance/substanceTokens.js
+++ packages/components/src/editing/parser/substance/substanceTokens.js
@@ -1,18 +1,26 @@
-import {ExternalTokenizer, ContextTracker} from "@lezer/lr"
-import {newline, LineComment, BlockComment, spaces, insertSemi} from "./substance.terms.js"
+import { ContextTracker, ExternalTokenizer } from "@lezer/lr";
+import {
+  BlockComment,
+  LineComment,
+  insertSemi,
+  newline,
+  spaces,
+} from "./substance.terms.js";
 
 export const trackNewline = new ContextTracker({
-    start: false,
-    shift(context, term) {
-      return term === LineComment || term === BlockComment || term === spaces
-        ? context : term === newline
-    },
-    strict: false
-  })
+  start: false,
+  shift(context, term) {
+    return term === LineComment || term === BlockComment || term === spaces
+      ? context
+      : term === newline;
+  },
+  strict: false,
+});
 
-export const insertSemicolon = new ExternalTokenizer((input, stack) => {
-    let {next} = input
-    if (next === -1 || stack.context)
-      input.acceptToken(insertSemi)
-  }, {contextual: true, fallback: true})
-  
+export const insertSemicolon = new ExternalTokenizer(
+  (input, stack) => {
+    let { next } = input;
+    if (next === -1 || stack.context) input.acceptToken(insertSemi);
+  },
+  { contextual: true, fallback: true },
+);
