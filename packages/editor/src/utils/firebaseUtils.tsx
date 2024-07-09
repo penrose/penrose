b3aa68fab5a491fbeb74892a8f4e64db986c19a7
Kyle Lee <kylelee.sea@gmail.com>
2024-07-09 12:24:45 -0400

    changed to use new firebase acc

--- packages/editor/src/utils/firebaseUtils.tsx
+++ packages/editor/src/utils/firebaseUtils.tsx
@@ -25,18 +25,18 @@ import {
 import { useClearAutosave } from "../state/callbacks.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_WZrM0sWpOt3oKmPp_D77rS7TsqaTC-w",
-  authDomain: "penrose-test.firebaseapp.com",
-  projectId: "penrose-test",
-  storageBucket: "penrose-test.appspot.com",
-  messagingSenderId: "997650567502",
-  appId: "1:997650567502:web:f46626596ca78b065d8e1f",
-  measurementId: "G-GMD2T1MJ24",
+  apiKey: "AIzaSyBhmlpWCnREhtaeVR9fpAllSDSu9UtCXkc",
+  authDomain: "penrose-32c8a.firebaseapp.com",
+  projectId: "penrose-32c8a",
+  storageBucket: "penrose-32c8a.appspot.com",
+  messagingSenderId: "898391995102",
+  appId: "1:898391995102:web:af47663b5e5e28b2eac84f",
+  measurementId: "G-W7X5S06DH1",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 export const authObject = getAuth(firebaseApp);
-export const db = getFirestore(firebaseApp);
+export const db = getFirestore(firebaseApp, "diagram-store");
 export const oauthProvider = new GithubAuthProvider();
 oauthProvider.addScope("gist");
 
