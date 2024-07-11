b1037475dcc6b71e4e75f87b410e6d00f57763bf
wodeni <woden@cs.cmu.edu>
2024-07-11 17:15:40 +0000

    Deploying to gh-pages from @ penrose/penrose@39f88c473ad09cb17c70e99606e2edb936a2a471 🚀

--- storybook/assets/vector-wedge.substance-49fb44bc.js
+++ storybook/assets/vector-wedge.substance-49fb44bc.js
@@ -1,4 +1,4 @@
-import{_ as e,__tla as m}from"./iframe-0fd4e399.js";let l,n,s,c,u,_=Promise.all([(()=>{try{return m}catch{}})()]).then(async()=>{let i;n=`type Scalar
+import{_ as e,__tla as m}from"./iframe-e48e659b.js";let l,n,s,c,u,_=Promise.all([(()=>{try{return m}catch{}})()]).then(async()=>{let i;n=`type Scalar
 
 -- define vectors and bivectors as subtypes of a base k-vector type
 type kVector
