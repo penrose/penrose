a501e55e17afcabbfeecaeabe60843d1950da25c
liangyiliang <liangyiliang@outlook.com>
2024-07-23 19:40:00 +0000

    Deploying to gh-pages from @ penrose/penrose@4e3fdc4cb6dcb4865560cc23201ed990119f09e5 🚀

---
+++ storybook/assets/Simple.stories-bab3ab2e.js
@@ -0,0 +1,16 @@
+import{j as _}from"./jsx-runtime-1438501e.js";import{S as h,__tla as y}from"./Simple-1ac5f9ca.js";import{c as f,o as v,e as x}from"./PenrosePrograms-0b0acd98.js";import"./index-f46741a2.js";import{__tla as E}from"./svg-83933681.js";import{__tla as w}from"./vector-wedge.substance-2d2afd02.js";import{__tla as b}from"./iframe-beb8bfef.js";import"../sb-preview/runtime.js";let r,a,e,u,g,j=Promise.all([(()=>{try{return y}catch{}})(),(()=>{try{return E}catch{}})(),(()=>{try{return w}catch{}})(),(()=>{try{return b}catch{}})()]).then(async()=>{var s,o,i,m,p,c,l,d,n;let t;g={title:"Example/Simple Component",component:h},t=S=>_.jsx("div",{style:{width:"50%",height:"50%"},children:_.jsx(h,{...S})}),r=t.bind({}),r.args=f,e=t.bind({}),e.args=v,a=t.bind({}),a.args=x,r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`args => <div style={{
+  width: "50%",
+  height: "50%"
+}}>
+    <Simple {...args} />
+  </div>`,...(i=(o=r.parameters)==null?void 0:o.docs)==null?void 0:i.source}}},e.parameters={...e.parameters,docs:{...(m=e.parameters)==null?void 0:m.docs,source:{originalSource:`args => <div style={{
+  width: "50%",
+  height: "50%"
+}}>
+    <Simple {...args} />
+  </div>`,...(c=(p=e.parameters)==null?void 0:p.docs)==null?void 0:c.source}}},a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`args => <div style={{
+  width: "50%",
+  height: "50%"
+}}>
+    <Simple {...args} />
+  </div>`,...(n=(d=a.parameters)==null?void 0:d.docs)==null?void 0:n.source}}},u=["ContinuousMap","OneSet","Error"]});export{r as ContinuousMap,a as Error,e as OneSet,u as __namedExportsOrder,j as __tla,g as default};
