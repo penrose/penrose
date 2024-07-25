0f3a534bcedf660ca3669168ee30105043b28d0d
Kyle Lee <kylelee.sea@gmail.com>
2024-07-25 14:44:44 -0400

    improved tooltip styling

--- packages/components/src/editing/hooks/style/styleTooltips.ts
+++ packages/components/src/editing/hooks/style/styleTooltips.ts
@@ -22,12 +22,14 @@ const createTooltip = (start: number, end: number, dom: HTMLDivElement) => {
   };
 };
 
-const createFunctionTooltipElt = (dict: any, name: string) => {
+const createFunctionDOM = (dict: any, name: string) => {
   let paramStr = toParamString(dict, name);
   let desc = dict.description ?? "";
+
   const md = Markdown();
   md.use(markdownItKatex);
   var result = md.render(desc);
+
   let dom = document.createElement("div");
   dom.classList.add("tooltip");
   dom.innerHTML =
@@ -35,84 +37,32 @@ const createFunctionTooltipElt = (dict: any, name: string) => {
   return dom;
 };
 
-const createFunctionTooltip = (
-  start: number,
-  end: number,
-  dict: any,
-  name: string,
-) => {
-  let paramStr = toParamString(dict, name);
-  let desc = dict.description ?? "";
-  const md = Markdown();
-  md.use(markdownItKatex);
-  var result = md.render(desc);
+const createPropertiesDOM = (shapeProps: string[], name: string) => {
+  let dom = document.createElement("div");
+  dom.classList.add("tooltip");
+  dom.innerHTML =
+    '<span style="color: #4B69C6;">' + name + " Properties:" + "</span>";
 
-  return {
-    pos: start,
-    end,
-    above: true,
-    create(view: EditorView) {
-      let dom = document.createElement("div");
-      dom.classList.add("tooltip");
-      dom.innerHTML =
-        '<span style="color: #4B69C6;">' + paramStr + "</span><br>" + result;
-      return { dom };
-    },
-  };
+  const ul = document.createElement("ul");
+  ul.style.margin = "0 10px";
+  ul.style.padding = "0 10px";
+
+  shapeProps.forEach((prop) => {
+    const li = document.createElement("li");
+    li.textContent = prop;
+    ul.appendChild(li);
+  });
+
+  dom.appendChild(ul);
+  return dom;
 };
 
-// Tooltip showing all properties that belong to a shape type in list form
-const createPropertiesTooltip = (
-  start: number,
-  end: number,
-  shapeProps: string[],
-  name: string,
-) => {
-  return {
-    pos: start,
-    end,
-    above: true,
-    create(view: EditorView) {
-      let dom = document.createElement("div");
-      dom.classList.add("tooltip");
-      dom.innerHTML =
-        '<span style="color: #4B69C6;">' + name + " Properties:" + "</span>";
-
-      const ul = document.createElement("ul");
-      ul.style.margin = "0 10px";
-      ul.style.padding = "0 10px";
-
-      shapeProps.forEach((prop) => {
-        const li = document.createElement("li");
-        li.textContent = prop;
-        ul.appendChild(li);
-      });
-
-      dom.appendChild(ul);
-      return { dom };
-    },
-  };
-};
-
-// Tooltip to show type info for one property belonging to a shape
-const createShapePropTooltip = (
-  start: number,
-  end: number,
-  propName: string,
-  type: string,
-) => {
-  return {
-    pos: start,
-    end,
-    above: true,
-    create(view: EditorView) {
-      let dom = document.createElement("div");
-      dom.classList.add("tooltip");
-      dom.innerHTML =
-        '<span style="color: #4B69C6;">' + propName + " : " + "</span>" + type;
-      return { dom };
-    },
-  };
+const createShapePropDOM = (propName: string, type: string) => {
+  let dom = document.createElement("div");
+  dom.classList.add("tooltip");
+  dom.innerHTML =
+    '<span style="color: #4B69C6;">' + propName + " : " + "</span>" + type;
+  return dom;
 };
 
 export const wordHover = hoverTooltip((view, pos, side) => {
@@ -130,17 +80,17 @@ export const wordHover = hoverTooltip((view, pos, side) => {
   const word = extractText(view.state.doc.toString(), cursor.to, cursor.from);
 
   if (isKeyOf(word, compDict)) {
-    let domElt = createFunctionTooltipElt(compDict[word], word);
+    let domElt = createFunctionDOM(compDict[word], word);
     return createTooltip(start, end, domElt);
   }
 
   if (isKeyOf(word, objDict)) {
-    let domElt = createFunctionTooltipElt(objDict[word], word);
+    let domElt = createFunctionDOM(objDict[word], word);
     return createTooltip(start, end, domElt);
   }
 
   if (isKeyOf(word, constrDict)) {
-    let domElt = createFunctionTooltipElt(constrDict[word], word);
+    let domElt = createFunctionDOM(constrDict[word], word);
     return createTooltip(start, end, domElt);
   }
 
@@ -152,15 +102,8 @@ export const wordHover = hoverTooltip((view, pos, side) => {
     const shapeDefs = getShapeDefs();
 
     if (word in shapeDefs) {
-      let text =
-        word + " Properties: " + Object.keys(shapeDefs[word]).join(", ");
-      return createPropertiesTooltip(
-        start,
-        end,
-        Object.keys(shapeDefs[word]),
-        word,
-      );
-      // return createTooltip(start, end, text);
+      let domElt = createPropertiesDOM(Object.keys(shapeDefs[word]), word);
+      return createTooltip(start, end, domElt);
     }
   }
 
@@ -179,16 +122,8 @@ export const wordHover = hoverTooltip((view, pos, side) => {
       );
 
       if (shapeName in shapeDefs && word in shapeDefs[shapeName]) {
-        // console.log(shapeDefs[shapeName]);
-        return createShapePropTooltip(
-          start,
-          end,
-          word,
-          shapeDefs[shapeName][word],
-        );
-
-        // const text = `${word}:${shapeDefs[shapeName][word]}`;
-        // return createTooltip(start, end, text);
+        let domElt = createShapePropDOM(word, shapeDefs[shapeName][word]);
+        return createTooltip(start, end, domElt);
       }
     }
   }
