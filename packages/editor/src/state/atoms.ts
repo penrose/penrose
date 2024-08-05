535c91b199d4d5ff47ca93a31082eb817c0a4bcd
wodeni <wn2155@columbia.edu>
2024-08-05 17:08:51 -0400

    fix: revert showkeybinding atom default

--- packages/editor/src/state/atoms.ts
+++ packages/editor/src/state/atoms.ts
@@ -139,7 +139,7 @@ export const autosaveTimerState = atom<AutosaveTimer>({
 
 export const showKeybindingsState = atom<boolean>({
   key: "showKeybindingsState",
-  default: true,
+  default: false,
 });
 
 /**
