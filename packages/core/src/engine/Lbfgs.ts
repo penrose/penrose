26a5817ffea3e097ca65050b406938a9733ff827
Griffin Teller <griffinteller@gmail.com>
2025-06-17 12:13:47 -0400

    Revert "fix: avoid non-finite values in  L-BFGS line search (#1903)"
    This reverts commit 0a18d1c25c9529714438e1271aefcc227b08b924.

--- packages/core/src/engine/Lbfgs.ts
+++ packages/core/src/engine/Lbfgs.ts
@@ -58,13 +58,6 @@ const dot = (u: Float64Array, v: Float64Array): number => {
   return z;
 };
 
-const allFinite = (x: Float64Array): boolean => {
-  for (let i = 0; i < x.length; i++) {
-    if (!isFinite(x[i])) return false;
-  }
-  return true;
-};
-
 /**
  * Return the line search step size, having set `x` to the new point.
  *
@@ -101,7 +94,7 @@ const lineSearch = (
     const isArmijo = fx <= fx0 + cfg.armijo * t * dufAtx0;
     const isWolfe = -dot(r, grad) >= cfg.wolfe * dufAtx0; // weak Wolfe condition
 
-    if (!isArmijo || !allFinite(x) || !allFinite(grad) || !isFinite(fx)) b = t;
+    if (!isArmijo) b = t;
     else if (!isWolfe) a = t;
     else break; // found good interval
 
